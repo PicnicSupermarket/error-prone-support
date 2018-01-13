73bbaedd6b4997248cfd39c8c65c8ee77a301649
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2018-01-13 15:14:17 +0100

    Document `MethodReferenceUsageCheck` TODO

--- error-prone-contrib/src/main/java/systems/picnic/errorprone/bugpatterns/MethodReferenceUsageCheck.java
+++ error-prone-contrib/src/main/java/systems/picnic/errorprone/bugpatterns/MethodReferenceUsageCheck.java
@@ -45,6 +45,8 @@ import javax.lang.model.element.Name;
 // - `i -> new int[i]` to `int[]::new`.
 // - `() -> new Foo()` to `Foo::new` (and variations).
 // XXX: For JDK 9, add support for "var handles"!
+// XXX: Link to Effective Java, Third Edition, Item 43. In there the suggested approach is not so
+// black-and-white. Maybe we can more closely approximate it?
 @AutoService(BugChecker.class)
 @BugPattern(
   name = "MethodReferenceUsage",
