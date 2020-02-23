99257e718cb4ce7c2579bb4ce5a4e95165e821c8
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2020-02-23 16:57:30 +0100

    Resolve `XXX` in `LexicographicalAnnotationAttributeListingCheck`

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/LexicographicalAnnotationAttributeListingCheck.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/LexicographicalAnnotationAttributeListingCheck.java
@@ -40,9 +40,10 @@ import java.util.stream.Stream;
 
 /**
  * A {@link BugChecker} which flags annotation array listings which aren't sorted lexicographically.
+ *
+ * <p>The idea behind this checker is that maintaining a sorted sequence simplifies conflict
+ * resolution, and can even avoid it if two branches add the same entry.
  */
-// XXX: Add more documentation. Explain that sorting reduces chances of conflicts and simplifies
-// their resolution when they do happen.
 @AutoService(BugChecker.class)
 @BugPattern(
     name = "LexicographicalAnnotationAttributeListing",
