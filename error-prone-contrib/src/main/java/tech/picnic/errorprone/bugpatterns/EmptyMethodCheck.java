d3b628db47f9c028f4d62d45702dcb81c2892147
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2020-02-23 16:29:43 +0100

    Finalize `EmptyMethodCheck`

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/EmptyMethodCheck.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/EmptyMethodCheck.java
@@ -1,5 +1,6 @@
 package tech.picnic.errorprone.bugpatterns;
 
+import com.google.auto.service.AutoService;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.LinkType;
 import com.google.errorprone.BugPattern.SeverityLevel;
@@ -7,31 +8,37 @@ import com.google.errorprone.BugPattern.StandardTags;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
+import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
+import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.MethodTree;
+import com.sun.tools.javac.code.Symbol.MethodSymbol;
 
-/** A {@link BugChecker} which flags empty methods. */
-// XXX: Disable until fixed.
-// @AutoService(BugChecker.class)
+/** A {@link BugChecker} which flags empty methods that seemingly can simply be deleted. */
+@AutoService(BugChecker.class)
 @BugPattern(
     name = "EmptyMethod",
     summary = "Empty method can likely be deleted",
     linkType = LinkType.NONE,
     severity = SeverityLevel.WARNING,
-    tags = StandardTags.LIKELY_ERROR)
+    tags = StandardTags.SIMPLIFICATION,
+    providesFix = BugPattern.ProvidesFix.REQUIRES_HUMAN_ATTENTION)
 public final class EmptyMethodCheck extends BugChecker implements MethodTreeMatcher {
   private static final long serialVersionUID = 1L;
 
-  // XXX: Restrict: only warn about:
-  // - static methods
-  // - methods that are provably not in an inheritance hierarchy.
-  // - public methods in a Mockito test class.
   @Override
   public Description matchMethod(MethodTree tree, VisitorState state) {
     if (tree.getBody() == null || !tree.getBody().getStatements().isEmpty()) {
       return Description.NO_MATCH;
     }
 
-    return buildDescription(tree).build();
+    MethodSymbol sym = ASTHelpers.getSymbol(tree);
+    if (sym == null
+        || ASTHelpers.methodCanBeOverridden(sym)
+        || ASTHelpers.hasAnnotation(sym, "java.lang.Override", state)) {
+      return Description.NO_MATCH;
+    }
+
+    return describeMatch(tree, SuggestedFix.delete(tree));
   }
 }
--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/EmptyMethodCheckTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/EmptyMethodCheckTest.java
@@ -1,19 +1,23 @@
 package tech.picnic.errorprone.bugpatterns;
 
+import com.google.errorprone.BugCheckerRefactoringTestHelper;
+import com.google.errorprone.BugCheckerRefactoringTestHelper.TestMode;
 import com.google.errorprone.CompilationTestHelper;
 import org.junit.jupiter.api.Test;
 
 public final class EmptyMethodCheckTest {
   private final CompilationTestHelper compilationTestHelper =
       CompilationTestHelper.newInstance(EmptyMethodCheck.class, getClass());
+  private final BugCheckerRefactoringTestHelper refactoringTestHelper =
+      BugCheckerRefactoringTestHelper.newInstance(new EmptyMethodCheck(), getClass());
 
   @Test
-  public void testNegative() {
+  public void testIdentification() {
     compilationTestHelper
         .addSourceLines(
             "A.java",
             "class A {",
-            "  Object m() {",
+            "  Object m1() {",
             "    return null;",
             "  }",
             "",
@@ -21,25 +25,38 @@ public final class EmptyMethodCheckTest {
             "    System.out.println(42);",
             "  }",
             "",
+            "  void m3() {}",
+            "",
+            "  // BUG: Diagnostic contains:",
+            "  static void m4() {}",
+            "",
             "  interface F {",
             "    void fun();",
             "  }",
+            "}")
+        .addSourceLines(
+            "B.java",
+            "final class B implements A.F {",
+            "  @Override",
+            "  public void fun() {}",
+            "",
+            "  // BUG: Diagnostic contains:",
+            "  void m3() {}",
             "}")
         .doTest();
   }
 
   @Test
-  public void testPositive() {
-    compilationTestHelper
-        .addSourceLines(
-            "A.java",
-            "class A {",
-            "  // BUG: Diagnostic contains:",
-            "  void m() {}",
+  public void testReplacement() {
+    refactoringTestHelper
+        .addInputLines(
+            "in/A.java",
+            "final class A {",
+            "  void instanceMethod() {}",
             "",
-            "  // BUG: Diagnostic contains:",
-            "  static void m2() {}",
+            "  static void staticMethod() {}",
             "}")
-        .doTest();
+        .addOutputLines("out/A.java", "final class A {}")
+        .doTest(TestMode.TEXT_MATCH);
   }
 }
