404f9194eb718cf193122dc4b01f4e60d33bdc50
Halil İbrahim Şener <hisener@yahoo.com>
2020-06-01 10:51:40 +0200

    PSM-442 Add `AmbiguousJsonCreatorCheck`

---
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/AmbiguousJsonCreatorCheck.java
@@ -0,0 +1,73 @@
+package tech.picnic.errorprone.bugpatterns;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.LinkType;
+import com.google.errorprone.BugPattern.ProvidesFix;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.BugPattern.StandardTags;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.AnnotationTreeMatcher;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.AnnotationType;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Symbol;
+import java.util.Map;
+import java.util.Optional;
+import javax.lang.model.element.AnnotationValue;
+
+/** A {@link BugChecker} which flags ambiguous {@code @JsonCreator}s in enums. */
+@AutoService(BugChecker.class)
+@BugPattern(
+    name = "AmbiguousJsonCreator",
+    summary = "JsonCreator.Mode should be set for single-argument creators",
+    linkType = LinkType.NONE,
+    severity = SeverityLevel.SUGGESTION,
+    tags = StandardTags.LIKELY_ERROR,
+    providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
+public final class AmbiguousJsonCreatorCheck extends BugChecker implements AnnotationTreeMatcher {
+  private static final long serialVersionUID = 1L;
+  private static final Matcher<AnnotationTree> JSON_CREATOR_ANNOTATION =
+      new AnnotationType("com.fasterxml.jackson.annotation.JsonCreator");
+
+  @Override
+  public Description matchAnnotation(AnnotationTree tree, VisitorState state) {
+    if (!JSON_CREATOR_ANNOTATION.matches(tree, state)) {
+      return Description.NO_MATCH;
+    }
+
+    ClassTree clazz = state.findEnclosing(ClassTree.class);
+    if (clazz == null || clazz.getKind() != Tree.Kind.ENUM) {
+      return Description.NO_MATCH;
+    }
+
+    MethodTree method = state.findEnclosing(MethodTree.class);
+    if (method == null || method.getParameters().size() != 1) {
+      return Description.NO_MATCH;
+    }
+
+    Optional<Symbol.VarSymbol> mode =
+        ASTHelpers.getAnnotationMirror(tree).getElementValues().entrySet().stream()
+            .filter(entry -> entry.getKey().getSimpleName().contentEquals("mode"))
+            .map(Map.Entry::getValue)
+            .map(AnnotationValue::getValue)
+            .filter(Symbol.VarSymbol.class::isInstance)
+            .map(o -> (Symbol.VarSymbol) o)
+            .filter(varSymbol -> !varSymbol.getSimpleName().contentEquals("DEFAULT"))
+            .findFirst();
+
+    if (mode.isPresent()) {
+      return Description.NO_MATCH;
+    }
+
+    return describeMatch(
+        tree, SuggestedFix.replace(tree, "@JsonCreator(mode = JsonCreator.Mode.DELEGATING)"));
+  }
+}
