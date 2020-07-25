7c3e798bbcd775d2af85ebfa4d15310c4cd62af0
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2020-07-25 22:07:27 +0200

    DIST-502 Suggestions

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/RequestParameterAnnotationCheck.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/RequestParameterAnnotationCheck.java
@@ -1,9 +1,16 @@
 package tech.picnic.errorprone.bugpatterns;
 
-import static java.util.function.Predicate.not;
+import static com.google.errorprone.matchers.ChildMultiMatcher.MatchType.ALL;
+import static com.google.errorprone.matchers.ChildMultiMatcher.MatchType.AT_LEAST_ONE;
+import static com.google.errorprone.matchers.Matchers.annotations;
+import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.Matchers.isSameType;
+import static com.google.errorprone.matchers.Matchers.isSubtypeOf;
+import static com.google.errorprone.matchers.Matchers.isType;
+import static com.google.errorprone.matchers.Matchers.methodHasParameters;
+import static com.google.errorprone.matchers.Matchers.not;
 
 import com.google.auto.service.AutoService;
-import com.google.common.collect.ImmutableList;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.LinkType;
 import com.google.errorprone.BugPattern.ProvidesFix;
@@ -11,83 +18,79 @@ import com.google.errorprone.BugPattern.SeverityLevel;
 import com.google.errorprone.BugPattern.StandardTags;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
+import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
-import com.google.errorprone.util.ASTHelpers;
-import com.sun.source.tree.AnnotationTree;
+import com.google.errorprone.matchers.Matcher;
 import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.VariableTree;
-import com.sun.tools.javac.code.Type;
-import java.util.Optional;
+import com.sun.source.tree.Tree;
 
+// XXX: TODOs for Stephan (besides the other XXXes):
+// 1. Tweak documentation and class name.
+// 2. Review tests.
 /**
  * A {@link BugChecker} which flags Spring HTTP request parameter or body annotations as missing if
  * Spring HTTP request mapping annotations are present.
  *
- * <p>Matched mappings are {@code @{Get,Put,Post,Delete,Patch}Mapping}. {@code @RequestMapping}
- * mappings are already covered by {@link SpringMvcAnnotationCheck}.
+ * <p>Matched mappings are {@code @{Delete,Get,Patch,Post,Put,Request}Mapping}.
  */
 @AutoService(BugChecker.class)
 @BugPattern(
-    name = "RequestParameterAnnotationCheck",
+    name = "RequestParameterAnnotation",
     summary = "Flag missing parameter annotations for Spring HTTP request mappings.",
     linkType = LinkType.NONE,
     severity = SeverityLevel.WARNING,
     tags = StandardTags.LIKELY_ERROR,
     providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
-public final class RequestParameterAnnotationCheck extends BugChecker
-    implements BugChecker.MethodTreeMatcher {
+public final class RequestParameterAnnotationCheck extends BugChecker implements MethodTreeMatcher {
   private static final long serialVersionUID = 1L;
   private static final String ANN_PACKAGE_PREFIX = "org.springframework.web.bind.annotation.";
-  private static final ImmutableList<String> MAPPING_ANNOTATIONS =
-      ImmutableList.of(
-          ANN_PACKAGE_PREFIX + "DeleteMapping",
-          ANN_PACKAGE_PREFIX + "GetMapping",
-          ANN_PACKAGE_PREFIX + "PatchMapping",
-          ANN_PACKAGE_PREFIX + "PostMapping",
-          ANN_PACKAGE_PREFIX + "PutMapping");
-  private static final ImmutableList<String> PARAMETER_ANNOTATIONS =
-      ImmutableList.of(
-          ANN_PACKAGE_PREFIX + "PathVariable",
-          ANN_PACKAGE_PREFIX + "RequestBody",
-          ANN_PACKAGE_PREFIX + "RequestParam");
+  private static final Matcher<Tree> HAS_MAPPING_ANNOTATION =
+      annotations(
+          AT_LEAST_ONE,
+          anyOf(
+              isType(ANN_PACKAGE_PREFIX + "DeleteMapping"),
+              isType(ANN_PACKAGE_PREFIX + "GetMapping"),
+              isType(ANN_PACKAGE_PREFIX + "PatchMapping"),
+              isType(ANN_PACKAGE_PREFIX + "PostMapping"),
+              isType(ANN_PACKAGE_PREFIX + "PutMapping"),
+              isType(ANN_PACKAGE_PREFIX + "RequestMapping")));
+  // XXX: Add other parameters as necessary. See
+  // https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-arguments.
+  private static final Matcher<MethodTree> LACKS_PARAMETER_ANNOTATION =
+      not(
+          methodHasParameters(
+              ALL,
+              anyOf(
+                  annotations(
+                      AT_LEAST_ONE,
+                      anyOf(
+                          isType(ANN_PACKAGE_PREFIX + "PathVariable"),
+                          isType(ANN_PACKAGE_PREFIX + "RequestBody"),
+                          isType(ANN_PACKAGE_PREFIX + "RequestHeader"),
+                          isType(ANN_PACKAGE_PREFIX + "RequestParam"),
+                          isType("tech.picnic.webapp.JsonParam"))),
+                  isSameType("java.io.InputStream"),
+                  isSameType("javax.servlet.http.HttpServletRequest"),
+                  isSameType("javax.servlet.http.HttpServletResponse"),
+                  isSameType("org.springframework.http.HttpMethod"),
+                  isSubtypeOf("org.springframework.web.context.request.WebRequest"))));
 
   @Override
   public Description matchMethod(MethodTree tree, VisitorState state) {
-    return Optional.of(tree)
-        .filter(RequestParameterAnnotationCheck::matchesAnyMappingAnnotation)
-        .filter(not(RequestParameterAnnotationCheck::matchesAllParameters))
-        .map(this::describeMatch)
-        .orElse(Description.NO_MATCH);
-  }
-
-  private static boolean matchesAnyMappingAnnotation(MethodTree tree) {
-    return tree.getModifiers().getAnnotations().stream()
-        .anyMatch(RequestParameterAnnotationCheck::matchesMappingAnnotation);
-  }
-
-  private static boolean matchesAllParameters(MethodTree tree) {
-    return tree.getParameters().stream()
-        .allMatch(RequestParameterAnnotationCheck::matchAnyParameterAnnotation);
-  }
-
-  private static boolean matchAnyParameterAnnotation(VariableTree t) {
-    return t.getModifiers().getAnnotations().stream()
-        .anyMatch(RequestParameterAnnotationCheck::matchesParameterAnnotation);
-  }
-
-  private static boolean matchesMappingAnnotation(AnnotationTree tree) {
-    return matchGivenAnnotations(tree, MAPPING_ANNOTATIONS);
-  }
-
-  private static boolean matchesParameterAnnotation(AnnotationTree tree) {
-    return matchGivenAnnotations(tree, PARAMETER_ANNOTATIONS);
-  }
-
-  private static boolean matchGivenAnnotations(
-      AnnotationTree tree, ImmutableList<String> mappingAnnotations) {
-    return Optional.ofNullable(ASTHelpers.getType(tree.getAnnotationType()))
-        .map(Type::toString)
-        .filter(mappingAnnotations::contains)
-        .isPresent();
+    // XXX: Auto-add `@RequestParam` where applicable.
+    // XXX: What about the `PurchasingProposerRequestParams` in POM? Implies `@RequestBody`?
+    // (Documentation doesn't mention this, IIUC.)
+    return HAS_MAPPING_ANNOTATION.matches(tree, state)
+            && LACKS_PARAMETER_ANNOTATION.matches(tree, state)
+        ? buildDescription(tree)
+            // XXX: Drop this `addFix`. (I added this to quickly find all violations in downstream
+            // code using `./patch.sh RequestParameterAnnotation`.)
+            .addFix(SuggestedFix.prefixWith(tree, "/* HIT */"))
+            .setMessage(
+                "Not all parameters of this request mapping method are annotated; this may be a mistake. "
+                    + "If the unannotated parameters represent query string parameters, annotate them with `@RequestParam`.")
+            .build()
+        : Description.NO_MATCH;
   }
 }
