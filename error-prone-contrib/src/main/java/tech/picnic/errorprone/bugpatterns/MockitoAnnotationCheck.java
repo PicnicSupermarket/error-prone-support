54f93ffa8c29c64be974e8ab29ded488edbbfd7a
Arturs Drozdovs <arturs.drozdovs@teampicnic.com>
2020-11-21 13:24:57 +0100

    Cleanup / add proper fix suggestions

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoAnnotationCheck.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoAnnotationCheck.java
@@ -23,73 +23,65 @@ import com.google.errorprone.matchers.Matcher;
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.ImportTree;
-import com.sun.source.tree.Tree.Kind;
-import java.util.List;
 
 /** A {@link BugChecker} which flags classes importing Mockito, but not enforcing strict mocks. */
 @AutoService(BugChecker.class)
 @BugPattern(
-        name = "MockitoAnnotation",
-        summary = "Prefer using strict stubs with Mockito",
-        linkType = LinkType.NONE,
-        severity = SeverityLevel.SUGGESTION,
-        tags = StandardTags.STYLE,
-        providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
+    name = "MockitoAnnotation",
+    summary = "Prefer using strict stubs with Mockito",
+    linkType = LinkType.NONE,
+    severity = SeverityLevel.SUGGESTION,
+    tags = StandardTags.STYLE,
+    providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
 public final class MockitoAnnotationCheck extends BugChecker implements CompilationUnitTreeMatcher {
-    private static final long serialVersionUID = 1L;
-    private static final Matcher<AnnotationTree> MOCKITO_ANNOTATION_MATCHER =
-            allOf(
-                    isType("org.mockito.junit.jupiter.MockitoSettings"), hasArgumentWithValue(
-                            "strictness",
-                            isSameType("static org.mockito.quality.Strictness.STRICT_STUBS")));
+  private static final long serialVersionUID = 1L;
+  private static final String MOCKITO_SETTINGS = "org.mockito.junit.jupiter.MockitoSettings";
+  private static final String STRICT_STUBS = "org.mockito.quality.Strictness.STRICT_STUBS";
+  private static final String MOCKITO_ANNOTATION = "@MockitoSettings(strictness = STRICT_STUBS)";
+  private static final Matcher<AnnotationTree> MOCKITO_ANNOTATION_MATCHER =
+      allOf(isType(MOCKITO_SETTINGS), hasArgumentWithValue("strictness", isSameType(STRICT_STUBS)));
 
-    @Override
-    public Description matchCompilationUnit(
-            CompilationUnitTree compilationUnitTree, VisitorState state) {
-        List<? extends ImportTree> imports = compilationUnitTree.getImports();
-
-        if (imports.isEmpty()) {
-            return NO_MATCH;
-        }
-
-        boolean importsMockito =
-                imports.stream()
-                        // Pattern matching using "packageStartsWith" doesn't work, because it only
-                        // looks at the visitor and not the tree
-                        .map(Object::toString)
-                        .anyMatch(importLine -> importLine.startsWith("org.mockito"));
-        if (!importsMockito) {
-            return NO_MATCH;
-        }
-
-        ImmutableList<ClassTree> classDeclarations =
-                compilationUnitTree.getTypeDecls().stream()
-                        .filter(c -> c.getKind() == Kind.CLASS)
-                        .map(ClassTree.class::cast)
-                        .collect(toImmutableList());
-
-        ImmutableList<ClassTree> violatingClasses =
-                classDeclarations.stream()
-                        .filter(clazz -> !hasMockitoAnnotation(clazz, state))
-                        .collect(toImmutableList());
-
-        if (violatingClasses.isEmpty()) {
-            return NO_MATCH;
-        }
-
-        SuggestedFix.Builder fixBuilder = SuggestedFix.builder();
-        for (ClassTree violatingClass : violatingClasses) {
-            // And here I would like to say
-            // "Please add @MockitoSettings(strictness = STRICT_STUBS)",
-            // but don't know how =(
-            fixBuilder.delete(violatingClass);
-        }
-        return describeMatch(violatingClasses.iterator().next(), fixBuilder.build());
+  @Override
+  public Description matchCompilationUnit(
+      CompilationUnitTree compilationUnitTree, VisitorState state) {
+    // Pattern matching using "packageStartsWith" doesn't work, because it only
+    // looks at the visitor and not the tree
+    boolean importsMockito =
+        compilationUnitTree.getImports().stream()
+            .map(Object::toString)
+            .anyMatch(importLine -> importLine.startsWith("org.mockito"));
+    if (!importsMockito) {
+      return NO_MATCH;
     }
 
-    private boolean hasMockitoAnnotation(ClassTree classTree, VisitorState state) {
-        return classTree.getModifiers().getAnnotations().stream()
-                .anyMatch(tree -> MOCKITO_ANNOTATION_MATCHER.matches(tree, state));
-    }
+    ImmutableList<ClassTree> classDeclarations =
+        compilationUnitTree.getTypeDecls().stream()
+            .filter(ClassTree.class::isInstance)
+            .map(ClassTree.class::cast)
+            .collect(toImmutableList());
+
+    ImmutableList<ClassTree> violatingClasses =
+        classDeclarations.stream()
+            .filter(clazz -> !hasMockitoAnnotation(clazz, state))
+            .collect(toImmutableList());
+
+    return violatingClasses.stream()
+        .findFirst()
+        .map(MockitoAnnotationCheck::buildFix)
+        .map(builder -> describeMatch(compilationUnitTree, builder))
+        .orElse(NO_MATCH);
+  }
+
+  private static SuggestedFix buildFix(ClassTree violatingClass) {
+    return SuggestedFix.builder()
+        .addImport(MOCKITO_SETTINGS)
+        .addStaticImport(STRICT_STUBS)
+        .prefixWith(violatingClass, MOCKITO_ANNOTATION)
+        .build();
+  }
+
+  private boolean hasMockitoAnnotation(ClassTree classTree, VisitorState state) {
+    return classTree.getModifiers().getAnnotations().stream()
+        .anyMatch(tree -> MOCKITO_ANNOTATION_MATCHER.matches(tree, state));
+  }
 }
--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/MockitoAnnotationCheckTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/MockitoAnnotationCheckTest.java
@@ -6,68 +6,68 @@ import com.google.errorprone.CompilationTestHelper;
 import org.junit.jupiter.api.Test;
 
 public final class MockitoAnnotationCheckTest {
-    private final CompilationTestHelper compilationTestHelper =
-            CompilationTestHelper.newInstance(MockitoAnnotationCheck.class, getClass());
-    private final BugCheckerRefactoringTestHelper refactoringTestHelper =
-            BugCheckerRefactoringTestHelper.newInstance(new MockitoAnnotationCheck(), getClass());
+  private final CompilationTestHelper compilationTestHelper =
+      CompilationTestHelper.newInstance(MockitoAnnotationCheck.class, getClass());
+  private final BugCheckerRefactoringTestHelper refactoringTestHelper =
+      BugCheckerRefactoringTestHelper.newInstance(new MockitoAnnotationCheck(), getClass());
 
-    @Test
-    public void testIdentification() {
-        compilationTestHelper
-                .addSourceLines(
-                        "A.java",
-                        "import static org.mockito.Mockito.mock;",
-                        "",
-                        "import org.junit.jupiter.api.Tag;",
-                        "import org.junit.jupiter.api.Test;",
-                        "",
-                        "@Tag(\"unit\")",
-                        "class MockitoTest {",
-                        "",
-                        "    @Test",
-                        "    void mockitoTest() {",
-                        "        mock(String.class);",
-                        "    }",
-                        "}")
-                .doTest();
-    }
+  @Test
+  public void testIdentification() {
+    compilationTestHelper
+        .addSourceLines(
+            "A.java",
+            "import static org.mockito.Mockito.mock;",
+            "",
+            "import org.junit.jupiter.api.Tag;",
+            "import org.junit.jupiter.api.Test;",
+            "",
+            "@Tag(\"unit\")",
+            "class MockitoTest {",
+            "",
+            "    @Test",
+            "    void mockitoTest() {",
+            "        mock(String.class);",
+            "    }",
+            "}")
+        .doTest();
+  }
 
-    @Test
-    public void testReplacement() {
-        refactoringTestHelper
-                .addInputLines(
-                        "in/A.java",
-                        "import static org.mockito.Mockito.mock;",
-                        "",
-                        "import org.junit.jupiter.api.Tag;",
-                        "import org.junit.jupiter.api.Test;",
-                        "",
-                        "@Tag(\"unit\")",
-                        "class MockitoTest {",
-                        "",
-                        "    @Test",
-                        "    void mockitoTest() {",
-                        "        mock(String.class);",
-                        "    }",
-                        "}")
-                .addOutputLines(
-                        "out/A.java",
-                        "import static org.mockito.Mockito.mock;",
-                        "import static org.mockito.quality.Strictness.STRICT_STUBS;",
-                        "",
-                        "import org.junit.jupiter.api.Tag;",
-                        "import org.junit.jupiter.api.Test;",
-                        "import org.mockito.junit.jupiter.MockitoSettings;",
-                        "",
-                        "@Tag(\"unit\")",
-                        "@MockitoSettings(strictness = STRICT_STUBS)",
-                        "class MockitoTest {",
-                        "",
-                        "    @Test",
-                        "    void mockitoTest() {",
-                        "        mock(String.class);",
-                        "    }",
-                        "}")
-                .doTest(TestMode.TEXT_MATCH);
-    }
+  @Test
+  public void testReplacement() {
+    refactoringTestHelper
+        .addInputLines(
+            "in/A.java",
+            "import static org.mockito.Mockito.mock;",
+            "",
+            "import org.junit.jupiter.api.Tag;",
+            "import org.junit.jupiter.api.Test;",
+            "",
+            "@Tag(\"unit\")",
+            "class MockitoTest {",
+            "",
+            "    @Test",
+            "    void mockitoTest() {",
+            "        mock(String.class);",
+            "    }",
+            "}")
+        .addOutputLines(
+            "out/A.java",
+            "import static org.mockito.Mockito.mock;",
+            "import static org.mockito.quality.Strictness.STRICT_STUBS;",
+            "",
+            "import org.junit.jupiter.api.Tag;",
+            "import org.junit.jupiter.api.Test;",
+            "import org.mockito.junit.jupiter.MockitoSettings;",
+            "",
+            "@Tag(\"unit\")",
+            "@MockitoSettings(strictness = STRICT_STUBS)",
+            "class MockitoTest {",
+            "",
+            "    @Test",
+            "    void mockitoTest() {",
+            "        mock(String.class);",
+            "    }",
+            "}")
+        .doTest(TestMode.TEXT_MATCH);
+  }
 }
