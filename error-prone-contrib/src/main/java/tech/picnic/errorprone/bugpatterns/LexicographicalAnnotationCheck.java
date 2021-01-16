b68d8b0a06ee722acbe93d29fd1821000609f234
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-01-16 19:54:20 +0100

    First push for lexicographical annotation check

---
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/LexicographicalAnnotationCheck.java
@@ -0,0 +1,115 @@
+package tech.picnic.errorprone.bugpatterns;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.LinkType;
+import com.google.errorprone.BugPattern.ProvidesFix;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.BugPattern.StandardTags;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.MultiMatcher;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Attribute;
+import com.sun.tools.javac.util.List;
+
+import static com.google.errorprone.matchers.ChildMultiMatcher.MatchType.AT_LEAST_ONE;
+import static com.google.errorprone.matchers.Matchers.annotations;
+
+/**
+ * A {@link BugChecker} which flags annotations which aren't sorted lexicographically.
+ *
+ * <p>The idea behind this checker is that maintaining a sorted sequence simplifies conflict
+ * resolution, and can even avoid it if two branches add the same entry.
+ */
+@AutoService(BugChecker.class)
+@BugPattern(
+    name = "LexicographicalAnnotation",
+    summary = "Where possible, sort annotations lexicographically",
+    linkType = LinkType.NONE,
+    severity = SeverityLevel.SUGGESTION,
+    tags = StandardTags.STYLE,
+    providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
+public final class LexicographicalAnnotationCheck extends BugChecker implements MethodTreeMatcher {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public Description matchMethod(MethodTree tree, VisitorState state) {
+    List<Attribute.Compound> rawAttributes = ASTHelpers.getSymbol(tree).getRawAttributes();
+    if (rawAttributes.length() < 2) {
+      return Description.NO_MATCH;
+    }
+    Attribute.Compound value = rawAttributes.get(0).getValue();
+
+    return Description.NO_MATCH;
+  }
+
+  //  private Optional<Fix> sortArrayElements(AnnotationTree tree, VisitorState state) {
+  //    /*
+  //     * We loop over the array's attributes, trying to sort each array associated with a
+  //     * non-blacklisted attribute. A single compound fix, if any, is returned.
+  //     */
+  //    return matcher
+  //        .extractMatchingArguments(tree)
+  //        .map(expr -> extractArray(expr).flatMap(arr -> suggestSorting(arr, state)))
+  //        .flatMap(Optional::stream)
+  //        .reduce(SuggestedFix.Builder::merge)
+  //        .map(SuggestedFix.Builder::build);
+  //  }
+
+//    private static Optional<SuggestedFix.Builder> suggestSorting(
+//        NewArrayTree array, VisitorState state) {
+  //    if (array.getInitializers().size() < 2 || !canSort(array, state)) {
+  //      /* There's nothing to sort, or we don't want to sort. */
+  //      return Optional.empty();
+  //    }
+  //
+  //    List<? extends ExpressionTree> actualOrdering = array.getInitializers();
+  //    ImmutableList<? extends ExpressionTree> desiredOrdering = doSort(actualOrdering, state);
+  //    if (actualOrdering.equals(desiredOrdering)) {
+  //      /* In the (presumably) common case the elements are already sorted. */
+  //      return Optional.empty();
+  //    }
+  //
+  //    /* The elements aren't sorted. Suggest the sorted alternative. */
+  //    String suggestion =
+  //        desiredOrdering.stream()
+  //            .map(expr -> Util.treeToString(expr, state))
+  //            .collect(joining(", ", "{", "}"));
+  //    return Optional.of(SuggestedFix.builder().replace(array, suggestion));
+  //  }
+
+  //  private static boolean canSort(Tree array, VisitorState state) {
+  //    Type type = ASTHelpers.getType(array);
+  //    if (type == null) {
+  //      return false;
+  //    }
+  //
+  //    Symtab symtab = state.getSymtab();
+  //    Type elemType = state.getTypes().elemtype(type);
+  //
+  //    /* For now we don't force sorting on numeric types. */
+  //    return Stream.of(
+  //            symtab.annotationType, symtab.classType, symtab.enumSym.type, symtab.stringType)
+  //        .anyMatch(t -> ASTHelpers.isSubtype(elemType, t, state));
+  //  }
+
+//    private static ImmutableList<? extends ExpressionTree> doSort(
+//            Iterable<? extends ExpressionTree> elements, VisitorState state) {
+//      // XXX: Perhaps we should use `Collator` with `.setStrength(Collator.PRIMARY)` and
+//      // `getCollationKey`. Not clear whether that's worth the hassle at this point.
+//      return ImmutableList.sortedCopyOf(
+//          Comparator.comparing(
+//              e -> getStructure(e, state),
+//              Comparators.lexicographical(
+//                  Comparators.lexicographical(
+//                      String.CASE_INSENSITIVE_ORDER.thenComparing(naturalOrder())))),
+//          elements);
+//
+//      }
+}
---
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/LexicographicalAnnotationCheckTest.java
@@ -0,0 +1,153 @@
+package tech.picnic.errorprone.bugpatterns;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.jupiter.api.Test;
+
+public final class LexicographicalAnnotationCheckTest {
+  private final CompilationTestHelper compilationTestHelper =
+      CompilationTestHelper.newInstance(
+          LexicographicalAnnotationCheck.class, getClass());
+//  private final BugCheckerRefactoringTestHelper refactoringTestHelper =
+//      BugCheckerRefactoringTestHelper.newInstance(
+//          new LexicographicalAnnotationCheck(), getClass());
+
+  @Test
+  public void testIdentification() {
+    compilationTestHelper
+        .addSourceLines(
+            "A.java",
+            "import static java.math.RoundingMode.UP;",
+            "import static java.math.RoundingMode.DOWN;",
+            "",
+            "import com.fasterxml.jackson.annotation.JsonPropertyOrder;",
+            "import io.swagger.annotations.ApiImplicitParam;",
+            "import io.swagger.annotations.ApiImplicitParams;",
+            "import io.swagger.v3.oas.annotations.Parameter;",
+            "import io.swagger.v3.oas.annotations.Parameters;",
+            "import java.math.RoundingMode;",
+            "import javax.xml.bind.annotation.XmlType;",
+            "",
+            "interface A {",
+            "  @interface Foo {",
+            "    String[] value() default {};",
+            "    int[] ints() default {};",
+            "    Class<?>[] cls() default {};",
+            "    RoundingMode[] enums() default {};",
+            "    Bar[] anns() default {};",
+            "  }",
+//            "",
+            "  @interface Bar {",
+            "    String[] value() default {};",
+            "  }",
+            "",
+            " // BUG: Diagnostic contains:",
+            "  @Foo() @Bar() A noString();",
+//            "  @Foo({\"a\"}) A oneString();",
+//            "  @Foo({\"a\", \"b\"}) A sortedStrings();",
+//            "  // BUG: Diagnostic contains:",
+//            "  @Foo({\"b\", \"a\"}) A unsortedString();",
+//            "  @Foo({\"ab\", \"Ac\"}) A sortedStringCaseInsensitive();",
+//            "  // BUG: Diagnostic contains:",
+//            "  @Foo({\"ac\", \"Ab\"}) A unsortedStringCaseInsensitive();",
+//            "  @Foo({\"A\", \"a\"}) A sortedStringCaseInsensitiveWithTotalOrderFallback();",
+//            "  // BUG: Diagnostic contains:",
+//            "  @Foo({\"a\", \"A\"}) A unsortedStringCaseInsensitiveWithTotalOrderFallback();",
+//            "",
+//            "  @Foo(ints = {}) A noInts();",
+//            "  @Foo(ints = {0}) A oneInt();",
+//            "  @Foo(ints = {0, 1}) A sortedInts();",
+//            "  @Foo(ints = {1, 0}) A unsortedInts();",
+//            "",
+//            "  @Foo(cls = {}) A noClasses();",
+//            "  @Foo(cls = {int.class}) A oneClass();",
+//            "  @Foo(cls = {int.class, long.class}) A sortedClasses();",
+//            "  // BUG: Diagnostic contains:",
+//            "  @Foo(cls = {long.class, int.class}) A unsortedClasses();",
+//            "",
+//            "  @Foo(enums = {}) A noEnums();",
+//            "  @Foo(enums = {DOWN}) A oneEnum();",
+//            "  @Foo(enums = {DOWN, UP}) A sortedEnums();",
+//            "  // BUG: Diagnostic contains:",
+//            "  @Foo(enums = {UP, DOWN}) A unsortedEnums();",
+//            "",
+//            "  @Foo(anns = {}) A noAnns();",
+//            "  @Foo(anns = {@Bar(\"a\")}) A oneAnn();",
+//            "  @Foo(anns = {@Bar(\"a\"), @Bar(\"b\")}) A sortedAnns();",
+//            "  // BUG: Diagnostic contains:",
+//            "  @Foo(anns = {@Bar(\"b\"), @Bar(\"a\")}) A unsortedAnns();",
+//            "  // BUG: Diagnostic contains:",
+//            "  @Foo(anns = {@Bar(\"a\"), @Bar({\"b\", \"a\"})}) A unsortedInnderAnns();",
+//            "",
+//            "  @Foo({\"a=foo\", \"a.b=bar\", \"a.c=baz\"}) A hierarchicallySorted();",
+//            "  // BUG: Diagnostic contains:",
+//            "  @Foo({\"a.b=bar\", \"a.c=baz\", \"a=foo\"}) A hierarchicallyUnsorted();",
+//            "",
+//            "  @JsonPropertyOrder({\"field2\", \"field1\"}) A dto();",
+//            "  @ApiImplicitParams({@ApiImplicitParam(\"p2\"), @ApiImplicitParam(\"p1\")}) A firstEndpoint();",
+//            "  @Parameters({@Parameter(name = \"p2\"), @Parameter(name = \"p1\")}) A secondEndpoint();",
+//            "",
+//            "  @XmlType(propOrder = {\"field2\", \"field1\"})",
+//            "  class Dummy {}",
+            "}")
+        .doTest();
+  }
+
+  // XXX: Note that in the output below in one instance redundant `value =` assignments are
+  // introduced. Avoiding that might make the code too complex. Instead, users can have the
+  // `CanonicalAnnotationSyntaxCheck` correct the situation in a subsequent run.
+//  @Test
+//  public void testReplacement() {
+//    refactoringTestHelper
+//        .addInputLines(
+//            "in/A.java",
+//            "import static java.math.RoundingMode.UP;",
+//            "import static java.math.RoundingMode.DOWN;",
+//            "",
+//            "import java.math.RoundingMode;",
+//            "",
+//            "interface A {",
+//            "  @interface Foo {",
+//            "    String[] value() default {};",
+//            "    Class<?>[] cls() default {};",
+//            "    RoundingMode[] enums() default {};",
+//            "    Bar[] anns() default {};",
+//            "  }",
+//            "",
+//            "  @interface Bar {",
+//            "    String[] value() default {};",
+//            "  }",
+//            "",
+//            "  @Foo({\"b\", \"a\"}) A unsortedString();",
+//            "  @Foo(cls = {long.class, int.class}) A unsortedClasses();",
+//            "  @Foo(enums = {UP, DOWN}) A unsortedEnums();",
+//            "  @Foo(anns = {@Bar(\"b\"), @Bar(\"a\")}) A unsortedAnns();",
+//            "  @Foo(anns = {@Bar(\"a\"), @Bar({\"b\", \"a\"})}) A unsortedInnderAnns();",
+//            "}")
+//        .addOutputLines(
+//            "out/A.java",
+//            "import static java.math.RoundingMode.UP;",
+//            "import static java.math.RoundingMode.DOWN;",
+//            "",
+//            "import java.math.RoundingMode;",
+//            "",
+//            "interface A {",
+//            "  @interface Foo {",
+//            "    String[] value() default {};",
+//            "    Class<?>[] cls() default {};",
+//            "    RoundingMode[] enums() default {};",
+//            "    Bar[] anns() default {};",
+//            "  }",
+//            "",
+//            "  @interface Bar {",
+//            "    String[] value() default {};",
+//            "  }",
+//            "",
+//            "  @Foo({\"a\", \"b\"}) A unsortedString();",
+//            "  @Foo(cls = {int.class, long.class}) A unsortedClasses();",
+//            "  @Foo(enums = {DOWN, UP}) A unsortedEnums();",
+//            "  @Foo(anns = {@Bar(\"a\"), @Bar(\"b\")}) A unsortedAnns();",
+//            "  @Foo(anns = {@Bar(\"a\"), @Bar({\"a\", \"b\"})}) A unsortedInnderAnns();",
+//            "}")
+//        .doTest(TestMode.TEXT_MATCH);
+//  }
+}
