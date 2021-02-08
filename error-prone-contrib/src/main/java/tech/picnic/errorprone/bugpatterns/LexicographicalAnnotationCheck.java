94f85ad6d6e01e758d00197ceeea3f4495fb035a
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-02-08 11:07:44 +0100

    Try to find out how to provide a fix for the annotations

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/LexicographicalAnnotationCheck.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/LexicographicalAnnotationCheck.java
@@ -12,6 +12,7 @@ import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
 import com.google.errorprone.fixes.Fix;
+import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.MultiMatcher;
 import com.google.errorprone.util.ASTHelpers;
@@ -23,12 +24,17 @@ import com.sun.tools.javac.util.List;
 
 import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static com.google.errorprone.matchers.ChildMultiMatcher.MatchType.ALL;
 import static com.google.errorprone.matchers.ChildMultiMatcher.MatchType.AT_LEAST_ONE;
+import static com.google.errorprone.matchers.InjectMatchers.IS_APPLICATION_OF_JAVAX_INJECT;
 import static com.google.errorprone.matchers.Matchers.annotations;
+import static com.google.errorprone.matchers.Matchers.anything;
 
 /**
  * A {@link BugChecker} which flags annotations which aren't sorted lexicographically.
@@ -47,89 +53,57 @@ import static com.google.errorprone.matchers.Matchers.annotations;
 public final class LexicographicalAnnotationCheck extends BugChecker implements MethodTreeMatcher {
   private static final long serialVersionUID = 1L;
 //  https://github.com/google/error-prone/pull/2125/files
+
+  private static final MultiMatcher<MethodTree, AnnotationTree> FINDER =
+          annotations(ALL, anything());
+
   @Override
   public Description matchMethod(MethodTree tree, VisitorState state) {
     List<Attribute.Compound> rawAttributes = ASTHelpers.getSymbol(tree).getRawAttributes();
+
+//    Stream<Attribute.Compound> declarationAndTypeAttributes = ASTHelpers.getDeclarationAndTypeAttributes(ASTHelpers.getSymbol(tree));
+//    java.util.List<Attribute.Compound> collect1 = declarationAndTypeAttributes.collect(Collectors.toList());
     if (rawAttributes.length() < 2) {
       return Description.NO_MATCH;
     }
+
+//    MultiMatcher.MultiMatchResult<AnnotationTree> annotationTreeMultiMatchResult = FINDER.multiMatchResult(tree, state);
+//    if (annotationTreeMultiMatchResult.matches())
 
     ImmutableList<Attribute.Compound> collect = rawAttributes.stream()
             .sorted(Comparator.comparing(e -> e.type.tsym.getQualifiedName().toString()))
             .collect(toImmutableList());
 
-    boolean doAnnotationsMatch = IntStream.range(0, rawAttributes.size())
+    boolean annotationsMatch = IntStream.range(0, rawAttributes.size())
             .allMatch(i -> collect.get(i).equals(rawAttributes.get(i)));
 
-    Attribute.Compound value = rawAttributes.get(0).getValue();
+    if (annotationsMatch) {
+      return Description.NO_MATCH;
+    }
 
+    String treeToString = Util.treeToString(tree, state);
 
+     Attribute.Compound value = rawAttributes.get(0).getValue();
 
+    String suggestion = collect.stream()
+            .map(annotation -> "@" + annotation.type.tsym.name.toString() + "()")
+            .collect(Collectors.joining("\r\n"));
+//    String suggestion =
+//            collect.stream()
+//                        .map(comp -> Util.treeToString(comp, state))
+//            //            .collect(joining(", ", "{", "}"));
+//            //    return Optional.of(SuggestedFix.builder().replace(array, suggestion));
+
+//    SuggestedFix.builder().replace()
 
     return Description.NO_MATCH;
+
   }
 
-//    private Optional<Fix> sortArrayElements(AnnotationTree tree, VisitorState state) {
-  //    /*
-  //     * We loop over the array's attributes, trying to sort each array associated with a
-  //     * non-blacklisted attribute. A single compound fix, if any, is returned.
-  //     */
-  //    return matcher
-  //        .extractMatchingArguments(tree)
-  //        .map(expr -> extractArray(expr).flatMap(arr -> suggestSorting(arr, state)))
-  //        .flatMap(Optional::stream)
-  //        .reduce(SuggestedFix.Builder::merge)
-  //        .map(SuggestedFix.Builder::build);
-  //  }
 
-//    private static Optional<SuggestedFix.Builder> suggestSorting(
-//        NewArrayTree array, VisitorState state) {
-  //    if (array.getInitializers().size() < 2 || !canSort(array, state)) {
-  //      /* There's nothing to sort, or we don't want to sort. */
-  //      return Optional.empty();
-  //    }
-  //
-  //    List<? extends ExpressionTree> actualOrdering = array.getInitializers();
-  //    ImmutableList<? extends ExpressionTree> desiredOrdering = doSort(actualOrdering, state);
-  //    if (actualOrdering.equals(desiredOrdering)) {
-  //      /* In the (presumably) common case the elements are already sorted. */
-  //      return Optional.empty();
-  //    }
-  //
-  //    /* The elements aren't sorted. Suggest the sorted alternative. */
-  //    String suggestion =
-  //        desiredOrdering.stream()
-  //            .map(expr -> Util.treeToString(expr, state))
-  //            .collect(joining(", ", "{", "}"));
-  //    return Optional.of(SuggestedFix.builder().replace(array, suggestion));
-  //  }
 
-  //  private static boolean canSort(Tree array, VisitorState state) {
-  //    Type type = ASTHelpers.getType(array);
-  //    if (type == null) {
-  //      return false;
-  //    }
-  //
-  //    Symtab symtab = state.getSymtab();
-  //    Type elemType = state.getTypes().elemtype(type);
-  //
-  //    /* For now we don't force sorting on numeric types. */
-  //    return Stream.of(
-  //            symtab.annotationType, symtab.classType, symtab.enumSym.type, symtab.stringType)
-  //        .anyMatch(t -> ASTHelpers.isSubtype(elemType, t, state));
-  //  }
 
-//    private static ImmutableList<? extends ExpressionTree> doSort(
-//            Iterable<? extends ExpressionTree> elements, VisitorState state) {
-//      // XXX: Perhaps we should use `Collator` with `.setStrength(Collator.PRIMARY)` and
-//      // `getCollationKey`. Not clear whether that's worth the hassle at this point.
-//      return ImmutableList.sortedCopyOf(
-//          Comparator.comparing(
-//              e -> getStructure(e, state),
-//              Comparators.lexicographical(
-//                  Comparators.lexicographical(
-//                      String.CASE_INSENSITIVE_ORDER.thenComparing(naturalOrder())))),
-//          elements);
-//
-//      }
+  //    SuggestedFix.builder().
+//    return describeMatch(collect, SuggestedFix.replace(, ));
+
 }
