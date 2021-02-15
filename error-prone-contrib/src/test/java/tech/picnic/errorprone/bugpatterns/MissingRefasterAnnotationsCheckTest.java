6f9886817193f4d7c851a3f2bd0c18b12ed197c6
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-02-15 11:14:56 +0100

    Add test cases and improve the comments

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/MissingRefasterAnnotationsCheckTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/MissingRefasterAnnotationsCheckTest.java
@@ -10,7 +10,7 @@ public final class MissingRefasterAnnotationsCheckTest {
           .expectErrorMessage(
               "X",
               Predicates.containsPattern(
-                  "The Refaster template contains a method without annotation"));
+                  "The Refaster template contains a method without Refaster annotation"));
 
   @Test
   public void testIdentification() {
@@ -20,6 +20,7 @@ public final class MissingRefasterAnnotationsCheckTest {
             "import com.google.errorprone.refaster.annotation.AfterTemplate;",
             "import com.google.errorprone.refaster.annotation.AlsoNegation;",
             "import com.google.errorprone.refaster.annotation.BeforeTemplate;",
+            "import java.util.Map;",
             "",
             "final class RefasterTemplateStringIsEmpty {",
             "  private RefasterTemplateStringIsEmpty() {}",
@@ -31,7 +32,7 @@ public final class MissingRefasterAnnotationsCheckTest {
             "      return string.equals(\"\");",
             "    }",
             "",
-            "    // Here the @BeforeTemplate is missing",
+            "", // @BeforeTemplate is missing
             "    boolean lengthEquals0(String string) {",
             "      return string.length() == 0;",
             "    }",
@@ -40,6 +41,43 @@ public final class MissingRefasterAnnotationsCheckTest {
             "    @AlsoNegation",
             "    boolean optimizedMethod(String string) {",
             "      return string.isEmpty();",
+            "    }",
+            "  }",
+            "",
+            "  // BUG: Diagnostic matches: X",
+            "  static final class SecondStringIsEmpty {",
+            "    @BeforeTemplate",
+            "    boolean equalsEmptyString(String string) {",
+            "      return string.equals(\"\");",
+            "    }",
+            "",
+            "    @BeforeTemplate",
+            "    boolean lengthEquals0(String string) {",
+            "      return string.length() == 0;",
+            "    }",
+            "",
+            "", // @AfterTemplate is missing
+            "    boolean optimizedMethod(String string) {",
+            "      return string.isEmpty();",
+            "    }",
+            "  }",
+            "",
+            "  // BUG: Diagnostic matches: X",
+            "  abstract class ComputeIfAbsent<K, V> {",
+            "",
+            "", // @Placeholder is missing
+            "    abstract V function(K key);",
+            "",
+            "    @BeforeTemplate",
+            "    void before(Map<K, V> map, K key) {",
+            "      if (!map.containsKey(key)) {",
+            "        map.put(key, function(key));",
+            "      }",
+            "    }",
+            "",
+            "    @AfterTemplate",
+            "    void after(Map<K, V> map, K key) {",
+            "      map.computeIfAbsent(key, (K k) -> function(k));",
             "    }",
             "  }",
             "}")
