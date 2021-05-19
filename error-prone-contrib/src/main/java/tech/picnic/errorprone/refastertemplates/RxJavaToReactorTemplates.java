6b535aa8fcb86c3982a0c0d80aaf90be62b6b4be
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-05-19 10:10:11 +0200

    Add tests for added templates

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refastertemplates/RxJavaToReactorTemplates.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refastertemplates/RxJavaToReactorTemplates.java
@@ -19,6 +19,36 @@ final class RxJavaToReactorTemplates {
   private RxJavaToReactorTemplates() {}
 
   // XXX: `function` type change; look into `Refaster.canBeCoercedTo(...)`.
+  static final class FlowableFilter<S, T extends S> {
+
+    @BeforeTemplate
+    Flowable<T> before(Flowable<T> flowable, Predicate<S> predicate) {
+      return flowable.filter(predicate);
+    }
+
+    @AfterTemplate
+    Flowable<T> after(Flowable<T> flowable, java.util.function.Predicate<S> predicate) {
+      return flowable
+          .as(RxJava2Adapter::flowableToFlux)
+          .filter(predicate)
+          .as(RxJava2Adapter::fluxToFlowable);
+    }
+  }
+
+  static final class FlowableFirstElement<T> {
+
+    @BeforeTemplate
+    Maybe<T> before(Flowable<T> flowable) {
+      return flowable.firstElement();
+    }
+
+    @AfterTemplate
+    Maybe<T> after(Flowable<T> flowable) {
+      return flowable.as(RxJava2Adapter::flowableToFlux).next().as(RxJava2Adapter::monoToMaybe);
+    }
+  }
+
+  // XXX: `function` type change; look into `Refaster.canBeCoercedTo(...)`.
   static final class FlowableFlatMap<I, T extends I, O, P extends Publisher<? extends O>> {
     @BeforeTemplate
     Flowable<O> before(Flowable<T> flowable, Function<I, P> function) {
@@ -35,34 +65,23 @@ final class RxJavaToReactorTemplates {
   }
 
   // XXX: `function` type change; look into `Refaster.canBeCoercedTo(...)`.
-  static final class FlowableFilter<S, T extends S> {
+  static final class FlowableMap<I, T extends I, O> {
     @BeforeTemplate
-    Flowable<T> before(Flowable<T> flowable, Predicate<S> predicate) {
-      return flowable.filter(predicate);
+    Flowable<O> before(Flowable<T> flowable, Function<I, O> function) {
+      return flowable.map(function);
     }
 
     @AfterTemplate
-    Flowable<T> after(Flowable<T> flowable, java.util.function.Predicate<S> predicate) {
+    Flowable<O> after(Flowable<T> flowable, java.util.function.Function<I, O> function) {
       return flowable
           .as(RxJava2Adapter::flowableToFlux)
-          .filter(predicate)
+          .map(function)
           .as(RxJava2Adapter::fluxToFlowable);
     }
   }
 
-  static final class FlowableFirstElement<T> {
-    @BeforeTemplate
-    Maybe<T> before(Flowable<T> flowable) {
-      return flowable.firstElement();
-    }
-
-    @AfterTemplate
-    Maybe<T> after(Flowable<T> flowable) {
-      return flowable.as(RxJava2Adapter::flowableToFlux).next().as(RxJava2Adapter::monoToMaybe);
-    }
-  }
-  // XXX: `function` type change; look into `Refaster.canBeCoercedTo(...)`.
   static final class FlowableToMap<I, T extends I, O> {
+
     @BeforeTemplate
     Single<Map<O, T>> before(Flowable<T> flowable, Function<I, O> function) {
       return flowable.toMap(function);
@@ -78,6 +97,7 @@ final class RxJavaToReactorTemplates {
   }
 
   static final class FlowableSwitchIfEmptyPublisher<S, T extends S> {
+
     @BeforeTemplate
     Flowable<S> before(Flowable<S> flowable, Publisher<S> publisher) {
       return flowable.switchIfEmpty(publisher);
@@ -92,22 +112,8 @@ final class RxJavaToReactorTemplates {
     }
   }
 
-//  static final class SingleFlatMap<I, T extends I, O, P extends Single<? extends O>> {
-//    @BeforeTemplate
-//    Single<O> before(Single<T> single, Function<I, P> function) {
-//      return single.flatMap(function);
-//    }
-//
-//    @AfterTemplate
-//    Single<O> after(Single<T> single, java.util.function.Function<I, P> function) {
-//      return single
-//          .as(RxJava2Adapter::singleToMono)
-//          .flatMap(function)
-//          .as(RxJava2Adapter::monoToSingle);
-//    }
-//  }
-
   static final class MaybeSwitchIfEmpty<S, T extends S> {
+
     @BeforeTemplate
     Single<S> before(Maybe<S> maybe, Single<T> single) {
       return maybe.switchIfEmpty(single);
@@ -122,6 +128,55 @@ final class RxJavaToReactorTemplates {
     }
   }
 
+  // XXX: `function` type change; look into `Refaster.canBeCoercedTo(...)`.
+  //  static final class SingleFlatMap<I, T extends I, O, P extends Single<? extends O>> {
+  //    @BeforeTemplate
+  //    Single<O> before(Single<T> single, Function<I, P> function) {
+  //      return single.flatMap(function);
+  //    }
+  //
+  //    @AfterTemplate
+  //    Single<O> after(Single<T> single, java.util.function.Function<I, P> function) {
+  //      return single
+  //          .as(RxJava2Adapter::singleToMono)
+  //          .flatMap(function)
+  //          .as(RxJava2Adapter::monoToSingle);
+  //    }
+
+  // XXX: `function` type change; look into `Refaster.canBeCoercedTo(...)`.
+  static final class SingleFilter<S, T extends S> {
+    @BeforeTemplate
+    Maybe<T> before(Single<T> single, Predicate<S> predicate) {
+      return single.filter(predicate);
+    }
+
+    @AfterTemplate
+    Maybe<T> after(Single<T> single, java.util.function.Predicate<S> predicate) {
+      return single
+          .as(RxJava2Adapter::singleToMono)
+          .filter(predicate)
+          .as(RxJava2Adapter::monoToMaybe);
+    }
+  }
+
+  static final class FluxToFlowableToFlux<T> {
+
+    @BeforeTemplate
+    Flux<T> before(Flux<T> flux, BackpressureStrategy strategy) {
+      return Refaster.anyOf(
+          flux.as(RxJava2Adapter::fluxToObservable)
+              .toFlowable(strategy)
+              .as(RxJava2Adapter::flowableToFlux),
+          flux.as(RxJava2Adapter::fluxToFlowable).as(RxJava2Adapter::flowableToFlux));
+    }
+
+    @AfterTemplate
+    Flux<T> after(Flux<T> flux) {
+      return flux;
+    }
+  }
+
+  // XXX: Stephan, what should we do with the naming here? Since it is not entirely correct now.
   static final class MonoToFlowableToMono<T> {
     @BeforeTemplate
     Mono<Void> before(Mono<Void> mono) {
@@ -138,38 +193,6 @@ final class RxJavaToReactorTemplates {
     @AfterTemplate
     Mono<T> after(Mono<T> mono) {
       return mono;
-    }
-  }
-
-  static final class FluxToFlowableToFlux<T> {
-    @BeforeTemplate
-    Flux<T> before(Flux<T> flux, BackpressureStrategy strategy) {
-      return Refaster.anyOf(
-          flux.as(RxJava2Adapter::fluxToObservable)
-              .toFlowable(strategy)
-              .as(RxJava2Adapter::flowableToFlux),
-          flux.as(RxJava2Adapter::fluxToFlowable).as(RxJava2Adapter::flowableToFlux));
-    }
-
-    @AfterTemplate
-    Flux<T> after(Flux<T> flux) {
-      return flux;
-    }
-  }
-
-  // XXX: `function` type change; look into `Refaster.canBeCoercedTo(...)`.
-  static final class FlowableMapToFluxMapToFlowable<I, T extends I, O> {
-    @BeforeTemplate
-    Flowable<O> before(Flowable<T> flowable, Function<I, O> function) {
-      return flowable.map(function);
-    }
-
-    @AfterTemplate
-    Flowable<O> after(Flowable<T> flowable, java.util.function.Function<I, O> function) {
-      return flowable
-          .as(RxJava2Adapter::flowableToFlux)
-          .map(function)
-          .as(RxJava2Adapter::fluxToFlowable);
     }
   }
 }
