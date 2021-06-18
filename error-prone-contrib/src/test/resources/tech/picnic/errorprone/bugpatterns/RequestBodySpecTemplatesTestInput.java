553e816f62b0314fd135061fa152ac8479202cc6
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-06-18 10:17:15 +0200

    Add a usage of  to fix the test

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/RequestBodySpecTemplatesTestInput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/RequestBodySpecTemplatesTestInput.java
@@ -7,5 +7,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 final class RequestBodySpecTemplatesTest implements RefasterTemplateTestCase {
   public void testBodyValue() {
     WebClient.create("foo").post().body(fromValue("bar")).retrieve();
+
+    // Extra usage of `fromValue` to prevent the test from failing. The import will be unused and
+    // cause the build to fail.
+    fromValue("");
   }
 }
--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/RequestBodySpecTemplatesTestOutput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/RequestBodySpecTemplatesTestOutput.java
@@ -1,9 +1,15 @@
 package tech.picnic.errorprone.bugpatterns;
 
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+
 import org.springframework.web.reactive.function.client.WebClient;
 
 final class RequestBodySpecTemplatesTest implements RefasterTemplateTestCase {
   public void testBodyValue() {
     WebClient.create("foo").post().bodyValue("bar").retrieve();
+
+    // Extra usage of `fromValue` to prevent the test from failing. The import will be unused and
+    // cause the build to fail.
+    fromValue("");
   }
 }
