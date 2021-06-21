28f09d3440a3f0e5ea8fa6c3d71950b345fcc044
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-06-21 19:46:03 +0200

    Apply suggestions of PR

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refastertemplates/RequestBodySpecTemplates.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refastertemplates/RequestBodySpecTemplates.java
@@ -4,23 +4,29 @@ import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 
 import com.google.errorprone.refaster.annotation.AfterTemplate;
 import com.google.errorprone.refaster.annotation.BeforeTemplate;
-import org.reactivestreams.Publisher;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.client.WebClient.RequestBodySpec;
 import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
 
-/** Refaster template to improve readability of {@link RequestBodySpec#body(Publisher, Class)} */
+/** Refaster template to improve readability of {@link RequestBodySpec#body(BodyInserter)} */
 final class RequestBodySpecTemplates {
   private RequestBodySpecTemplates() {}
 
   /** Prefer using {@link RequestBodySpec#bodyValue(Object)} */
   static final class BodyValue<T> {
     @BeforeTemplate
-    public RequestHeadersSpec<?> before(RequestBodySpec requestBodySpec, T value) {
+    RequestHeadersSpec<?> before(RequestBodySpec requestBodySpec, T value) {
+      return requestBodySpec.body(fromValue(value));
+    }
+
+    @BeforeTemplate
+    WebTestClient.RequestHeadersSpec<?> before2(WebTestClient.RequestBodySpec requestBodySpec, T value) {
       return requestBodySpec.body(fromValue(value));
     }
 
     @AfterTemplate
-    public RequestHeadersSpec<?> after(RequestBodySpec requestBodySpec, T value) {
+    RequestHeadersSpec<?> after(RequestBodySpec requestBodySpec, T value) {
       return requestBodySpec.bodyValue(value);
     }
   }
--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/RequestBodySpecTemplatesTestInput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/RequestBodySpecTemplatesTestInput.java
@@ -2,11 +2,14 @@ package tech.picnic.errorprone.bugpatterns;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 
 final class RequestBodySpecTemplatesTest implements RefasterTemplateTestCase {
   public void testBodyValue() {
-    WebClient.create("foo").post().body(fromValue("bar")).retrieve();
+    WebClient.create("foo").post().body(fromValue("bar"));
+
+    WebTestClient.bindToServer().build().post().body(fromValue("bar"));
 
     // Extra usage of `fromValue` to prevent the test from failing. The import will be unused and
     // cause the build to fail.
