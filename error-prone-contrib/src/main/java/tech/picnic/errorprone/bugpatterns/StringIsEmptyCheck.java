0d574016d04fc8eeac8d5b7066764159eaa8666f
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-11-17 13:58:20 +0100

    Improve the retrieval of receiver

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/StringIsEmptyCheck.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/StringIsEmptyCheck.java
@@ -3,9 +3,9 @@ package tech.picnic.errorprone.bugpatterns;
 import static com.google.errorprone.BugPattern.LinkType;
 import static com.google.errorprone.BugPattern.SeverityLevel;
 import static com.google.errorprone.BugPattern.StandardTags;
-import static com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import static com.google.errorprone.bugpatterns.BugChecker.*;
 import static com.google.errorprone.matchers.method.MethodMatchers.instanceMethod;
-import static com.sun.source.tree.Tree.Kind;
+import static com.sun.source.tree.Tree.*;
 
 import com.google.auto.service.AutoService;
 import com.google.errorprone.BugPattern;
@@ -14,10 +14,15 @@ import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.StringLiteral;
+import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.IdentifierTree;
 import com.sun.source.tree.LiteralTree;
 import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Symbol;
 
 @AutoService(BugChecker.class)
 @BugPattern(
@@ -32,16 +37,13 @@ public final class StringIsEmptyCheck extends BugChecker implements MethodInvoca
       instanceMethod().onDescendantOf("java.lang.String").named("equals");
 
   @Override
+  @SuppressWarnings("TreeToString")
   public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
     if (STRING_EQUALS_INVOCATION.matches(tree, state)
         && tree.getArguments().get(0).getKind() == Kind.STRING_LITERAL
         && ((LiteralTree) tree.getArguments().get(0)).getValue().equals("")) {
       return describeMatch(
-          tree,
-          SuggestedFix.replace(
-              tree,
-              Util.treeToString(((MemberSelectTree) tree.getMethodSelect()).getExpression(), state)
-                  + ".isEmpty()"));
+          tree, SuggestedFix.replace(tree, ASTHelpers.getReceiver(tree) + ".isEmpty()"));
     }
     return Description.NO_MATCH;
   }
