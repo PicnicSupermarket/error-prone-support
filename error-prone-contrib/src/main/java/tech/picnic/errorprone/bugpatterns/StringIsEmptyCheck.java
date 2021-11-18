db949f852b07debdfa3bc1f224d5a46c6cab77bd
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-11-18 13:11:49 +0100

    Remove things for SS

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/StringIsEmptyCheck.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/StringIsEmptyCheck.java
@@ -3,9 +3,9 @@ package tech.picnic.errorprone.bugpatterns;
 import static com.google.errorprone.BugPattern.LinkType;
 import static com.google.errorprone.BugPattern.SeverityLevel;
 import static com.google.errorprone.BugPattern.StandardTags;
-import static com.google.errorprone.bugpatterns.BugChecker.*;
+import static com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
 import static com.google.errorprone.matchers.method.MethodMatchers.instanceMethod;
-import static com.sun.source.tree.Tree.*;
+import static com.sun.source.tree.Tree.Kind;
 
 import com.google.auto.service.AutoService;
 import com.google.errorprone.BugPattern;
@@ -14,30 +14,22 @@ import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
-import com.google.errorprone.matchers.StringLiteral;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.IdentifierTree;
 import com.sun.source.tree.LiteralTree;
-import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.Tree;
-import com.sun.tools.javac.code.Symbol;
 
 @AutoService(BugChecker.class)
 @BugPattern(
     name = "StringIsEmpty",
-    summary = "Prefer String#isEmpty over String#equals.",
-    linkType = LinkType.NONE,
+    summary = "Prefer `String#isEmpty` over `String#equals`",
     severity = SeverityLevel.SUGGESTION,
     tags = StandardTags.STYLE)
 public final class StringIsEmptyCheck extends BugChecker implements MethodInvocationTreeMatcher {
-
   private static final Matcher<ExpressionTree> STRING_EQUALS_INVOCATION =
       instanceMethod().onDescendantOf("java.lang.String").named("equals");
 
   @Override
-  @SuppressWarnings("TreeToString")
   public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
     if (STRING_EQUALS_INVOCATION.matches(tree, state)
         && tree.getArguments().get(0).getKind() == Kind.STRING_LITERAL
