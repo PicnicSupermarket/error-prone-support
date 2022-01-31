fe958130dba353440f53848d21c9c008b39fc740
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2022-01-31 17:10:56 +0100

    Introduce `AssertJMapTemplates`

---
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/AssertJMapTemplatesTestOutput.java
@@ -0,0 +1,18 @@
+package tech.picnic.errorprone.bugpatterns;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import org.assertj.core.api.AbstractMapAssert;
+
+final class AssertJMapTemplatesTest implements RefasterTemplateTestCase {
+  AbstractMapAssert<?, ?, Integer, Integer> testAbstractMapAssertContainsExactlyEntriesOf() {
+    return assertThat(ImmutableMap.of(1, 2, 3, 4))
+        .containsExactlyInAnyOrderEntriesOf(ImmutableMap.of(1, 2, 3, 4));
+  }
+
+  AbstractMapAssert<?, ?, Integer, Integer> testAbstractMapAssertContainsEntry() {
+    return assertThat(ImmutableMap.of(1, 2)).containsExactly(Map.entry(1, 2));
+  }
+}
