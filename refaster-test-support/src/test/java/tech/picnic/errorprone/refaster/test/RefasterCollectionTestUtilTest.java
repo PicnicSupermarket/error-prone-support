be1967f1a4726f8eaaf9d30f10d40f6b8782658f
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2022-03-31 20:56:41 +0200

    Add explanation in `RefasterCollectionTestUtilTest.java`

--- refaster-test-support/src/test/java/tech/picnic/errorprone/refaster/test/RefasterCollectionTestUtilTest.java
+++ refaster-test-support/src/test/java/tech/picnic/errorprone/refaster/test/RefasterCollectionTestUtilTest.java
@@ -3,7 +3,15 @@ package tech.picnic.errorprone.refaster.test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-/** Validate error reporting of the Refaster template collections. */
+/**
+ * Validate error reporting of the Refaster template collections.
+ *
+ * <p>The goal of the Refaster template collections under test is to verify the reporting of {@link
+ * RefasterCollectionTestUtil} using the associated `TestInput` and `TestOutput` files. Normally,
+ * {@link RefasterCollectionTestUtil} will report error messages directly in the console where the
+ * results of the tests are shown. However, to verify that these messages are correct, the
+ * `*TestOutput` files in this package contain error reporting that is normally not written.
+ */
 final class RefasterCollectionTestUtilTest {
   @ParameterizedTest
   @ValueSource(
