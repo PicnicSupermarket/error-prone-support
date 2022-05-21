c55074d2fd8ef5391b96b062ac8e7a5ad9fd0eed
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2022-05-21 16:11:22 +0200

    Format

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/AssertJExceptionTemplatesTestInput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/AssertJExceptionTemplatesTestInput.java
@@ -1,30 +1,30 @@
 package tech.picnic.errorprone.bugpatterns;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.google.common.collect.ImmutableSet;
 import org.assertj.core.api.AbstractThrowableAssert;
-import org.assertj.core.api.ThrowableAssertAlternative;
-
-import static org.assertj.core.api.Assertions.*;
 
 final class AssertJExceptionTemplatesTest implements RefasterTemplateTestCase {
-    ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>> testThrowableAssertAlternativeHasMessageArgs() {
-        return ImmutableSet.of(
-                assertThatThrownBy(() -> {})
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage(String.format("foo %s", "bar")),
-                assertThatThrownBy(() -> {})
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage(String.format("foo %s %f", "bar", 1))
-        );
-    }
+  ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>>
+      testThrowableAssertAlternativeHasMessageArgs() {
+    return ImmutableSet.of(
+        assertThatThrownBy(() -> {})
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(String.format("foo %s", "bar")),
+        assertThatThrownBy(() -> {})
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(String.format("foo %s %f", "bar", 1)));
+  }
 
-    ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>> testThrowableAssertAlternativeWithFailMessageArgs() {
-        return ImmutableSet.of(
-                assertThatThrownBy(() -> {})
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .withFailMessage(String.format("foo %s", "bar")),
-                assertThatThrownBy(() -> {})
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .withFailMessage(String.format("foo %s %f", "bar", 1)));
-    }
+  ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>>
+      testThrowableAssertAlternativeWithFailMessageArgs() {
+    return ImmutableSet.of(
+        assertThatThrownBy(() -> {})
+            .isInstanceOf(IllegalArgumentException.class)
+            .withFailMessage(String.format("foo %s", "bar")),
+        assertThatThrownBy(() -> {})
+            .isInstanceOf(IllegalArgumentException.class)
+            .withFailMessage(String.format("foo %s %f", "bar", 1)));
+  }
 }
--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/AssertJExceptionTemplatesTestOutput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/bugpatterns/AssertJExceptionTemplatesTestOutput.java
@@ -1,30 +1,30 @@
 package tech.picnic.errorprone.bugpatterns;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.google.common.collect.ImmutableSet;
 import org.assertj.core.api.AbstractThrowableAssert;
-import org.assertj.core.api.ThrowableAssertAlternative;
-
-import static org.assertj.core.api.Assertions.*;
 
 final class AssertJExceptionTemplatesTest implements RefasterTemplateTestCase {
-    ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>> testThrowableAssertAlternativeHasMessageArgs() {
-        return ImmutableSet.of(
-                assertThatThrownBy(() -> {})
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("foo %s", "bar"),
-                assertThatThrownBy(() -> {})
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("foo %s %f", "bar", 1)
-        );
-    }
+  ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>>
+      testThrowableAssertAlternativeHasMessageArgs() {
+    return ImmutableSet.of(
+        assertThatThrownBy(() -> {})
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("foo %s", "bar"),
+        assertThatThrownBy(() -> {})
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("foo %s %f", "bar", 1));
+  }
 
-    ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>> testThrowableAssertAlternativeWithFailMessageArgs() {
-        return ImmutableSet.of(
-                assertThatThrownBy(() -> {})
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .withFailMessage("foo %s", "bar"),
-                assertThatThrownBy(() -> {})
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .withFailMessage("foo %s %f", "bar", 1));
-    }
+  ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>>
+      testThrowableAssertAlternativeWithFailMessageArgs() {
+    return ImmutableSet.of(
+        assertThatThrownBy(() -> {})
+            .isInstanceOf(IllegalArgumentException.class)
+            .withFailMessage("foo %s", "bar"),
+        assertThatThrownBy(() -> {})
+            .isInstanceOf(IllegalArgumentException.class)
+            .withFailMessage("foo %s %f", "bar", 1));
+  }
 }
