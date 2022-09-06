528c0b4325f4ee2a3dd56774b24e559e9d812462
Elena Liashenko <elena.liashenko@teampicnic.com>
2022-09-06 11:47:15 +0200

    Add documentation to utils

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/util/NestedTypesUtils.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/util/NestedTypesUtils.java
@@ -8,10 +8,20 @@ import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.util.List;
 
+/** Utility class that can be used to identify nesting of the same type. */
 public final class NestedTypesUtils {
 
   private NestedTypesUtils() {}
 
+  /**
+   * Checks nesting of the same type.
+   *
+   * @param type Type we expect to be nested.
+   * @param tree The AST node of interest.
+   * @param state A {@link VisitorState} describing the context in which the given {@link Tree} is
+   *     found.
+   * @return {@code true} if the given node contains nested node of the same type.
+   */
   public static boolean isSameTypeNested(Supplier<Type> type, Tree tree, VisitorState state) {
     Type expectedType = type.get(state);
     Type actualType = ASTHelpers.getType(tree);
