1477c5a4f6cb34339750991fdc956f44d46b154a
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2022-09-22 21:46:44 +0200

    Another round

--- refaster-support/src/main/java/tech/picnic/errorprone/refaster/AnnotatedCompositeCodeTransformer.java
+++ refaster-support/src/main/java/tech/picnic/errorprone/refaster/AnnotatedCompositeCodeTransformer.java
@@ -2,23 +2,38 @@ package tech.picnic.errorprone.refaster;
 
 import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.collect.ImmutableList;
+import com.google.errorprone.BugPattern.SeverityLevel;
 import com.google.errorprone.CodeTransformer;
 import com.google.errorprone.DescriptionListener;
+import com.google.errorprone.matchers.Description;
 import com.sun.source.util.TreePath;
 import com.sun.tools.javac.util.Context;
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
+import java.util.Optional;
+import java.util.function.Function;
+import tech.picnic.errorprone.refaster.annotation.TemplateCollection;
 
 // XXX: Move? Rename? Use `@AutoValue`?
 // XXX: This is a bit of an implementation detail. Alternative idea: move this to `refaster-runner`,
 // and have `refaster-compiler` depend on `refaster-runner`. (Or the other way around?)
+// XXX: If we go this route, do we need `CodeTransformer#annotations()` at all? We can track the
+// meta-data we're interested in in a custom format (like we already do for the name).
+// XXX: Or should the name _also_ be derived from an annotation? Upshot is limited.
+// XXX: ^ Name could be dropped if we derive it from `description.checkName`, with the assumption
+// that package and class names follow idiomatic Java naming convention. Kinda icky...
 public final class AnnotatedCompositeCodeTransformer implements CodeTransformer, Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private final String name;
   private final ImmutableList<CodeTransformer> transformers;
   private final ImmutableClassToInstanceMap<Annotation> annotations;
 
   public AnnotatedCompositeCodeTransformer(
+      String name,
       ImmutableList<CodeTransformer> transformers,
       ImmutableClassToInstanceMap<Annotation> annotations) {
+    this.name = name;
     this.transformers = transformers;
     this.annotations = annotations;
   }
@@ -31,7 +46,53 @@ public final class AnnotatedCompositeCodeTransformer implements CodeTransformer,
   @Override
   public void apply(TreePath path, Context context, DescriptionListener listener) {
     for (CodeTransformer transformer : transformers) {
-      transformer.apply(path, context, listener);
+      transformer.apply(
+          path,
+          context,
+          description -> listener.onDescribed(augmentDescription(description, transformer)));
     }
+  }
+
+  private Description augmentDescription(Description description, CodeTransformer delegate) {
+    String linkPattern =
+        extract(AnnotatedCompositeCodeTransformer::getLinkPattern, delegate, this).orElse("");
+    SeverityLevel severityLevel =
+        extract(AnnotatedCompositeCodeTransformer::getSeverity, delegate, this)
+            .orElse(SeverityLevel.SUGGESTION);
+    String message =
+        extract(AnnotatedCompositeCodeTransformer::getDescription, delegate, this)
+            .orElse("Refactoring opportunity");
+
+    // XXX: Replace only the first `$`.
+    // XXX: Test this.
+    return Description.builder(
+            description.position,
+            description.checkName,
+            String.format(linkPattern, name.replace('$', '#')),
+            severityLevel,
+            message)
+        .addAllFixes(description.fixes)
+        .build();
+  }
+
+  // XXX: Deduplicate the code below.
+
+  private static <S, T> Optional<T> extract(Function<S, Optional<T>> extractor, S first, S second) {
+    return extractor.apply(first).or(() -> extractor.apply(second));
+  }
+
+  private static Optional<String> getLinkPattern(CodeTransformer codeTransformer) {
+    return Optional.ofNullable(codeTransformer.annotations().getInstance(TemplateCollection.class))
+        .map(TemplateCollection::linkPattern);
+  }
+
+  private static Optional<SeverityLevel> getSeverity(CodeTransformer codeTransformer) {
+    return Optional.ofNullable(codeTransformer.annotations().getInstance(TemplateCollection.class))
+        .map(TemplateCollection::severity);
+  }
+
+  private static Optional<String> getDescription(CodeTransformer codeTransformer) {
+    return Optional.ofNullable(codeTransformer.annotations().getInstance(TemplateCollection.class))
+        .map(TemplateCollection::description);
   }
 }
--- refaster-test-support/src/test/java/tech/picnic/errorprone/refaster/test/MatchInWrongMethodRules.java
+++ refaster-test-support/src/test/java/tech/picnic/errorprone/refaster/test/MatchInWrongMethodRules.java
@@ -1,5 +1,7 @@
 package tech.picnic.errorprone.refaster.test;
 
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+
 import com.google.errorprone.refaster.annotation.AfterTemplate;
 import com.google.errorprone.refaster.annotation.BeforeTemplate;
 import tech.picnic.errorprone.refaster.annotation.TemplateCollection;
@@ -7,8 +9,10 @@ import tech.picnic.errorprone.refaster.annotation.TemplateCollection;
 /** Refaster rule collection to validate reporting of a match occurring in an unexpected place. */
 @TemplateCollection(linkPattern = "XXX")
 final class MatchInWrongMethodTemplates {
-  // XXX: Test merging/overriding.
-  //  @TemplateCollection(linkPattern = "XXX")
+  private MatchInWrongMethodTemplates() {}
+
+  // XXX: Demo: nesting overrides.
+  @TemplateCollection(linkPattern = "YYY", severity = ERROR, description = "Foo")
   static final class StringIsEmpty {
     @BeforeTemplate
     boolean before(String string) {
