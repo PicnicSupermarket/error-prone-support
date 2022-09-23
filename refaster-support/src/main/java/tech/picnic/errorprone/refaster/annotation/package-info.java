9622b43fe7c4c8189fefce7c4735d8b436022146
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2022-09-23 22:26:58 +0200

    Flag why build currently doesn't fail, while it should.

---
+++ refaster-support/src/main/java/tech/picnic/errorprone/refaster/annotation/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * A collection annotations that may be placed on Refaster template classes and Refaster template
+ * collection classes, thus influencing the way in which associated template matches are reported in
+ * non-patch mode.
+ */
+@com.google.errorprone.annotations.CheckReturnValue
+@javax.annotation.ParametersAreNonnullByDefault
+package tech.picnic.errorprone.refaster.annotation;
