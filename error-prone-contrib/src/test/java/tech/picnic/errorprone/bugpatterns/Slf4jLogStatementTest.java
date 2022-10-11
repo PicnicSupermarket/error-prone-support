5cec0dd5085bb6a0ccccd79c86ddf2090bcb21b9
Pieter Dirk Soels <p.d.soels@hotmail.com>
2022-10-11 14:57:19 +0200

    Rename `Slf4JLogStatementTest` to `Slf4jLogStatementTest` (#289)

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/Slf4jLogStatementTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/Slf4jLogStatementTest.java
@@ -5,7 +5,7 @@ import com.google.errorprone.BugCheckerRefactoringTestHelper.TestMode;
 import com.google.errorprone.CompilationTestHelper;
 import org.junit.jupiter.api.Test;
 
-final class Slf4JLogStatementTest {
+final class Slf4jLogStatementTest {
   private final CompilationTestHelper compilationTestHelper =
       CompilationTestHelper.newInstance(Slf4jLogStatement.class, getClass());
   private final BugCheckerRefactoringTestHelper refactoringTestHelper =
