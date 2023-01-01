63a13863bd33fbbbe1e5e63fdcb109cf56b7f784
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-01-01 14:42:24 +0100

    Suggestions

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/RequestParamType.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/RequestParamType.java
@@ -24,23 +24,25 @@ import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.VariableTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.suppliers.Suppliers;
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.VariableTree;
+import tech.picnic.errorprone.bugpatterns.util.Flags;
 
 /** A {@link BugChecker} that flags {@code @RequestParam} parameters with an unsupported type. */
 @AutoService(BugChecker.class)
 @BugPattern(
-    summary = "`@RequestParam` does not support `ImmutableCollection` and `ImmutableMap` subtypes",
+    summary =
+        "By default, `@RequestParam` does not support `ImmutableCollection` and `ImmutableMap` subtypes",
     link = BUG_PATTERNS_BASE_URL + "RequestParamType",
     linkType = CUSTOM,
     severity = ERROR,
     tags = LIKELY_ERROR)
 public final class RequestParamType extends BugChecker implements VariableTreeMatcher {
   private static final long serialVersionUID = 1L;
-  private static final String FLAG_PREFIX = "RequestParamType:";
-  private static final String INCLUDED_CLASS_FLAG = FLAG_PREFIX + "Includes";
+  private static final String SUPPORTED_CUSTOM_TYPES_FLAG = "RequestParamType:SupportedCustomTypes";
 
-  private final Matcher<VariableTree> hasUnsupportedRequestParams;
+  private final Matcher<VariableTree> hasUnsupportedRequestParamType;
 
   /** Instantiates a default {@link RequestParamType} instance. */
   public RequestParamType() {
@@ -53,42 +55,27 @@ public final class RequestParamType extends BugChecker implements VariableTreeMa
    * @param flags Any provided command line flags.
    */
   public RequestParamType(ErrorProneFlags flags) {
-    hasUnsupportedRequestParams = createVariableTreeMatcher(flags);
+    hasUnsupportedRequestParamType = hasUnsupportedRequestParamType(flags);
   }
 
   @Override
   public Description matchVariable(VariableTree tree, VisitorState state) {
-    return hasUnsupportedRequestParams.matches(tree, state)
+    return hasUnsupportedRequestParamType.matches(tree, state)
         ? describeMatch(tree)
         : Description.NO_MATCH;
   }
 
-  private static Matcher<VariableTree> createVariableTreeMatcher(ErrorProneFlags flags) {
+  private static Matcher<VariableTree> hasUnsupportedRequestParamType(ErrorProneFlags flags) {
     return allOf(
         annotations(AT_LEAST_ONE, isType("org.springframework.web.bind.annotation.RequestParam")),
         anyOf(isSubtypeOf(ImmutableCollection.class), isSubtypeOf(ImmutableMap.class)),
-        not(anyOf(getSupportedClasses(includedClassNames(flags)))));
+        not(isSubtypeOfAny(Flags.getList(flags, SUPPORTED_CUSTOM_TYPES_FLAG))));
   }
 
-  private static ImmutableList<String> includedClassNames(ErrorProneFlags flags) {
-    return flags.getList(INCLUDED_CLASS_FLAG).map(ImmutableList::copyOf).orElse(ImmutableList.of());
-  }
-
-  private static ImmutableList<Matcher<Tree>> getSupportedClasses(
-      ImmutableList<String> inclusions) {
-    return inclusions.stream()
-        .map(String::trim)
-        .filter(inclusion -> !inclusion.isEmpty())
-        .map(inclusion -> isSubtypeOf(createClass(inclusion)))
-        .collect(toImmutableList());
-  }
-
-  private static Class<?> createClass(String className) {
-    try {
-      return Class.forName(className);
-    } catch (ClassNotFoundException e) {
-      throw new IllegalArgumentException(
-          String.format("Invalid class name '%s' in `%s`", className, INCLUDED_CLASS_FLAG), e);
-    }
+  private static Matcher<Tree> isSubtypeOfAny(ImmutableList<String> inclusions) {
+    return anyOf(
+        inclusions.stream()
+            .map(inclusion -> isSubtypeOf(Suppliers.typeFromString(inclusion)))
+            .collect(toImmutableList()));
   }
 }
--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/RequestParamTypeTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/RequestParamTypeTest.java
@@ -1,6 +1,5 @@
 package tech.picnic.errorprone.bugpatterns;
 
-import com.google.common.collect.ImmutableList;
 import com.google.errorprone.CompilationTestHelper;
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +9,7 @@ final class RequestParamTypeTest {
   private final CompilationTestHelper restrictedCompilationTestHelper =
       CompilationTestHelper.newInstance(RequestParamType.class, getClass())
           .setArgs(
-              ImmutableList.of(
-                  "-XepOpt:RequestParamType:Includes=com.google.common.collect.ImmutableCollection"));
-  private final CompilationTestHelper restrictedWithSubTypeCompilationTestHelper =
-      CompilationTestHelper.newInstance(RequestParamType.class, getClass())
-          .setArgs(
-              ImmutableList.of(
-                  "-XepOpt:RequestParamType:Includes=com.google.common.collect.ImmutableSet"));
-  private final CompilationTestHelper restrictedWithInvalidFlagCompilationTestHelper =
-      CompilationTestHelper.newInstance(RequestParamType.class, getClass())
-          .setArgs(ImmutableList.of("-XepOpt:RequestParamType:Includes="));
+              "-XepOpt:RequestParamType:SupportedCustomTypes=com.google.common.collect.ImmutableSet,com.google.common.collect.ImmutableSortedMultiset");
 
   @Test
   void identification() {
@@ -79,151 +69,50 @@ final class RequestParamTypeTest {
   }
 
   @Test
-  void identificationOfIncludedClass() {
+  void identificationRestricted() {
     restrictedCompilationTestHelper
         .addSourceLines(
             "A.java",
             "import com.google.common.collect.ImmutableBiMap;",
+            "import com.google.common.collect.ImmutableCollection;",
             "import com.google.common.collect.ImmutableList;",
             "import com.google.common.collect.ImmutableMap;",
+            "import com.google.common.collect.ImmutableMultiset;",
             "import com.google.common.collect.ImmutableSet;",
-            "import java.util.List;",
-            "import java.util.Map;",
-            "import java.util.Set;",
-            "import org.jspecify.annotations.Nullable;",
-            "import org.springframework.web.bind.annotation.DeleteMapping;",
+            "import com.google.common.collect.ImmutableSortedMultiset;",
+            "import com.google.common.collect.ImmutableSortedSet;",
             "import org.springframework.web.bind.annotation.GetMapping;",
-            "import org.springframework.web.bind.annotation.PostMapping;",
-            "import org.springframework.web.bind.annotation.PutMapping;",
-            "import org.springframework.web.bind.annotation.RequestBody;",
-            "import org.springframework.web.bind.annotation.RequestParam;",
-            "",
-            "interface A {",
-            "  @PostMapping",
-            "  A properRequestParam(@RequestBody String body);",
-            "",
-            "  @GetMapping",
-            "  A properRequestParam(@RequestParam int param);",
-            "",
-            "  @GetMapping",
-            "  A properRequestParam(@RequestParam List<String> param);",
-            "",
-            "  @PostMapping",
-            "  A properRequestParam(@RequestBody String body, @RequestParam Set<String> param);",
-            "",
-            "  @PutMapping",
-            "  A properRequestParam(@RequestBody String body, @RequestParam Map<String, String> param);",
-            "",
-            "  @GetMapping",
-            "  // BUG: Diagnostic contains:",
-            "  A get(@RequestParam ImmutableBiMap<String, String> param);",
-            "",
-            "  @PostMapping",
-            "  A post(@Nullable @RequestParam ImmutableList<String> param);",
-            "",
-            "  @PutMapping",
-            "  A put(@RequestBody String body, @RequestParam ImmutableSet<String> param);",
-            "",
-            "  @DeleteMapping",
-            "  // BUG: Diagnostic contains:",
-            "  A delete(@RequestBody String body, @RequestParam ImmutableMap<String, String> param);",
-            "",
-            "  void negative(ImmutableSet<Integer> set, ImmutableMap<String, String> map);",
-            "}")
-        .doTest();
-  }
-
-  @Test
-  void identificationOfIncludedSubClass() {
-    restrictedWithSubTypeCompilationTestHelper
-        .addSourceLines(
-            "A.java",
-            "import com.google.common.collect.ImmutableBiMap;",
-            "import com.google.common.collect.ImmutableList;",
-            "import com.google.common.collect.ImmutableMap;",
-            "import com.google.common.collect.ImmutableSet;",
-            "import org.jspecify.annotations.Nullable;",
-            "import org.springframework.web.bind.annotation.DeleteMapping;",
-            "import org.springframework.web.bind.annotation.GetMapping;",
-            "import org.springframework.web.bind.annotation.PostMapping;",
-            "import org.springframework.web.bind.annotation.PutMapping;",
-            "import org.springframework.web.bind.annotation.RequestBody;",
             "import org.springframework.web.bind.annotation.RequestParam;",
             "",
             "interface A {",
             "  @GetMapping",
             "  // BUG: Diagnostic contains:",
-            "  A get(@RequestParam ImmutableBiMap<String, String> param);",
-            "",
-            "  @PostMapping",
-            "  // BUG: Diagnostic contains:",
-            "  A post(@Nullable @RequestParam ImmutableList<String> param);",
-            "",
-            "  @PutMapping",
-            "  A put(@RequestBody String body, @RequestParam ImmutableSet<String> param);",
-            "",
-            "  @DeleteMapping",
-            "  // BUG: Diagnostic contains:",
-            "  A delete(@RequestBody String body, @RequestParam ImmutableMap<String, String> param);",
-            "",
-            "  void negative(ImmutableSet<Integer> set, ImmutableMap<String, String> map);",
-            "}")
-        .doTest();
-  }
-
-  @Test
-  void identificationOfInvalidFlag() {
-    restrictedWithInvalidFlagCompilationTestHelper
-        .addSourceLines(
-            "A.java",
-            "import com.google.common.collect.ImmutableBiMap;",
-            "import com.google.common.collect.ImmutableList;",
-            "import com.google.common.collect.ImmutableMap;",
-            "import com.google.common.collect.ImmutableSet;",
-            "import java.util.List;",
-            "import java.util.Map;",
-            "import java.util.Set;",
-            "import org.jspecify.annotations.Nullable;",
-            "import org.springframework.web.bind.annotation.DeleteMapping;",
-            "import org.springframework.web.bind.annotation.GetMapping;",
-            "import org.springframework.web.bind.annotation.PostMapping;",
-            "import org.springframework.web.bind.annotation.PutMapping;",
-            "import org.springframework.web.bind.annotation.RequestBody;",
-            "import org.springframework.web.bind.annotation.RequestParam;",
-            "",
-            "interface A {",
-            "  @PostMapping",
-            "  A properRequestParam(@RequestBody String body);",
-            "",
-            "  @GetMapping",
-            "  A properRequestParam(@RequestParam int param);",
-            "",
-            "  @GetMapping",
-            "  A properRequestParam(@RequestParam List<String> param);",
-            "",
-            "  @PostMapping",
-            "  A properRequestParam(@RequestBody String body, @RequestParam Set<String> param);",
-            "",
-            "  @PutMapping",
-            "  A properRequestParam(@RequestBody String body, @RequestParam Map<String, String> param);",
+            "  A immutableCollection(@RequestParam ImmutableCollection<String> param);",
             "",
             "  @GetMapping",
             "  // BUG: Diagnostic contains:",
-            "  A get(@RequestParam ImmutableBiMap<String, String> param);",
+            "  A immutableList(@RequestParam ImmutableList<String> param);",
             "",
-            "  @PostMapping",
+            "  @GetMapping",
+            "  A immutableSet(@RequestParam ImmutableSet<String> param);",
+            "",
+            "  @GetMapping",
+            "  A immutableSortedSet(@RequestParam ImmutableSortedSet<String> param);",
+            "",
+            "  @GetMapping",
             "  // BUG: Diagnostic contains:",
-            "  A post(@Nullable @RequestParam ImmutableList<String> param);",
+            "  A immutableMultiset(@RequestParam ImmutableMultiset<String> param);",
             "",
-            "  @PutMapping",
+            "  @GetMapping",
+            "  A immutableSortedMultiset(@RequestParam ImmutableSortedMultiset<String> param);",
+            "",
+            "  @GetMapping",
             "  // BUG: Diagnostic contains:",
-            "  A put(@RequestBody String body, @RequestParam ImmutableSet<String> param);",
+            "  A immutableMap(@RequestParam ImmutableMap<String, String> param);",
             "",
-            "  @DeleteMapping",
+            "  @GetMapping",
             "  // BUG: Diagnostic contains:",
-            "  A delete(@RequestBody String body, @RequestParam ImmutableMap<String, String> param);",
-            "",
-            "  void negative(ImmutableSet<Integer> set, ImmutableMap<String, String> map);",
+            "  A immutableBiMap(@RequestParam ImmutableBiMap<String, String> param);",
             "}")
         .doTest();
   }
