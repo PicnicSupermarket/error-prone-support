d4626ade2f78a0a5523c0435bb8c20141871dddb
Benedek Halasi <t0h3ow7t2@mozmail.com>
2023-01-23 09:55:06 +0100

    Revert all "Introduce `Flux.to{Stream,Iterable}` Refaster rules" connected commits

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/ReactorRules.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/ReactorRules.java
@@ -4,7 +4,6 @@ import static com.google.common.collect.MoreCollectors.toOptional;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
 import static com.google.errorprone.refaster.ImportPolicy.STATIC_IMPORT_ALWAYS;
 import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static reactor.function.TupleUtils.function;
 
@@ -20,7 +19,6 @@ import com.google.errorprone.refaster.annotation.UseImportPolicy;
 import java.time.Duration;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.function.BiConsumer;
@@ -29,7 +27,6 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
 import org.jspecify.annotations.Nullable;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
@@ -1304,35 +1301,6 @@ final class ReactorRules {
     @AfterTemplate
     Duration after(StepVerifier.LastStep step, Duration duration) {
       return step.verifyTimeout(duration);
-    }
-  }
-
-  /** Avoid accidental blocking with {@link Flux#toStream()}. */
-  // XXX: The alternative may lose some performance due to
-  // buffering and prefetching
-  static final class FluxToStream<T> {
-    @BeforeTemplate
-    Stream<T> before(Flux<T> flux) {
-      return flux.toStream();
-    }
-
-    @AfterTemplate
-    Stream<T> after(Flux<T> flux) {
-      return flux.collect(toList()).block().stream();
-    }
-  }
-
-  /** Avoid accidental blocking with {@link Flux#toIterable()}. */
-  // XXX: The alternative may lose some performance due to buffering and prefetching
-  static final class FluxToIterable<T> {
-    @BeforeTemplate
-    Iterable<T> before(Flux<T> flux) {
-      return flux.toIterable();
-    }
-
-    @AfterTemplate
-    List<T> after(Flux<T> flux) {
-      return flux.collect(toList()).block();
     }
   }
 }
