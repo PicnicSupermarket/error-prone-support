e1b5f723c13f79174ef4d07861dee1ce072fea43
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2023-01-27 12:07:08 +0100

    Suggestion to restructure the check

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoMockClassReference.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoMockClassReference.java
@@ -4,13 +4,13 @@ import static com.google.errorprone.BugPattern.LinkType.CUSTOM;
 import static com.google.errorprone.BugPattern.SeverityLevel.SUGGESTION;
 import static com.google.errorprone.BugPattern.StandardTags.SIMPLIFICATION;
 import static com.google.errorprone.matchers.Matchers.allOf;
-import static com.google.errorprone.matchers.Matchers.anyOf;
 import static com.google.errorprone.matchers.Matchers.argument;
 import static com.google.errorprone.matchers.Matchers.argumentCount;
 import static com.google.errorprone.matchers.Matchers.isSameType;
 import static com.google.errorprone.matchers.Matchers.isVariable;
 import static com.google.errorprone.matchers.Matchers.not;
 import static com.google.errorprone.matchers.Matchers.staticMethod;
+import static com.sun.source.tree.Tree.Kind.ASSIGNMENT;
 import static com.sun.source.tree.Tree.Kind.RETURN;
 import static com.sun.source.tree.Tree.Kind.VARIABLE;
 import static java.util.Objects.requireNonNull;
@@ -26,10 +26,12 @@ import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.AssignmentTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.ReturnTree;
 import com.sun.source.tree.Tree;
+import com.sun.source.tree.Tree.Kind;
 import com.sun.source.tree.VariableTree;
 
 /**
@@ -53,11 +55,6 @@ public final class MockitoMockClassReference extends BugChecker
           argumentCount(1),
           argument(0, allOf(isSameType(Class.class.getName()), not(isVariable()))),
           staticMethod().onClass("org.mockito.Mockito").namedAnyOf("mock", "spy"));
-  // XXX: Replace `var` usage with explicit type instead.
-  private static final Matcher<VariableTree> INCOMPATIBLE_VARIABLE =
-      anyOf(ASTHelpers::hasNoExplicitType, MockitoMockClassReference::hasTypeDifference);
-  private static final Matcher<ReturnTree> INCOMPATIBLE_RETURN =
-      MockitoMockClassReference::hasTypeDifference;
 
   /** Instantiates a new {@link MockitoMockClassReference} instance. */
   public MockitoMockClassReference() {}
@@ -69,24 +66,25 @@ public final class MockitoMockClassReference extends BugChecker
     }
 
     Tree parent = state.getPath().getParentPath().getLeaf();
-    if (parent.getKind() == VARIABLE
-        && INCOMPATIBLE_VARIABLE.matches((VariableTree) parent, state)) {
-      return Description.NO_MATCH;
-    } else if (parent.getKind() == RETURN
-        && INCOMPATIBLE_RETURN.matches((ReturnTree) parent, state)) {
+    Kind parentKind = parent.getKind();
+    if (parentKind != VARIABLE && parentKind != ASSIGNMENT && parentKind != RETURN) {
       return Description.NO_MATCH;
     }
 
-    return describeMatch(tree, SuggestedFix.delete(Iterables.getOnlyElement(tree.getArguments())));
+    return isParentIncompatible(parent, state)
+        ? Description.NO_MATCH
+        : describeMatch(tree, SuggestedFix.delete(Iterables.getOnlyElement(tree.getArguments())));
   }
 
-  private static boolean hasTypeDifference(VariableTree tree, VisitorState state) {
-    return hasTypeDifference(tree, tree.getInitializer(), state);
-  }
-
-  private static boolean hasTypeDifference(ReturnTree tree, VisitorState state) {
+  private static boolean isParentIncompatible(Tree parent, VisitorState state) {
+    if (parent instanceof VariableTree) {
+      return ASTHelpers.hasNoExplicitType((VariableTree) parent, state)
+          || hasTypeDifference(parent, ((VariableTree) parent).getInitializer(), state);
+    } else if (parent instanceof AssignmentTree) {
+      return hasTypeDifference(parent, ((AssignmentTree) parent).getExpression(), state);
+    }
     Tree returnTypeTree = requireNonNull(state.findEnclosing(MethodTree.class)).getReturnType();
-    return hasTypeDifference(returnTypeTree, tree.getExpression(), state);
+    return hasTypeDifference(returnTypeTree, ((ReturnTree) parent).getExpression(), state);
   }
 
   private static boolean hasTypeDifference(Tree treeA, Tree treeB, VisitorState state) {
