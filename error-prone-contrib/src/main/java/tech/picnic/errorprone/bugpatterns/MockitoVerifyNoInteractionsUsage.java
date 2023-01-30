63d351c54addca063ed4749ec63e90e05b9f16c8
Kamil Gabaydullin <kamil.gabaydullin@teampicnic.com>
2023-01-30 10:40:57 +0100

    Issue#475 Review suggestions

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoVerifyNoInteractionsUsage.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoVerifyNoInteractionsUsage.java
@@ -3,6 +3,7 @@ package tech.picnic.errorprone.bugpatterns;
 import static com.google.errorprone.BugPattern.LinkType.CUSTOM;
 import static com.google.errorprone.BugPattern.SeverityLevel.SUGGESTION;
 import static com.google.errorprone.BugPattern.StandardTags.SIMPLIFICATION;
+import static com.google.errorprone.matchers.Matchers.staticMethod;
 import static java.util.stream.Collectors.joining;
 import static tech.picnic.errorprone.bugpatterns.util.Documentation.BUG_PATTERNS_BASE_URL;
 import static tech.picnic.errorprone.bugpatterns.util.MoreJUnitMatchers.TEST_METHOD;
@@ -15,14 +16,16 @@ import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
 import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.util.TreeScanner;
-import com.sun.tools.javac.code.Symbol;
 import java.util.List;
 import org.jspecify.annotations.Nullable;
 import org.mockito.Mockito;
+import tech.picnic.errorprone.bugpatterns.util.SourceCode;
 
 /**
  * A {@link BugChecker} that flags multiple usages of {@link Mockito#verifyNoInteractions} in favor
@@ -42,14 +45,16 @@ import org.mockito.Mockito;
 public final class MockitoVerifyNoInteractionsUsage extends BugChecker
     implements MethodTreeMatcher {
   private static final long serialVersionUID = 1L;
+  private static final Matcher<ExpressionTree> VERIFY_NO_INTERACTIONS =
+      staticMethod().onClass("org.mockito.Mockito").named("verifyNoInteractions");
 
   @Override
   public Description matchMethod(MethodTree tree, VisitorState state) {
-    boolean isTestMethod = TEST_METHOD.matches(tree, state);
-    if (!isTestMethod) {
+    if (!TEST_METHOD.matches(tree, state)) {
       return Description.NO_MATCH;
     }
-    var verifyNoInteractionsInvocations = getVerifyNoInteractionsInvocations(tree);
+    ImmutableList<MethodInvocationTree> verifyNoInteractionsInvocations =
+        getVerifyNoInteractionsInvocations(tree, state);
     if (verifyNoInteractionsInvocations.size() < 2) {
       return Description.NO_MATCH;
     }
@@ -72,21 +77,26 @@ public final class MockitoVerifyNoInteractionsUsage extends BugChecker
                 "");
           }
         });
-    fixBuilder.replace(lastInvocation, "verifyNoInteractions(" + combinedArgument + ")");
+
+    String callAsString = SourceCode.treeToString(lastInvocation, state);
+    fixBuilder.replace(
+        lastInvocation,
+        callAsString.startsWith("Mockito.")
+            ? "Mockito.verifyNoInteractions(" + combinedArgument + ")"
+            : "verifyNoInteractions(" + combinedArgument + ")");
 
     return describeMatch(tree, fixBuilder.build());
   }
 
   private static ImmutableList<MethodInvocationTree> getVerifyNoInteractionsInvocations(
-      MethodTree methodTree) {
+      MethodTree methodTree, VisitorState state) {
     ImmutableList.Builder<MethodInvocationTree> invocationTreeBuilder = ImmutableList.builder();
 
     new TreeScanner<@Nullable Void, @Nullable Void>() {
       @Override
       public @Nullable Void visitMethodInvocation(
           MethodInvocationTree node, @Nullable Void unused) {
-        Symbol symbol = ASTHelpers.getSymbol(node);
-        if (symbol.getSimpleName().toString().equals("verifyNoInteractions")) {
+        if (VERIFY_NO_INTERACTIONS.matches(node, state)) {
           invocationTreeBuilder.add(node);
         }
         return super.visitMethodInvocation(node, unused);
