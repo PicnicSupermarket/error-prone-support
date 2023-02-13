ff60c7be2578427f72d04e48aee997aebb1c434c
Gijs de Jong <berichtaangijs@gmail.com>
2023-02-13 11:35:51 +0100

    use jspecify `Nullable`

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/testngtojunit/migrators/DataProviderMigrator.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/testngtojunit/migrators/DataProviderMigrator.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import org.jspecify.nullness.Nullable;
+import org.jspecify.annotations.Nullable;
 import tech.picnic.errorprone.bugpatterns.testngtojunit.Migrator;
 import tech.picnic.errorprone.bugpatterns.util.SourceCode;
 
