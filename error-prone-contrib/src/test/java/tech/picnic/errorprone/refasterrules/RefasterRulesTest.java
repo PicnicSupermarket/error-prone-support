56ed83bf49d30438679896bd991c0060309d81c4
Max Sumrall <max.sumrall@teampicnic.com>
2023-02-13 09:56:28 +0100

    XXX-XXX Prefer jOOQs fetch().stream() over directly .stream()'ing

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/refasterrules/RefasterRulesTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/refasterrules/RefasterRulesTest.java
@@ -49,6 +49,7 @@ final class RefasterRulesTest {
           ImmutableSortedMultisetRules.class,
           ImmutableSortedSetRules.class,
           IntStreamRules.class,
+          JooqRules.class,
           JUnitRules.class,
           JUnitToAssertJRules.class,
           LongStreamRules.class,
---
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/JooqRulesTestInput.java
@@ -0,0 +1,11 @@
+package tech.picnic.errorprone.refasterrules;
+
+import java.util.stream.Stream;
+import org.jooq.impl.DSL;
+import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
+
+final class JooqRulesTest implements RefasterRuleCollectionTestCase {
+  Stream<?> testResultQueryFetchStream() {
+    return DSL.select().stream();
+  }
+}
