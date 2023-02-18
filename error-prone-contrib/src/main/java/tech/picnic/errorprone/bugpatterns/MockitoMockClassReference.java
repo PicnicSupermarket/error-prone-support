2c1d096b150db2b55198be6007f277ec9dc87ff2
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-02-18 21:50:08 +0100

    Suggestions

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoMockClassReference.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoMockClassReference.java
@@ -10,10 +10,6 @@ import static com.google.errorprone.matchers.Matchers.isSameType;
 import static com.google.errorprone.matchers.Matchers.isVariable;
 import static com.google.errorprone.matchers.Matchers.not;
 import static com.google.errorprone.matchers.Matchers.staticMethod;
-import static com.sun.source.tree.Tree.Kind.ASSIGNMENT;
-import static com.sun.source.tree.Tree.Kind.RETURN;
-import static com.sun.source.tree.Tree.Kind.VARIABLE;
-import static java.util.Objects.requireNonNull;
 import static tech.picnic.errorprone.bugpatterns.util.Documentation.BUG_PATTERNS_BASE_URL;
 
 import com.google.auto.service.AutoService;
@@ -27,11 +23,11 @@ import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.AssignmentTree;
+import com.sun.source.tree.LambdaExpressionTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.ReturnTree;
 import com.sun.source.tree.Tree;
-import com.sun.source.tree.Tree.Kind;
 import com.sun.source.tree.VariableTree;
 
 /**
@@ -65,29 +61,32 @@ public final class MockitoMockClassReference extends BugChecker
       return Description.NO_MATCH;
     }
 
+    return isReturnTypeDerivableFromContext(state)
+        ? describeMatch(tree, SuggestedFix.delete(Iterables.getOnlyElement(tree.getArguments())))
+        : Description.NO_MATCH;
+  }
+
+  private static boolean isReturnTypeDerivableFromContext(VisitorState state) {
     Tree parent = state.getPath().getParentPath().getLeaf();
-    Kind parentKind = parent.getKind();
-    if (parentKind != VARIABLE && parentKind != ASSIGNMENT && parentKind != RETURN) {
-      return Description.NO_MATCH;
+    switch (parent.getKind()) {
+      case VARIABLE:
+        return !ASTHelpers.hasNoExplicitType((VariableTree) parent, state)
+            && areSameType(parent, ((VariableTree) parent).getInitializer(), state);
+      case ASSIGNMENT:
+        return areSameType(parent, ((AssignmentTree) parent).getExpression(), state);
+      case RETURN:
+        Tree context = state.findEnclosing(LambdaExpressionTree.class, MethodTree.class);
+        return context instanceof MethodTree
+            && areSameType(
+                ((MethodTree) context).getReturnType(),
+                ((ReturnTree) parent).getExpression(),
+                state);
+      default:
+        return false;
     }
-
-    return isParentIncompatible(parent, state)
-        ? Description.NO_MATCH
-        : describeMatch(tree, SuggestedFix.delete(Iterables.getOnlyElement(tree.getArguments())));
   }
 
-  private static boolean isParentIncompatible(Tree parent, VisitorState state) {
-    if (parent instanceof VariableTree) {
-      return ASTHelpers.hasNoExplicitType((VariableTree) parent, state)
-          || hasTypeDifference(parent, ((VariableTree) parent).getInitializer(), state);
-    } else if (parent instanceof AssignmentTree) {
-      return hasTypeDifference(parent, ((AssignmentTree) parent).getExpression(), state);
-    }
-    Tree returnTypeTree = requireNonNull(state.findEnclosing(MethodTree.class)).getReturnType();
-    return hasTypeDifference(returnTypeTree, ((ReturnTree) parent).getExpression(), state);
-  }
-
-  private static boolean hasTypeDifference(Tree treeA, Tree treeB, VisitorState state) {
-    return !ASTHelpers.isSameType(ASTHelpers.getType(treeA), ASTHelpers.getType(treeB), state);
+  private static boolean areSameType(Tree treeA, Tree treeB, VisitorState state) {
+    return ASTHelpers.isSameType(ASTHelpers.getType(treeA), ASTHelpers.getType(treeB), state);
   }
 }
