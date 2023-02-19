fd70908075777fb438948bb482dd3aa4630687af
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-02-19 15:05:28 +0100

    Suggestions (2)

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoMockClassReference.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MockitoMockClassReference.java
@@ -5,7 +5,6 @@ import static com.google.errorprone.BugPattern.SeverityLevel.SUGGESTION;
 import static com.google.errorprone.BugPattern.StandardTags.SIMPLIFICATION;
 import static com.google.errorprone.matchers.Matchers.allOf;
 import static com.google.errorprone.matchers.Matchers.argument;
-import static com.google.errorprone.matchers.Matchers.argumentCount;
 import static com.google.errorprone.matchers.Matchers.isSameType;
 import static com.google.errorprone.matchers.Matchers.isVariable;
 import static com.google.errorprone.matchers.Matchers.not;
@@ -13,32 +12,34 @@ import static com.google.errorprone.matchers.Matchers.staticMethod;
 import static tech.picnic.errorprone.bugpatterns.util.Documentation.BUG_PATTERNS_BASE_URL;
 
 import com.google.auto.service.AutoService;
-import com.google.common.collect.Iterables;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
-import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.fixes.SuggestedFixes;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
-import com.sun.source.tree.AssignmentTree;
+import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.LambdaExpressionTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.ReturnTree;
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.VariableTree;
+import java.util.List;
 
 /**
  * A {@link BugChecker} that flags the use of {@link org.mockito.Mockito#mock(Class)} and {@link
- * org.mockito.Mockito#spy(Class)} where instead the type could be derived from the variable or
- * field with an explicit type.
+ * org.mockito.Mockito#spy(Class)} where instead the type to be mocked or spied can be derived from
+ * context.
  */
+// XXX: This check currently does not flag method invocation arguments. When adding support for
+// this, consider that in some cases the type to be mocked or spied must be specified explicitly so
+// as to disambiguate between method overloads.
+// XXX: This check currently does not flag (implicit or explicit) lambda return expressions.
 @AutoService(BugChecker.class)
 @BugPattern(
-    summary =
-        "Don't unnecessarily pass the type reference to Mockito's `mock(Class)` and spy(Class)`",
+    summary = "Don't unnecessarily pass a type to Mockito's `mock(Class)` and `spy(Class)` methods",
     link = BUG_PATTERNS_BASE_URL + "MockitoMockClassReference",
     linkType = CUSTOM,
     severity = SUGGESTION,
@@ -48,7 +49,6 @@ public final class MockitoMockClassReference extends BugChecker
   private static final long serialVersionUID = 1L;
   private static final Matcher<MethodInvocationTree> MOCKITO_MOCK_OR_SPY =
       allOf(
-          argumentCount(1),
           argument(0, allOf(isSameType(Class.class.getName()), not(isVariable()))),
           staticMethod().onClass("org.mockito.Mockito").namedAnyOf("mock", "spy"));
 
@@ -57,30 +57,26 @@ public final class MockitoMockClassReference extends BugChecker
 
   @Override
   public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
-    if (!MOCKITO_MOCK_OR_SPY.matches(tree, state)) {
+    if (!MOCKITO_MOCK_OR_SPY.matches(tree, state) || !isTypeDerivableFromContext(tree, state)) {
       return Description.NO_MATCH;
     }
 
-    return isReturnTypeDerivableFromContext(state)
-        ? describeMatch(tree, SuggestedFix.delete(Iterables.getOnlyElement(tree.getArguments())))
-        : Description.NO_MATCH;
+    List<? extends ExpressionTree> arguments = tree.getArguments();
+    return describeMatch(tree, SuggestedFixes.removeElement(arguments.get(0), arguments, state));
   }
 
-  private static boolean isReturnTypeDerivableFromContext(VisitorState state) {
+  private static boolean isTypeDerivableFromContext(MethodInvocationTree tree, VisitorState state) {
     Tree parent = state.getPath().getParentPath().getLeaf();
     switch (parent.getKind()) {
       case VARIABLE:
         return !ASTHelpers.hasNoExplicitType((VariableTree) parent, state)
-            && areSameType(parent, ((VariableTree) parent).getInitializer(), state);
+            && areSameType(tree, parent, state);
       case ASSIGNMENT:
-        return areSameType(parent, ((AssignmentTree) parent).getExpression(), state);
+        return areSameType(tree, parent, state);
       case RETURN:
         Tree context = state.findEnclosing(LambdaExpressionTree.class, MethodTree.class);
         return context instanceof MethodTree
-            && areSameType(
-                ((MethodTree) context).getReturnType(),
-                ((ReturnTree) parent).getExpression(),
-                state);
+            && areSameType(tree, ((MethodTree) context).getReturnType(), state);
       default:
         return false;
     }
