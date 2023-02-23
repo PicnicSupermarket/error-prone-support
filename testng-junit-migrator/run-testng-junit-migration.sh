5548d2510c6f12da9d83fb8a9068a19aa0775fce
Gijs de Jong <berichtaangijs@gmail.com>
2023-02-23 15:54:43 +0100

    Initial migration script

---
+++ testng-junit-migrator/run-testng-junit-migration.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# If this is not a Maven build, exit here to skip Maven-specific steps.
+if [ ! -f pom.xml ]; then
+  exit 0
+fi
+
+# Use the Maven Wrapper executable if present.
+MVN='mvn'
+if [ -x ./mvnw ]; then
+  MVN='./mvnw'
+fi
+
+"${MVN}" \
+  -T 1.0C \
+  -Perror-prone \
+  -Ppatch \
+  clean test-compile fmt:format \
+  -Derror-prone.patch-checks="TestNGJUnitMigration" \
+  -Dfrontend.skip \
+  -Dverification.skip
