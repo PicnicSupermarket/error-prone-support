23c51d1a81bd1f5b74ca5b67dc3806dd9906e67f
Bastien Diederichs <bastien.diederichs@teampicnic.com>
2023-03-10 10:31:31 +0100

    Clean doc

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/BugCheckerRules.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/BugCheckerRules.java
@@ -1,19 +1,20 @@
 package tech.picnic.errorprone.refasterrules;
 
 import com.google.errorprone.BugCheckerRefactoringTestHelper;
+import com.google.errorprone.BugCheckerRefactoringTestHelper.FixChooser;
 import com.google.errorprone.BugCheckerRefactoringTestHelper.FixChoosers;
 import com.google.errorprone.refaster.annotation.AfterTemplate;
 import com.google.errorprone.refaster.annotation.BeforeTemplate;
 import tech.picnic.errorprone.refaster.annotation.OnlineDocumentation;
 
+/** Refaster rules related to BugChecker classes. */
 @OnlineDocumentation
 final class BugCheckerRules {
   private BugCheckerRules() {}
 
   /**
-   * Drop {@link
-   * BugCheckerRefactoringTestHelper#setFixChooser(BugCheckerRefactoringTestHelper.FixChooser)} when
-   * set to the default {@link FixChoosers#FIRST}.
+   * Drop {@link BugCheckerRefactoringTestHelper#setFixChooser(FixChooser)} when set to the default
+   * {@link FixChoosers#FIRST}.
    */
   static final class SetFixChooserDefault {
     @BeforeTemplate
