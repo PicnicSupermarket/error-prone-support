fc064c5955a92ce284860371bc97da2822effe6d
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-03-12 11:46:51 +0100

    Suggestions

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/BugCheckerRulesTestInput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/BugCheckerRulesTestInput.java
@@ -3,7 +3,7 @@ package tech.picnic.errorprone.refasterrules;
 import com.google.common.collect.ImmutableSet;
 import com.google.errorprone.BugCheckerRefactoringTestHelper;
 import com.google.errorprone.BugCheckerRefactoringTestHelper.FixChoosers;
-import tech.picnic.errorprone.bugpatterns.StringCaseLocaleUsage;
+import com.google.errorprone.bugpatterns.BugChecker;
 import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
 
 final class BugCheckerRulesTest implements RefasterRuleCollectionTestCase {
@@ -12,8 +12,18 @@ final class BugCheckerRulesTest implements RefasterRuleCollectionTestCase {
     return ImmutableSet.of(FixChoosers.class);
   }
 
-  BugCheckerRefactoringTestHelper testSetFixChooserDefault() {
-    return BugCheckerRefactoringTestHelper.newInstance(StringCaseLocaleUsage.class, getClass())
-        .setFixChooser(FixChoosers.FIRST);
+  ImmutableSet<BugCheckerRefactoringTestHelper> testBugCheckerRefactoringTestHelperIdentity() {
+    return ImmutableSet.of(
+        BugCheckerRefactoringTestHelper.newInstance(BugChecker.class, getClass())
+            .setFixChooser(FixChoosers.FIRST),
+        BugCheckerRefactoringTestHelper.newInstance(BugChecker.class, getClass())
+            .setImportOrder("static-first"));
+  }
+
+  BugCheckerRefactoringTestHelper
+      testBugCheckerRefactoringTestHelperAddInputLinesExpectUnchanged() {
+    return BugCheckerRefactoringTestHelper.newInstance(BugChecker.class, getClass())
+        .addInputLines("A.java", "class A {}")
+        .addOutputLines("A.java", "class A {}");
   }
 }
