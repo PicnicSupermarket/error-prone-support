9ab79cb69b5c1d6b43726a245153cc6538931a2b
Gijs de Jong <berichtaangijs@gmail.com>
2023-03-13 09:28:19 +0100

    Introduce `EnabledArgumentMigrator`

---
+++ testng-junit-migrator/src/main/java/tech/picnic/errorprone/testngjunit/EnabledArgumentMigrator.java
@@ -0,0 +1,39 @@
+package tech.picnic.errorprone.testngjunit;
+
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.annotations.Immutable;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.LiteralTree;
+import com.sun.source.tree.MethodTree;
+import java.util.Optional;
+
+/**
+ * A {@link tech.picnic.errorprone.testngjunit.Migrator} that migrates the {@code enabled} argument.
+ */
+@Immutable
+public class EnabledArgumentMigrator implements Migrator {
+  @Override
+  public Optional<SuggestedFix> createFix(
+      ClassTree classTree, MethodTree methodTree, ExpressionTree dataValue, VisitorState state) {
+    if ((boolean) ((LiteralTree) dataValue).getValue()) {
+      return Optional.of(SuggestedFix.builder().build());
+    } else {
+      return Optional.of(
+          SuggestedFix.builder()
+              .addImport("org.junit.jupiter.api.Disabled")
+              .merge(SuggestedFix.prefixWith(methodTree, "@Disabled\n"))
+              .build());
+    }
+  }
+
+  @Override
+  public boolean canFix(
+      TestNGMetadata metadata,
+      TestNGMetadata.AnnotationMetadata annotation,
+      MethodTree methodTree,
+      VisitorState state) {
+    return annotation.getArguments().containsKey("enabled");
+  }
+}
