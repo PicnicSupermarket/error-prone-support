64f9d6b7a23876acb5e7de23824d89d100394bd4
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-04-05 11:25:03 +0200

    Introduce `SuggestedFixRules` Refaster rule collection (#559)

---
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/SuggestedFixRulesTestOutput.java
@@ -0,0 +1,36 @@
+package tech.picnic.errorprone.refasterrules;
+
+import com.google.errorprone.fixes.SuggestedFix;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.Tree;
+import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
+
+final class SuggestFixRulesTest implements RefasterRuleCollectionTestCase {
+  SuggestedFix testSuggestedFixDelete() {
+    return SuggestedFix.delete(null);
+  }
+
+  SuggestedFix testSuggestedFixReplaceTree() {
+    return Suggestions.replace(null, "foo");
+  }
+
+  SuggestedFix testSuggestedFixReplaceStartEnd() {
+    return Suggestions.replace(1, 2, "foo");
+  }
+
+  SuggestedFix testSuggestedFixReplaceTreeStartEnd() {
+    return Suggestions.replace(null, "foo", 1, 2);
+  }
+
+  SuggestedFix testSuggestedFixSwap() {
+    return Suggestions.swap((Tree) null, (ExpressionTree) null);
+  }
+
+  SuggestedFix testSuggestedFixPrefixWith() {
+    return Suggestions.prefixWith(null, "foo");
+  }
+
+  SuggestedFix testSuggestedFixPostfixWith() {
+    return Suggestions.postfixWith(null, "foo");
+  }
+}
