9652c6f990b205cfd52ab69195640ae01ae5d968
Gijs de Jong <berichtaangijs@gmail.com>
2023-04-13 12:33:09 +0200

    Add macOs support to migrator script

--- testng-junit-migrator/run-testng-junit-migration.sh
+++ testng-junit-migrator/run-testng-junit-migration.sh
@@ -9,91 +9,104 @@ if [ ! -f pom.xml ]; then
 fi
 
 function insert_dependency() {
-    groupId=${1:?groupId not specified or empty}
-    artifactId=${2:?artifactId not specified or empty}
-    classifier=${3?classifier not specified}
-    scope=${4?scope not specified}
-    pomFile=${5:-pom.xml}
+  groupId=${1:?groupId not specified or empty}
+  artifactId=${2:?artifactId not specified or empty}
+  classifier=${3?classifier not specified}
+  scope=${4?scope not specified}
+  pomFile=${5:-pom.xml}
 
-    # If the dependency declaration is already present (irrespective of scope),
-    # then we don't modify the file.
-    xmlstarlet sel -T -N 'x=http://maven.apache.org/POM/4.0.0' \
-        -t -m "/x:project/x:dependencies/x:dependency[
+  # If the dependency declaration is already present (irrespective of scope),
+  # then we don't modify the file.
+  xmlstarlet sel -T -N 'x=http://maven.apache.org/POM/4.0.0' \
+    -t -m "/x:project/x:dependencies/x:dependency[
                 x:groupId/text() = '${groupId}' and
                 x:artifactId/text() = '${artifactId}' and
                 (x:classifier/text() = '${classifier}' or '${classifier}' = '')
             ]" -nl "${pomFile}" && return 0
 
-    # Determine the index at which to insert the dependency declaration.
-    insertionIndex="$(
-        (xmlstarlet sel -T -N 'x=http://maven.apache.org/POM/4.0.0' \
-            -t -m '/x:project/x:dependencies/x:dependency' \
-            -v 'concat(x:groupId, " : ", x:artifactId, " : ", x:classifier, " : ", x:scope)' -nl \
-            "${pomFile}" || true) |
-            awk "\$0 < \"${groupId} : ${artifactId} : ${classifier} : ${scope}\"" |
-            wc -l
-    )"
+  # Determine the index at which to insert the dependency declaration.
+  insertionIndex="$(
+    (xmlstarlet sel -T -N 'x=http://maven.apache.org/POM/4.0.0' \
+      -t -m '/x:project/x:dependencies/x:dependency' \
+      -v 'concat(x:groupId, " : ", x:artifactId, " : ", x:classifier, " : ", x:scope)' -nl \
+      "${pomFile}" || true) |
+      awk "\$0 < \"${groupId} : ${artifactId} : ${classifier} : ${scope}\"" |
+      wc -l
+  )"
 
-    # Generate a placeholder that will be inserted at the place where the new
-    # dependency declaration should reside. We need to jump through this hoop
-    # because `xmlstarlet` does not support insertion of complex XML
-    # sub-documents.
-    placeholder="$(head -c 30 /dev/urandom | base64 | sed 's,[^a-zA-Z0-9],,g')"
+  # Generate a placeholder that will be inserted at the place where the new
+  # dependency declaration should reside. We need to jump through this hoop
+  # because `xmlstarlet` does not support insertion of complex XML
+  # sub-documents.
+  placeholder="$(head -c 30 /dev/urandom | base64 | sed 's,[^a-zA-Z0-9],,g')"
 
-    # Insert the placeholder. (Note that only one case will match.)
-    xmlstarlet ed -L -P -N 'x=http://maven.apache.org/POM/4.0.0' \
-        -s "/x:project[not(x:dependencies)]" \
-        -t elem -n dependencies -v "${placeholder}" \
-        -i "/x:project/x:dependencies/x:dependency[${insertionIndex} = 0 and position() = 1]" \
-        -t text -n placeholder -v "${placeholder}" \
-        -a "/x:project/x:dependencies/x:dependency[${insertionIndex}]" \
-        -t text -n placeholder -v "${placeholder}" \
-        "${pomFile}"
+  # Insert the placeholder. (Note that only one case will match.)
+  xmlstarlet ed -L -P -N 'x=http://maven.apache.org/POM/4.0.0' \
+    -s "/x:project[not(x:dependencies)]" \
+    -t elem -n dependencies -v "${placeholder}" \
+    -i "/x:project/x:dependencies/x:dependency[${insertionIndex} = 0 and position() = 1]" \
+    -t text -n placeholder -v "${placeholder}" \
+    -a "/x:project/x:dependencies/x:dependency[${insertionIndex}]" \
+    -t text -n placeholder -v "${placeholder}" \
+    "${pomFile}"
 
-    # Generate the XML subdocument we _actually_ want to insert.
-    decl="$(echo "
+  # Generate the XML subdocument we _actually_ want to insert.
+  decl="$(echo "
             <dependency>
                 <groupId>${groupId}</groupId>
                 <artifactId>${artifactId}</artifactId>
                 <classifier>${classifier}</classifier>
                 <scope>${scope}</scope>
             </dependency>" |
-        sed '/></d' |
-        sed ':a;N;$!ba;s/\n/\\n/g')"
+    sed '/></d' |
+    sed ':a;N;$!ba;s/\n/\\n/g')"
 
-    # Replace the placeholder with the actual dependency declaration.
-    sed -i "s,${placeholder},${decl}," "${pomFile}"
+  # Replace the placeholder with the actual dependency declaration.
+  sed -i "s,${placeholder},${decl}," "${pomFile}"
 }
 
 if [[ -n "${1-}" ]] && [[ "${1}" == "--count" ]]; then
   echo "Counting number of tests..."
-  test_results=$(mvn test | grep -n "Results:" -A 3 | grep -oP "Tests run: \K\d+(?=,)" | awk '{s+=$1} END {print s}')
+  test_results=$(mvn test | grep_command -n "Results:" -A 3 | grep_command -oP "Tests run: \K\d+(?=,)" | awk '{s+=$1} END {print s}')
   echo "Number of tests run: $test_results"
   exit
 fi
 
 function handle_file() {
-    module=${1:?module not specified or empty}
-    groupId=${2:?groupId not specified or empty}
-    artifactId=${3:?artifactId not specified or empty}
-    classifier=${4?classifier not specified}
-    scope=${5?scope not specified}
-    pomFile=${6:-pom.xml}
+  module=${1:?module not specified or empty}
+  groupId=${2:?groupId not specified or empty}
+  artifactId=${3:?artifactId not specified or empty}
+  classifier=${4?classifier not specified}
+  scope=${5?scope not specified}
+  pomFile=${6:-pom.xml}
 
-    if [[ -d $module ]] && [[ -f "$module/pom.xml" ]]; then
-      cd "$module"
-      insert_dependency "$groupId" "$artifactId" "$classifier" "$scope"
-      echo "[$module] Added $groupId:$artifactId"
-      cd -
-    fi
+  if [[ -d $module ]] && [[ -f "$module/pom.xml" ]]; then
+    cd "$module"
+    insert_dependency "$groupId" "$artifactId" "$classifier" "$scope"
+    echo "[$module] Added $groupId:$artifactId"
+    cd -
+  fi
 }
+
+case "$(uname -s)" in
+Linux*)
+  grep_command="grep"
+  ;;
+Darwin*)
+  grep_command="ggrep"
+  ;;
+*)
+  echo "Unsupported distribution $(uname -s) for this script."
+  exit 1
+  ;;
+esac
 
 echo "Migrating to JUnit 5..."
 echo "Adding required dependencies..."
 
-if grep -q "<packaging>pom</packaging>" "pom.xml"; then
+if grep_command -q "<packaging>pom</packaging>" "pom.xml"; then
 
-  for module in $(grep -rl "org.testng.annotations.Test" $(pwd) | awk -F "$(pwd)" '{print $2}' | awk -F '/' '{print $2}' | uniq); do
+  for module in $(grep_command -rl "org.testng.annotations.Test" $(pwd) | awk -F "$(pwd)" '{print $2}' | awk -F '/' '{print $2}' | uniq); do
     (
       handle_file "$module" "org.junit.jupiter" "junit-jupiter-api" "" "test"
     )
@@ -101,18 +114,18 @@ if grep -q "<packaging>pom</packaging>" "pom.xml"; then
       handle_file "$module" "org.junit.jupiter" "junit-jupiter-engine" "" "test"
     )
   done
-  for module in $(grep -rl "org.testng.annotations.DataProvider" $(pwd) | awk -F "$(pwd)" '{print $2}' | awk -F '/' '{print $2}' | uniq); do
+  for module in $(grep_command -rl "org.testng.annotations.DataProvider" $(pwd) | awk -F "$(pwd)" '{print $2}' | awk -F '/' '{print $2}' | uniq); do
     (
       handle_file "$module" "org.junit.jupiter" "junit-jupiter-params" "" "test"
     )
   done
 else
-  if grep -rq "org.testng.annotations.Test" "src/"; then
+  if grep_command -rq "org.testng.annotations.Test" "src/"; then
     handle_file "./" "org.junit.jupiter" "junit-jupiter-api" "" "test"
     handle_file "./" "org.junit.jupiter" "junit-jupiter-engine" "" "test"
   fi
-  if grep -rq "org.testng.annotations.DataProvider" "src/"; then
-      handle_file "./" "org.junit.jupiter" "junit-jupiter-params" "" "test"
+  if grep_command -rq "org.testng.annotations.DataProvider" "src/"; then
+    handle_file "./" "org.junit.jupiter" "junit-jupiter-params" "" "test"
   fi
 fi
 echo "Running migration..."
