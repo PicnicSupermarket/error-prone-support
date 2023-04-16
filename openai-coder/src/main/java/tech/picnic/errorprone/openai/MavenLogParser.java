098e07dc3b9094a93bff93cd5526317cb943e8eb
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-04-16 21:12:10 +0200

    X

--- openai-coder/src/main/java/tech/picnic/errorprone/openai/MavenLogParser.java
+++ openai-coder/src/main/java/tech/picnic/errorprone/openai/MavenLogParser.java
@@ -1,8 +1,14 @@
 package tech.picnic.errorprone.openai;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileVisitResult;
@@ -14,14 +20,60 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.annotation.WillClose;
 
 final class MavenLogParser {
+  private static final Pattern LOG_LINE_START_MARKER = Pattern.compile("^\\[([A-Z]+)\\] ");
+  private static final ImmutableSet<String> ISSUE_LOG_LEVELS = ImmutableSet.of("ERROR", "WARNING");
+
   private final FileSystem fileSystem;
   private final Path projectRoot;
 
   MavenLogParser(FileSystem fileSystem, Path projectRoot) {
     this.fileSystem = fileSystem;
     this.projectRoot = projectRoot.toAbsolutePath();
+  }
+
+  static List<String> extractIssues(@WillClose InputStream logs) throws IOException {
+    List<String> messages = new ArrayList<>();
+
+    boolean shouldRead = false;
+    StringBuilder nextMessage = new StringBuilder();
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(logs, UTF_8))) {
+      for (String line = br.readLine(); line != null; line = br.readLine()) {
+        Optional<String> logLevel = getLogLevel(line);
+
+        if (logLevel.isPresent()) {
+          if (!nextMessage.isEmpty()) {
+            messages.add(nextMessage.toString());
+            nextMessage.setLength(0);
+          }
+
+          shouldRead = ISSUE_LOG_LEVELS.contains(logLevel.orElseThrow());
+        }
+
+        if (shouldRead) {
+          if (!nextMessage.isEmpty()) {
+            nextMessage.append(System.lineSeparator());
+          }
+          nextMessage.append(line);
+        }
+      }
+    }
+
+    if (shouldRead && !nextMessage.isEmpty()) {
+      messages.add(nextMessage.toString());
+    }
+
+    return messages;
+  }
+
+  private static Optional<String> getLogLevel(String logLine) {
+    return Optional.of(LOG_LINE_START_MARKER.matcher(logLine))
+        .filter(Matcher::find)
+        .map(m -> m.group(1));
   }
 
   // XXX: Validate `@VisibleForTesting`.
--- openai-coder/src/test/java/tech/picnic/errorprone/openai/MavenLogParserTest.java
+++ openai-coder/src/test/java/tech/picnic/errorprone/openai/MavenLogParserTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
@@ -19,6 +20,72 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 final class MavenLogParserTest {
   private static final String WORK_DIRECTORY = "/project-root";
+
+  // XXX: Review whether we want these as fields or local variables.
+  private final FileSystem fileSystem =
+      Jimfs.newFileSystem(
+          Configuration.unix().toBuilder().setWorkingDirectory(WORK_DIRECTORY).build());
+  private final MavenLogParser parser =
+      new MavenLogParser(fileSystem, fileSystem.getPath(WORK_DIRECTORY));
+
+  private static Stream<Arguments> extractIssuesTestCases() {
+    return Stream.of(
+        arguments("""
+            Line without log level
+            """, ImmutableSet.of()),
+        arguments("""
+            [INFO] Line with log level
+            """, ImmutableSet.of()),
+        arguments(
+            """
+            [WARNING] Line with warning
+            """,
+            ImmutableSet.of("[WARNING] Line with warning")),
+        arguments(
+            """
+            [ERROR] Line with error
+            """,
+            ImmutableSet.of("[ERROR] Line with error")),
+        arguments(
+            """
+            [INFO] Info 1
+            [INFO] Info 2
+            [WARNING] Warning 1
+            [ERROR] Error 1
+            [INFO] Info 3
+            """,
+            ImmutableSet.of("[WARNING] Warning 1", "[ERROR] Error 1")),
+        arguments(
+            """
+            [INFO] Info 1
+            Info line 1, continued
+            [INFO] Info 2
+            [WARNING] Warning 1
+            Warning line 1, continued
+            [ERROR] Error 1
+            Error line 1, continued
+            Error line 1, continued still
+            [INFO] Info 3
+            [WARNING] Warning 2
+            """,
+            ImmutableSet.of(
+                """
+                [WARNING] Warning 1
+                Warning line 1, continued""",
+                """
+                [ERROR] Error 1
+                Error line 1, continued
+                Error line 1, continued still""",
+                """
+                [WARNING] Warning 2""")));
+  }
+
+  @ParameterizedTest
+  @MethodSource("extractIssuesTestCases")
+  void extractIssues(String logs, ImmutableSet<String> expectedIssues) throws IOException {
+    assertThat(MavenLogParser.extractIssues(new ByteArrayInputStream(logs.getBytes(UTF_8))))
+        .containsExactlyElementsOf(expectedIssues);
+  }
 
   private static Stream<Arguments> findPathTestCases() {
     return Stream.of(
@@ -45,15 +112,10 @@ final class MavenLogParserTest {
   @ParameterizedTest
   void findPath(ImmutableSet<String> existingPaths, String pathSuffix, Optional<String> expected)
       throws IOException {
-    FileSystem fs =
-        Jimfs.newFileSystem(
-            Configuration.unix().toBuilder().setWorkingDirectory(WORK_DIRECTORY).build());
-
     for (String existingPath : existingPaths) {
-      createEmptyFile(fs.getPath(existingPath));
+      createEmptyFile(fileSystem.getPath(existingPath));
     }
 
-    MavenLogParser parser = new MavenLogParser(fs, fs.getPath(WORK_DIRECTORY));
     assertThat(parser.findPath(pathSuffix)).map(Path::toString).isEqualTo(expected);
   }
 
