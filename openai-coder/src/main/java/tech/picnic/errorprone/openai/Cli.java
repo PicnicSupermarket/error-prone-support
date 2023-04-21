89e49b118ee021a89e3e47bc9c712c560fed176b
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-04-21 22:11:22 +0200

    Cleanup

--- openai-coder/src/main/java/tech/picnic/errorprone/openai/Cli.java
+++ openai-coder/src/main/java/tech/picnic/errorprone/openai/Cli.java
@@ -1,22 +1,18 @@
 package tech.picnic.errorprone.openai;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.ImmutableSetMultimap;
+import com.google.common.collect.ImmutableSet;
 import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Locale;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
+import java.util.OptionalInt;
 import org.fusesource.jansi.AnsiConsole;
-import org.jline.builtins.ConfigurationPath;
 import org.jline.console.SystemRegistry;
-import org.jline.console.impl.Builtins;
 import org.jline.console.impl.SystemRegistryImpl;
 import org.jline.keymap.KeyMap;
 import org.jline.reader.EndOfFileException;
@@ -31,95 +27,64 @@ import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 import org.jline.widget.TailTipWidgets;
 import picocli.CommandLine;
-import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.HelpCommand;
-import picocli.CommandLine.Model.CommandSpec;
-import picocli.CommandLine.Model.OptionSpec;
-import picocli.CommandLine.Model.PositionalParamSpec;
-import picocli.CommandLine.Model.UsageMessageSpec;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.ParentCommand;
-import picocli.CommandLine.ParseResult;
+import picocli.CommandLine.Parameters;
 import picocli.shell.jline3.PicocliCommands;
 import picocli.shell.jline3.PicocliCommands.PicocliCommandsFactory;
 import tech.picnic.errorprone.openai.IssueExtractor.Issue;
 
 // XXX: Review whether to enable a *subset* of JLine's built-ins. See
 // https://github.com/remkop/picocli/tree/main/picocli-shell-jline3#jline-316-and-picocli-44-example.
+
 public final class Cli {
   public static void main(String... args) {
-    IssueResolutionController issueResolutionController =
-        new IssueResolutionController(ImmutableSetMultimap.of());
-
     AnsiConsole.systemInstall();
-    try {
-      CommandSpec issues = command("issues", 'i', "List issues.");
-      CommandSpec submit =
-          command("submit", 's', "Submit issues to OpenAI.")
-              .addPositional(
-                  PositionalParamSpec.builder()
-                      .paramLabel("ISSUES")
-                      .type(List.class)
-                      .auxiliaryTypes(Integer.class)
-                      .description("The subset of issues to submit (default: all)")
-                      .build());
-      CommandSpec apply = command("apply", 'a', "Apply the changes suggested by OpenAI");
-      CommandSpec next = command("next", 'n', "Move to the next issue.");
-      CommandSpec previous = command("previous", 'p', "Move to the previous issue.");
+    try (Terminal terminal = TerminalBuilder.terminal()) {
+      IssueResolutionController issueResolutionController =
+          new IssueResolutionController(
+              terminal.writer(),
+              ImmutableSet.of(
+                  new Issue<>(Path.of("xxx"), OptionalInt.of(1), OptionalInt.empty(), "msg"),
+                  new Issue<>(Path.of("yyy"), OptionalInt.of(2), OptionalInt.empty(), "msg"),
+                  new Issue<>(Path.of("xxx"), OptionalInt.of(3), OptionalInt.empty(), "msg")));
 
       PicocliCommandsFactory factory = new PicocliCommandsFactory();
-      // Or, if you have your own factory, you can chain them like this:
-      // MyCustomFactory customFactory = createCustomFactory(); // your application custom factory
-      // PicocliCommandsFactory factory = new PicocliCommandsFactory(customFactory); // chain the
-      // factories
+      factory.setTerminal(terminal);
 
-      CommandLine cmd =
-          new CommandLine(
-              CommandSpec.create()
-                  .name("")
-                  .addSubcommand(null, issues)
-                  .addSubcommand(null, submit)
-                  .addSubcommand(null, apply)
-                  .addSubcommand(null, next)
-                  .addSubcommand(null, previous),
-              factory);
-      // XXX: Rename to `commands` if we don't enable the built-ins.
-      PicocliCommands commands = new PicocliCommands(cmd);
+      CommandLine commandLine = new CommandLine(issueResolutionController, factory);
+      PicocliCommands commands = new PicocliCommands(commandLine);
 
       Parser parser = new DefaultParser();
-      // XXX: Check `TerminalBuilder.builder().build()` options.
-      try (Terminal terminal = TerminalBuilder.terminal()) {
-        SystemRegistry systemRegistry =
-            new SystemRegistryImpl(parser, terminal, () -> Path.of("").toAbsolutePath(), null);
-        systemRegistry.setCommandRegistries(commands);
-        systemRegistry.register("help", commands);
 
-        LineReader reader =
-            LineReaderBuilder.builder()
-                .terminal(terminal)
-                .completer(systemRegistry.completer())
-                .parser(parser)
-                .build();
-        cmd.setExecutionStrategy(
-            pr -> run(pr, reader.getTerminal().writer(), issueResolutionController));
-        factory.setTerminal(terminal);
-        new TailTipWidgets(
-                reader, systemRegistry::commandDescription, 5, TailTipWidgets.TipType.COMPLETER)
-            .enable();
-        reader.getKeyMaps().get("main").bind(new Reference("tailtip-toggle"), KeyMap.ctrl('t'));
+      SystemRegistry systemRegistry =
+          new SystemRegistryImpl(parser, terminal, () -> Path.of("").toAbsolutePath(), null);
+      systemRegistry.setCommandRegistries(commands);
+      systemRegistry.register("help", commands);
 
-        while (true) {
-          try {
-            systemRegistry.cleanUp();
-            systemRegistry.execute(reader.readLine("prompt> ", null, (MaskingCallback) null, null));
-          } catch (UserInterruptException e) {
-            // XXX: Review whether indeed to ignore this.
-          } catch (EndOfFileException e) {
-            return;
-          } catch (Exception e) {
-            systemRegistry.trace(e);
-          }
+      LineReader reader =
+          LineReaderBuilder.builder()
+              .terminal(terminal)
+              .completer(systemRegistry.completer())
+              .parser(parser)
+              .build();
+      new TailTipWidgets(
+              reader, systemRegistry::commandDescription, 5, TailTipWidgets.TipType.COMPLETER)
+          .enable();
+      reader.getKeyMaps().get("main").bind(new Reference("tailtip-toggle"), KeyMap.ctrl('t'));
+
+      while (true) {
+        try {
+          systemRegistry.cleanUp();
+          systemRegistry.execute(
+              reader.readLine(
+                  issueResolutionController.prompt(), null, (MaskingCallback) null, null));
+        } catch (UserInterruptException e) {
+          // XXX: Review whether indeed to ignore this.
+        } catch (EndOfFileException e) {
+          return;
+        } catch (Exception e) {
+          systemRegistry.trace(e);
         }
       }
     } catch (Throwable t) {
@@ -130,71 +95,99 @@ public final class Cli {
     }
   }
 
+  // XXX: Should we even support those `files.isEmpty()` cases?
+  // XXX: Allow issues to be dropped?
+  // XXX: Mark files modified/track modification count?
+  // XXX: List full diff over multiple rounds?
+  @Command(name = "")
   static final class IssueResolutionController {
-    private final ImmutableList<ImmutableList<Issue<Path>>> issuesGroupedByPath;
+    private final PrintWriter out;
+    private final ImmutableList<FileIssues> files;
     private int currentIndex = 0;
 
-    // XXX: Maybe shouldn't already group by path at call site?
-    IssueResolutionController(ImmutableSetMultimap<Path, Issue<Path>> issues) {
-      this.issuesGroupedByPath =
-          issues.asMap().values().stream().map(ImmutableList::copyOf).collect(toImmutableList());
+    IssueResolutionController(PrintWriter output, ImmutableSet<Issue<Path>> issues) {
+      this.out = output;
+      this.files =
+          issues.stream()
+              .collect(
+                  collectingAndThen(
+                      groupingBy(Issue::file),
+                      m ->
+                          m.entrySet().stream()
+                              .map(
+                                  e ->
+                                      new FileIssues(
+                                          e.getKey(), ImmutableList.copyOf(e.getValue())))
+                              .collect(toImmutableList())));
     }
 
-    void list(PrintWriter out) {
-      if (currentIndex >= issuesGroupedByPath.size()) {
-        out.println("No more issues.");
+    @Command(aliases = "i", subcommands = HelpCommand.class, description = "List issues.")
+    void issues() {
+      if (files.isEmpty()) {
+        out.println("No issues.");
         return;
       }
 
-      ImmutableList<Issue<Path>> issues = issuesGroupedByPath.get(currentIndex);
-      out.printf("Issues for %s:%n", issues.get(0).file());
+      FileIssues fileIssues = files.get(currentIndex);
+      out.printf("Issues for %s:%n", fileIssues.file());
+      ImmutableList<Issue<Path>> issues = fileIssues.issues();
       for (int i = 0; i < issues.size(); i++) {
-        out.printf(Locale.ROOT, "%02d. %s%n", i, issues.get(i).description());
+        out.printf(Locale.ROOT, "%2d. %s%n", i, issues.get(i).description());
       }
     }
 
-    void next(PrintWriter out) {
-      currentIndex++;
-      list(out);
+    @Command(
+        aliases = "s",
+        subcommands = HelpCommand.class,
+        description = "Submit issues to OpenAI.")
+    void submit(
+        @Parameters(arity = "0..*", description = "The subset of issues to submit (default: all)")
+            List<Integer> issues) {
+      // XXX: Validate indices.
+      // XXX: Implement.
     }
 
-    void previous(PrintWriter out) {
-      currentIndex--;
-      list(out);
-    }
-  }
-
-  static int run(ParseResult pr, PrintWriter out, IssueResolutionController controller) {
-    Integer helpExitCode = CommandLine.executeHelpRequest(pr);
-    if (helpExitCode != null) {
-      return helpExitCode;
+    @Command(
+        aliases = "a",
+        subcommands = HelpCommand.class,
+        description = "Apply the changes suggested by OpenAI")
+    void apply() {
+      // XXX: Implement.
     }
 
-    ParseResult subcommand = Objects.requireNonNull(pr.subcommand(), "subcommand");
-    switch (subcommand.commandSpec().name()) {
-      case "issues" -> controller.list(out);
-      case "submit" -> out.println("submit");
-      case "apply" -> out.println("apply");
-      case "next" -> controller.next(out);
-      case "previous" -> controller.previous(out);
-      default -> throw new IllegalStateException(
-          "Unknown command: " + subcommand.commandSpec().name());
+    @Command(
+        aliases = "n",
+        subcommands = HelpCommand.class,
+        description = "Move to the next issue.")
+    void next() {
+      if (currentIndex < files.size() - 1) {
+        currentIndex++;
+        issues();
+      } else {
+        out.println("No next issues.");
+      }
     }
-    return 0;
-  }
 
-  private static CommandSpec command(String name, char alias, String description) {
-    return CommandSpec.create()
-        .name(name)
-        .aliases(Character.toString(alias))
-        .usageMessage(new UsageMessageSpec().description(description))
-        .addOption(helpOption());
-  }
+    @Command(
+        aliases = "p",
+        subcommands = HelpCommand.class,
+        description = "Move to the previous issue.")
+    void previous() {
+      if (currentIndex > 0) {
+        currentIndex--;
+        issues();
+      } else {
+        out.println("No previous issues.");
+      }
+    }
 
-  private static OptionSpec helpOption() {
-    return OptionSpec.builder("-h", "--help")
-        .usageHelp(true)
-        .description("Show this help message and exit.")
-        .build();
+    String prompt() {
+      return files.isEmpty()
+          ? "No issues>"
+          : String.format(
+              "File %s (%s/%s)>", files.get(currentIndex).file(), currentIndex + 1, files.size());
+    }
+
+    record FileIssues(Path file, ImmutableList<Issue<Path>> issues) {}
   }
 }
