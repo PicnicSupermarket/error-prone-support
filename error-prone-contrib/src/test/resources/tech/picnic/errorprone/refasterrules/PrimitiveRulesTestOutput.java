7bcf5253b233d82587f022fbd8c7924cc2b51784
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-05-01 08:45:33 +0200

    Extend `PrimitiveRules` Refaster rule collection
    Assorted methods and constants exposed by Guava's
    `com.google.common.primitives.*` types are now replaced with their JDK
    equivalents.
    
    While there, also update the parameter types of some existing `@AfterTemplate`
    methods, resolving an inconsistency flagged by @knutwannheden.

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/PrimitiveRulesTestOutput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/PrimitiveRulesTestOutput.java
@@ -1,13 +1,28 @@
 package tech.picnic.errorprone.refasterrules;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.primitives.Booleans;
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Chars;
+import com.google.common.primitives.Doubles;
+import com.google.common.primitives.Floats;
 import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
+import com.google.common.primitives.Shorts;
 import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
 
 final class PrimitiveRulesTest implements RefasterRuleCollectionTestCase {
   @Override
   public ImmutableSet<Object> elidedTypesAndStaticImports() {
-    return ImmutableSet.of(Ints.class);
+    return ImmutableSet.of(
+        Booleans.class,
+        Bytes.class,
+        Chars.class,
+        Doubles.class,
+        Floats.class,
+        Ints.class,
+        Longs.class,
+        Shorts.class);
   }
 
   ImmutableSet<Boolean> testLessThan() {
@@ -56,5 +71,97 @@ final class PrimitiveRulesTest implements RefasterRuleCollectionTestCase {
 
   int testLongToIntExact() {
     return Math.toIntExact(Long.MAX_VALUE);
+  }
+
+  int testBooleanHashCode() {
+    return Boolean.hashCode(true);
+  }
+
+  int testByteHashCode() {
+    return Byte.hashCode((byte) 1);
+  }
+
+  int testCharacterHashCode() {
+    return Character.hashCode('a');
+  }
+
+  int testShortHashCode() {
+    return Short.hashCode((short) 1);
+  }
+
+  int testIntegerHashCode() {
+    return Integer.hashCode(1);
+  }
+
+  int testLongHashCode() {
+    return Long.hashCode(1);
+  }
+
+  int testFloatHashCode() {
+    return Float.hashCode(1);
+  }
+
+  int testDoubleHashCode() {
+    return Double.hashCode(1);
+  }
+
+  int testBooleanCompare() {
+    return Boolean.compare(false, true);
+  }
+
+  int testCharacterCompare() {
+    return Character.compare('a', 'b');
+  }
+
+  int testShortCompare() {
+    return Short.compare((short) 1, (short) 2);
+  }
+
+  int testIntegerCompare() {
+    return Integer.compare(1, 2);
+  }
+
+  int testLongCompare() {
+    return Long.compare(1, 2);
+  }
+
+  int testFloatCompare() {
+    return Float.compare(1, 2);
+  }
+
+  int testDoubleCompare() {
+    return Double.compare(1, 2);
+  }
+
+  int testCharacterBytes() {
+    return Character.BYTES;
+  }
+
+  int testShortBytes() {
+    return Short.BYTES;
+  }
+
+  int testIntegerBytes() {
+    return Integer.BYTES;
+  }
+
+  int testLongBytes() {
+    return Long.BYTES;
+  }
+
+  int testFloatBytes() {
+    return Float.BYTES;
+  }
+
+  int testDoubleBytes() {
+    return Double.BYTES;
+  }
+
+  boolean testFloatIsFinite() {
+    return Float.isFinite(1);
+  }
+
+  boolean testDoubleIsFinite() {
+    return Double.isFinite(1);
   }
 }
