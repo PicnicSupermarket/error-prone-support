dce1a922680589c377bb0d99a4ccaf331cbdac85
Gijs de Jong <berichtaangijs@gmail.com>
2023-05-08 14:08:57 +0200

    Update package-info.java

--- testng-junit-migrator/src/main/java/tech/picnic/errorprone/testngjunit/package-info.java
+++ testng-junit-migrator/src/main/java/tech/picnic/errorprone/testngjunit/package-info.java
@@ -1,2 +1,4 @@
-/** TestNG to JUnit migration using Error-Prone */
+/** TestNG to JUnit migration using Error-Prone. */
+@com.google.errorprone.annotations.CheckReturnValue
+@org.jspecify.annotations.NullMarked
 package tech.picnic.errorprone.testngjunit;
