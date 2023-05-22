919de4c8f5657517b9c53553e3bbe7eb781c03d1
Gijs de Jong <berichtaangijs@gmail.com>
2023-05-22 11:41:32 +0200

    Introduce `MoreMatchers#hasTypeArguments()` matcher

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/util/MoreMatchers.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/util/MoreMatchers.java
@@ -5,6 +5,11 @@ import com.google.errorprone.matchers.Matchers;
 import com.google.errorprone.predicates.TypePredicate;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.NewClassTree;
+import com.sun.source.tree.ParameterizedTypeTree;
+import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Symbol;
 
 /**
@@ -29,6 +34,35 @@ public final class MoreMatchers {
     return (tree, state) -> {
       Symbol sym = ASTHelpers.getSymbol(tree);
       return sym != null && typePredicate.apply(sym.type, state);
+    };
+  }
+
+  /**
+   * Returns a {@link Matcher} that determines whether a given {@link ExpressionTree} has type
+   * arguments.
+   *
+   * @param <T> The type of tree to match against.
+   * @return A {@link Matcher} that matches trees with type annotations.
+   */
+  public static <T extends ExpressionTree> Matcher<T> hasTypeArguments() {
+    return (tree, state) -> {
+      switch (tree.getKind()) {
+        case METHOD_INVOCATION:
+          return !((MethodInvocationTree) tree).getTypeArguments().isEmpty();
+        case NEW_CLASS:
+          NewClassTree classTree = (NewClassTree) tree;
+          if (!classTree.getTypeArguments().isEmpty()) {
+            return true;
+          }
+
+          if (classTree.getIdentifier().getKind() != Tree.Kind.PARAMETERIZED_TYPE) {
+            return false;
+          }
+
+          return !((ParameterizedTypeTree) classTree.getIdentifier()).getTypeArguments().isEmpty();
+        default:
+          return false;
+      }
     };
   }
 
--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/util/MoreMatchersTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/util/MoreMatchersTest.java
@@ -7,15 +7,20 @@ import com.google.errorprone.CompilationTestHelper;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.AnnotationTreeMatcher;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.bugpatterns.BugChecker.NewClassTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.NewClassTree;
 import org.junit.jupiter.api.Test;
 
 final class MoreMatchersTest {
   @Test
   void hasMetaAnnotation() {
-    CompilationTestHelper.newInstance(TestMatcher.class, getClass())
+    CompilationTestHelper.newInstance(MetaAnnotationTestMatcher.class, getClass())
         .addSourceLines(
             "A.java",
             "import org.junit.jupiter.api.AfterAll;",
@@ -49,13 +54,54 @@ final class MoreMatchersTest {
 
   /** A {@link BugChecker} that delegates to {@link MoreMatchers#hasMetaAnnotation(String)} . */
   @BugPattern(summary = "Interacts with `MoreMatchers` for testing purposes", severity = ERROR)
-  public static final class TestMatcher extends BugChecker implements AnnotationTreeMatcher {
+  public static final class MetaAnnotationTestMatcher extends BugChecker
+      implements AnnotationTreeMatcher {
     private static final long serialVersionUID = 1L;
     private static final Matcher<AnnotationTree> DELEGATE =
         MoreMatchers.hasMetaAnnotation("org.junit.jupiter.api.TestTemplate");
 
     @Override
     public Description matchAnnotation(AnnotationTree tree, VisitorState state) {
+      return DELEGATE.matches(tree, state) ? describeMatch(tree) : Description.NO_MATCH;
+    }
+  }
+
+  @Test
+  void hasTypeArgumentsMatcher() {
+    CompilationTestHelper.newInstance(HasTypeArgumentsTestMatcher.class, getClass())
+        .addSourceLines(
+            "A.java",
+            "import com.google.common.collect.ImmutableSet;",
+            "import java.util.ArrayList;",
+            "import java.util.List;",
+            "",
+            "class A {",
+            "  <E> void foo(E first) {",
+            "    // BUG: Diagnostic contains:",
+            "    ImmutableSet.<E>builder().add(first).build();",
+            "    // BUG: Diagnostic contains:",
+            "    new ImmutableSet.Builder<E>().add(first).build();",
+            "",
+            "    ImmutableSet<Integer> foo = new ImmutableSet.Builder().add(1).build();",
+            "    List<Integer> bar = new ArrayList<>();",
+            "  }",
+            "}")
+        .doTest();
+  }
+  /** A {@link BugChecker} that delegates to {@link MoreMatchers#hasTypeArguments()} . */
+  @BugPattern(summary = "Interacts with `MoreMatchers` for testing purposes", severity = ERROR)
+  public static final class HasTypeArgumentsTestMatcher extends BugChecker
+      implements MethodInvocationTreeMatcher, NewClassTreeMatcher {
+    private static final long serialVersionUID = 1L;
+    private static final Matcher<ExpressionTree> DELEGATE = MoreMatchers.hasTypeArguments();
+
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+      return DELEGATE.matches(tree, state) ? describeMatch(tree) : Description.NO_MATCH;
+    }
+
+    @Override
+    public Description matchNewClass(NewClassTree tree, VisitorState state) {
       return DELEGATE.matches(tree, state) ? describeMatch(tree) : Description.NO_MATCH;
     }
   }
