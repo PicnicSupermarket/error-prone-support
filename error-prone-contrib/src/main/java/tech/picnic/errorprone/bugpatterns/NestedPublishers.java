146c5d3e9067ba0fd15dd06463415918ce8ef79c
mohamedsamehsalah <mohamedsamehsalah.mohamed@teampicnic.com>
2023-05-31 22:03:26 +0200

    Address comments to improve readability

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/NestedPublishers.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/NestedPublishers.java
@@ -20,13 +20,11 @@ import com.google.errorprone.suppliers.Suppliers;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.tools.javac.code.Type;
+import java.util.function.Function;
 import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
-/**
- * A {@link BugChecker} that flags nesting of {@link Publisher Publishers} inside {@link Mono
- * Monos}.
- */
+/** A {@link BugChecker} that flags nesting of {@link Publisher Publishers}. */
 @AutoService(BugChecker.class)
 @BugPattern(
     summary =
@@ -39,8 +37,7 @@ public final class NestedPublishers extends BugChecker implements MethodInvocati
   private static final long serialVersionUID = 1L;
   private static final Supplier<Type> PUBLISHER = type("org.reactivestreams.Publisher");
   private static final Supplier<Type> PUBLISHER_OF_PUBLISHERS =
-      VisitorState.memoize(
-          generic(PUBLISHER, subOf(generic(type("org.reactivestreams.Publisher"), unbound()))));
+      VisitorState.memoize(generic(PUBLISHER, subOf(generic(PUBLISHER, unbound()))));
   private static final Supplier<Type> GROUPED_FLUX =
       VisitorState.memoize(
           generic(
@@ -56,19 +53,28 @@ public final class NestedPublishers extends BugChecker implements MethodInvocati
     Type publisherOfPublisherType = PUBLISHER_OF_PUBLISHERS.get(state);
     Type groupedFluxType = GROUPED_FLUX.get(state);
     Type treeType = ASTHelpers.getType(tree);
-    if ((publisherOfPublisherType == null
-            || !state.getTypes().isSubtype(treeType, publisherOfPublisherType))
-        || (groupedFluxType != null
-            && isTypeArgumentGroupedFlux(state, groupedFluxType, treeType))) {
+    if (!isNestedPublisher(state, publisherOfPublisherType, treeType)
+        || isTypeArgumentGroupedFlux(state, groupedFluxType, treeType)) {
       return Description.NO_MATCH;
     }
 
     return describeMatch(tree);
   }
 
+  private static boolean isNestedPublisher(
+      VisitorState state, Type publisherOfPublisherType, Type treeType) {
+    return publisherOfPublisherType != null
+        && state.getTypes().isSubtype(treeType, publisherOfPublisherType);
+  }
+
+  /**
+   * Excluding the type when it matches {@code Flux<GroupedFlux<K, V>>} to not flag usages of {@link
+   * Flux#groupBy(Function)}.
+   */
   private static boolean isTypeArgumentGroupedFlux(
       VisitorState state, Type groupedFluxType, Type treeType) {
-    return treeType.getTypeArguments().stream()
-        .anyMatch(typez -> ASTHelpers.isSameType(typez, groupedFluxType, state));
+    return groupedFluxType != null
+        && treeType.getTypeArguments().stream()
+            .anyMatch(typez -> ASTHelpers.isSameType(typez, groupedFluxType, state));
   }
 }
