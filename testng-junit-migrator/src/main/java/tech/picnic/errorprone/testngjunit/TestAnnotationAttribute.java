8b9310d96eb61ed28fa1f0eddb2f75b259826690
Gijs de Jong <berichtaangijs@gmail.com>
2023-06-07 10:05:07 +0200

    Implement `UnsupportedAttributeMigrator` as `Migrator`

--- testng-junit-migrator/src/main/java/tech/picnic/errorprone/testngjunit/TestAnnotationAttribute.java
+++ testng-junit-migrator/src/main/java/tech/picnic/errorprone/testngjunit/TestAnnotationAttribute.java
@@ -2,17 +2,16 @@ package tech.picnic.errorprone.testngjunit;
 
 import static java.util.Arrays.stream;
 
-import java.util.Optional;
-
 /** The annotation attributes that are supported by the TestNG to JUnit Jupiter migration. */
 enum TestAnnotationAttribute {
-  DATAPROVIDER("dataProvider", new DataProviderAttributeMigrator()),
+  DATA_PROVIDER("dataProvider", new DataProviderAttributeMigrator()),
   DESCRIPTION("description", new DescriptionAttributeMigrator()),
   ENABLED("enabled", new EnabledAttributeMigrator()),
   EXPECTED_EXCEPTIONS("expectedExceptions", new ExpectedExceptionsAttributeMigrator()),
   GROUPS("groups", new GroupsAttributeMigrator()),
   PRIORITY("priority", new PriorityAttributeMigrator()),
-  TIMEOUT("timeOut", new TimeOutAttributeMigrator());
+  TIMEOUT("timeOut", new TimeOutAttributeMigrator()),
+  UNSUPPORTED("_unsupported", new UnsupportedAttributeMigrator());
 
   private final String name;
   private final Migrator attributeMigrator;
@@ -26,7 +25,7 @@ enum TestAnnotationAttribute {
     return attributeMigrator;
   }
 
-  static Optional<TestAnnotationAttribute> fromString(String attribute) {
-    return stream(values()).filter(v -> v.name.equals(attribute)).findFirst();
+  static TestAnnotationAttribute fromString(String attribute) {
+    return stream(values()).filter(v -> v.name.equals(attribute)).findFirst().orElse(UNSUPPORTED);
   }
 }
