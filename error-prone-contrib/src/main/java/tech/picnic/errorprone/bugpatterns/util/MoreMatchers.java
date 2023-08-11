3ca3b3c61fd7bb0d22a9258ccf78750c8bdd7573
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-08-11 22:23:36 +0200

    Suggestions

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/util/MoreMatchers.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/util/MoreMatchers.java
@@ -1,11 +1,14 @@
 package tech.picnic.errorprone.bugpatterns.util;
 
+import static com.google.errorprone.matchers.Matchers.typePredicateMatcher;
+import static tech.picnic.errorprone.bugpatterns.util.MoreTypePredicates.hasAnnotation;
+
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.matchers.Matchers;
-import com.google.errorprone.predicates.TypePredicate;
-import com.google.errorprone.util.ASTHelpers;
+import com.google.errorprone.suppliers.Supplier;
 import com.sun.source.tree.AnnotationTree;
-import com.sun.tools.javac.code.Symbol;
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Type;
 
 /**
  * A collection of general-purpose {@link Matcher}s.
@@ -25,15 +28,21 @@ public final class MoreMatchers {
    * @return A {@link Matcher} that matches trees with the specified meta-annotation.
    */
   public static <T extends AnnotationTree> Matcher<T> hasMetaAnnotation(String annotationType) {
-    TypePredicate typePredicate = hasAnnotation(annotationType);
-    return (tree, state) -> {
-      Symbol sym = ASTHelpers.getSymbol(tree);
-      return sym != null && typePredicate.apply(sym.type, state);
-    };
+    return typePredicateMatcher(hasAnnotation(annotationType));
   }
 
-  // XXX: Consider moving to a `MoreTypePredicates` utility class.
-  private static TypePredicate hasAnnotation(String annotationClassName) {
-    return (type, state) -> ASTHelpers.hasAnnotation(type.tsym, annotationClassName, state);
+  /**
+   * Returns a {@link Matcher} that determines whether the type of a given {@link Tree} is a subtype
+   * of the type return by the specified {@link Supplier}.
+   *
+   * <p>This method differs from {@link Matchers#isSubtypeOf(Supplier)} in that it does not perform
+   * type erasure.
+   *
+   * @param <T> The type of tree to match against.
+   * @param type The {@link Supplier} that returns the type to match against.
+   * @return A {@link Matcher} that matches trees with the specified type.
+   */
+  public static <T extends Tree> Matcher<T> isSubTypeOf(Supplier<Type> type) {
+    return typePredicateMatcher(MoreTypePredicates.isSubTypeOf(type));
   }
 }
