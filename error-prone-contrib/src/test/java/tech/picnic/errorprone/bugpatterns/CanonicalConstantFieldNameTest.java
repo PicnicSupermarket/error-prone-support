2cd5015618794fca6db86839a808daf58e8cd135
mohamedsamehsalah <mohamedsamehsalah.mohamed@teampicnic.com>
2023-10-01 14:29:29 +0200

    Address comments: Match on compilation unit and bug fixes

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/CanonicalConstantFieldNameTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/CanonicalConstantFieldNameTest.java
@@ -14,9 +14,11 @@ final class CanonicalConstantFieldNameTest {
             "class A {",
             "  // BUG: Diagnostic contains: Suggested fix for constant name conflicts with an already defined",
             "  // variable `NUMBER`.",
-            "  private static final int number = 1;",
+            "  private static final int number = B.NUMBER;",
             "",
-            "  private static final int NUMBER = 2;",
+            "  class B {",
+            "    private static final int NUMBER = 1;",
+            "  }",
             "}")
         .doTest();
   }
@@ -29,14 +31,24 @@ final class CanonicalConstantFieldNameTest {
             "class A {",
             "  private static final int number = 1;",
             "",
+            "  int nonConstantNumber = 2;",
+            "",
+            "  int referenceToNumberFromAnotherClass = B.numberFromAnotherClass;",
+            "",
             "  static int getConstantNumber() {",
             "    return number;",
             "  }",
             "",
             "  static int getLocalNumber() {",
-            "    int number = 2;",
+            "    int number = 3;",
             "",
             "    return number;",
+            "  }",
+            "",
+            "  class B {",
+            "    private static final int number = 4;",
+            "",
+            "    private static final int numberFromAnotherClass = 5;",
             "  }",
             "}")
         .addOutputLines(
@@ -44,14 +56,24 @@ final class CanonicalConstantFieldNameTest {
             "class A {",
             "  private static final int NUMBER = 1;",
             "",
+            "  int nonConstantNumber = 2;",
+            "",
+            "  int referenceToNumberFromAnotherClass = B.NUMBER_FROM_ANOTHER_CLASS;",
+            "",
             "  static int getConstantNumber() {",
             "    return NUMBER;",
             "  }",
             "",
             "  static int getLocalNumber() {",
-            "    int number = 2;",
+            "    int number = 3;",
             "",
             "    return number;",
+            "  }",
+            "",
+            "  class B {",
+            "    private static final int NUMBER = 4;",
+            "",
+            "    private static final int NUMBER_FROM_ANOTHER_CLASS = 5;",
             "  }",
             "}")
         .doTest(TestMode.TEXT_MATCH);
@@ -77,6 +99,48 @@ final class CanonicalConstantFieldNameTest {
             "  public static final int number = 1;",
             "",
             "  static final int anotherNumber = 2;",
+            "}")
+        .doTest(TestMode.TEXT_MATCH);
+  }
+
+  @Test
+  void excludeFlaggedConstants() {
+    BugCheckerRefactoringTestHelper.newInstance(CanonicalConstantFieldName.class, getClass())
+        .setArgs("-XepOpt:CanonicalConstantFieldName:ExcludedConstantFliedNames=excludedField")
+        .addInputLines(
+            "A.java",
+            "class A {",
+            "  private static final int number = 1;",
+            "",
+            "  private static final int excludedField = 3;",
+            "}")
+        .addOutputLines(
+            "A.java",
+            "class A {",
+            "  private static final int NUMBER = 1;",
+            "",
+            "  private static final int excludedField = 3;",
+            "}")
+        .doTest(TestMode.TEXT_MATCH);
+  }
+
+  @Test
+  void IncludePublicConstants() {
+    BugCheckerRefactoringTestHelper.newInstance(CanonicalConstantFieldName.class, getClass())
+        .setArgs("-XepOpt:CanonicalConstantFieldName:IncludePublicConstantFields=true")
+        .addInputLines(
+            "A.java",
+            "class A {",
+            "  int nonConstantNumber = 1;",
+            "",
+            "  public static final int number = 2;",
+            "}")
+        .addOutputLines(
+            "A.java",
+            "class A {",
+            "  int nonConstantNumber = 1;",
+            "",
+            "  public static final int NUMBER = 2;",
             "}")
         .doTest(TestMode.TEXT_MATCH);
   }
