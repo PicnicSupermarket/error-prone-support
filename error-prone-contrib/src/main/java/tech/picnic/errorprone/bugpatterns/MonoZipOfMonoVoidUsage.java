14a0bb968d97b843c99512de8c76c8a62ce0bd60
Dima Legeza <dmytro.legeza@gmail.com>
2023-11-28 16:40:21 +0100

    * Drop the replacement logic * Limit the rule scope to explicit usage of empty publishers (Mono<Void> and Mono.empty) within the zipping logic

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MonoZipOfMonoVoidUsage.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MonoZipOfMonoVoidUsage.java
@@ -4,23 +4,23 @@ import static com.google.errorprone.BugPattern.LinkType.CUSTOM;
 import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
 import static com.google.errorprone.BugPattern.StandardTags.LIKELY_ERROR;
 import static com.google.errorprone.matchers.Matchers.allOf;
-import static com.google.errorprone.matchers.Matchers.anyMethod;
+import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.Matchers.instanceMethod;
 import static com.google.errorprone.matchers.Matchers.staticMethod;
 import static com.google.errorprone.matchers.Matchers.toType;
-import static java.util.stream.Collectors.joining;
+import static com.google.errorprone.util.ASTHelpers.isSameType;
 import static tech.picnic.errorprone.bugpatterns.util.Documentation.BUG_PATTERNS_BASE_URL;
 import static tech.picnic.errorprone.bugpatterns.util.MoreTypes.generic;
 import static tech.picnic.errorprone.bugpatterns.util.MoreTypes.type;
+import static tech.picnic.errorprone.bugpatterns.util.MoreTypes.unbound;
 
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Streams;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.MemberReferenceTreeMatcher;
 import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
-import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.fixes.SuggestedFixes;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
@@ -30,8 +30,7 @@ import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MemberReferenceTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.tools.javac.code.Type;
-import java.util.stream.Stream;
-import org.reactivestreams.Publisher;
+import com.sun.tools.javac.util.List;
 import reactor.core.publisher.Mono;
 
 /**
@@ -40,20 +39,17 @@ import reactor.core.publisher.Mono;
  *
  * <p>{@link Mono#zip(Mono, Mono)} and {@link Mono#zipWith(Mono)} perform incorrectly upon retrieval
  * of the empty publisher and prematurely terminates the reactive chain from the execution. In most
- * cases this is not the desired behaviour and {@link Mono#concatWith(Publisher)} or {@link
- * Mono#then(Mono)} should be preferred, as it produces consistent results and performs in the
- * predictable manner.
+ * cases this is not the desired behaviour.
  *
  * <p>NB: Mono&lt;?>#zipWith(Mono&lt;Void>) is allowed be the Reactor API, but it is an incorrect
- * usage of the API. It will be replaced with Mono&lt;?>#concatWith(Mono&lt;Void>) but it will lead
- * to the compilation errors, indicating that the logic under the hood should be revisited and
- * fixed.
+ * usage of the API. It will be flagged by ErrorProne but the fix won't be supplied. The problem
+ * with the original code should be revisited and fixed in a structural manner by the developer.
  */
 @AutoService(BugChecker.class)
 @BugPattern(
     summary =
-        "`Mono#zip` and `Mono#zipWith` should not be executed against `Mono#empty` parameter; "
-            + "please use `Mono#then` or `Mono#concatWith` instead",
+        "`Mono#zip` and `Mono#zipWith` should not be executed against `Mono#empty` or `Mono<Void>` parameter; "
+            + "please revisit the parameters used and make sure to supply correct publishers instead",
     link = BUG_PATTERNS_BASE_URL + "MonoZipOfMonoVoidUsage",
     linkType = CUSTOM,
     severity = ERROR,
@@ -61,70 +57,109 @@ import reactor.core.publisher.Mono;
 public final class MonoZipOfMonoVoidUsage extends BugChecker
     implements MethodInvocationTreeMatcher, MemberReferenceTreeMatcher {
   private static final long serialVersionUID = 1L;
-  private static final String MONO = "reactor.core.publisher.Mono";
+  private static final Supplier<Type> MONO = type(Mono.class.getName());
+  // Mono.empty() yields `Mono<Object>` under the hood
+  private static final Supplier<Type> MONO_OBJECT_TYPE =
+      VisitorState.memoize(generic(MONO, type(Object.class.getName())));
+  // In fact, we use `Mono<Void>` everywhere in codebases instead of `Mono<Object>` to represent
+  // empty publisher
   private static final Supplier<Type> MONO_VOID_TYPE =
-      VisitorState.memoize(generic(type(MONO), type("java.lang.Void")));
+      VisitorState.memoize(generic(MONO, type(Void.class.getName())));
+
+  private static final Supplier<Type> MONO_UNBOUND_TYPE =
+      VisitorState.memoize(generic(MONO, unbound()));
 
   // On Mono.zip, at least one element should match empty in order to proceed.
   private static final Matcher<ExpressionTree> MONO_ZIP_AND_WITH =
-      allOf(
-          anyMethod().onDescendantOf(MONO).namedAnyOf("zip", "zipWith"),
-          toType(MethodInvocationTree.class, hasArgumentOfType(MONO_VOID_TYPE)));
+      anyOf(
+          allOf(
+              instanceMethod().onDescendantOf(MONO_UNBOUND_TYPE).namedAnyOf("zip", "zipWith"),
+              toType(
+                  MethodInvocationTree.class,
+                  hasArgumentOfTypes(ImmutableList.of(MONO_VOID_TYPE, MONO_OBJECT_TYPE)))),
+          allOf(
+              instanceMethod().onDescendantOf(MONO_VOID_TYPE).namedAnyOf("zip", "zipWith"),
+              toType(MethodInvocationTree.class, hasArgumentOfType(MONO_UNBOUND_TYPE))),
+          allOf(
+              instanceMethod().onDescendantOf(MONO_OBJECT_TYPE).namedAnyOf("zip", "zipWith"),
+              toType(MethodInvocationTree.class, hasArgumentOfType(MONO_UNBOUND_TYPE))));
 
   // On Mono.zip, at least one element should match empty in order to proceed.
   private static final Matcher<ExpressionTree> STATIC_MONO_ZIP =
       allOf(
           staticMethod().onClass(MONO).named("zip"),
-          toType(MethodInvocationTree.class, hasArgumentOfType(MONO_VOID_TYPE)));
+          toType(
+              MethodInvocationTree.class,
+              hasArgumentOfTypes(ImmutableList.of(MONO_OBJECT_TYPE, MONO_VOID_TYPE))));
 
   /** Instantiates a new {@link MonoZipOfMonoVoidUsage} instance. */
   public MonoZipOfMonoVoidUsage() {}
 
   @Override
   public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
-    if (!MONO_ZIP_AND_WITH.matches(tree, state)) {
+    boolean dynamicMono = MONO_ZIP_AND_WITH.matches(tree, state);
+    boolean staticMono = STATIC_MONO_ZIP.matches(tree, state);
+    if (!dynamicMono && !staticMono) {
       return Description.NO_MATCH;
     }
 
-    Description.Builder description = buildDescription(tree);
-
-    if (STATIC_MONO_ZIP.matches(tree, state)) {
-      ImmutableList<? extends ExpressionTree> arguments = ImmutableList.copyOf(tree.getArguments());
-
-      String replacement =
-          Streams.concat(
-                  Stream.of(
-                      arguments.stream().findFirst().map(ExpressionTree::toString).orElseThrow()),
-                  arguments.stream()
-                      .skip(1)
-                      .map(ExpressionTree::toString)
-                      .map(arg -> String.format("(%s)", arg)))
-              .collect(joining(".then"));
-      SuggestedFix staticZipUsage = SuggestedFix.replace(tree, replacement);
-      description.addFix(staticZipUsage);
-    } else {
-      SuggestedFix instanceZipUsage =
-          SuggestedFixes.renameMethodInvocation(tree, "concatWith", state);
-      description.addFix(instanceZipUsage);
-    }
-
-    return description.build();
+    return buildDescription(tree)
+        .setMessage(
+            "`Mono#zip` and `Mono#zipWith` should not be executed against empty publisher; "
+                + "remove it or suppress this warning and add a comment explaining its purpose")
+        .addFix(SuggestedFixes.addSuppressWarnings(state, canonicalName()))
+        .build();
   }
 
   @Override
   public Description matchMemberReference(MemberReferenceTree tree, VisitorState state) {
-    if (!MONO_ZIP_AND_WITH.matches(tree, state)) {
+    boolean dynamicMono = MONO_ZIP_AND_WITH.matches(tree, state);
+    boolean staticMono = STATIC_MONO_ZIP.matches(tree, state);
+
+    if (!dynamicMono && !staticMono) {
       return Description.NO_MATCH;
     }
-
-    // Method references are expected to occur very infrequently; generating both variants of
-    // suggested fixes is not worth the trouble.
     return describeMatch(tree);
   }
 
   private static Matcher<MethodInvocationTree> hasArgumentOfType(Supplier<Type> type) {
+    return hasArgumentOfTypes(ImmutableList.of(type));
+  }
+
+  /**
+   * We need to extract real types from the generics because {@link ASTHelpers} cannot distinguish
+   * {@link Mono}&lt;{@link Integer}&gt; and {@link Mono}&lt;{@link Void}&gt; and reports those
+   * being the same.
+   *
+   * <p>In case of {@link Mono}, we can infer the real type out of the parameters of the invocation
+   * ({@link MethodInvocationTree#getArguments()}):
+   *
+   * <p>- either we have explicit variable declared and the provided type which will be inferred,
+   *
+   * <p>- or we have a method invocation, like {@link Mono#just(Object)} or {@link Mono#empty()},
+   * for which we can also infer type.
+   *
+   * <p>Similarly, we can infer the matching type
+   *
+   * <p>In this case we will always have only one parameter.
+   */
+  private static Matcher<MethodInvocationTree> hasArgumentOfTypes(
+      ImmutableList<Supplier<Type>> types) {
     return (tree, state) ->
         tree.getArguments().stream()
-            .anyMatch(arg -> ASTHelpers.isSubtype(ASTHelpers.getType(arg), type.get(state), state));
+            .anyMatch(
+                arg -> {
+                  List<Type> allParams = ASTHelpers.getType(arg).allparams();
+                  if (allParams.isEmpty()) {
+                    return false;
+                  }
+
+                  Type argumentType = allParams.get(0);
+                  return types.stream()
+                      .map(type -> type.get(state).allparams().get(0))
+                      .anyMatch(
+                          requiredMatchingType ->
+                              isSameType(argumentType, requiredMatchingType, state));
+                });
   }
 }
--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/MonoZipOfMonoVoidUsageTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/MonoZipOfMonoVoidUsageTest.java
@@ -1,7 +1,5 @@
 package tech.picnic.errorprone.bugpatterns;
 
-import com.google.errorprone.BugCheckerRefactoringTestHelper;
-import com.google.errorprone.BugCheckerRefactoringTestHelper.TestMode;
 import com.google.errorprone.CompilationTestHelper;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +20,9 @@ final class MonoZipOfMonoVoidUsageTest {
             "    // BUG: Diagnostic contains:",
             "    Mono.zip(a, a);",
             "    // BUG: Diagnostic contains:",
+            "    Mono.zip(d, c, b, a);",
+            "    Mono.zip(d, c, b);",
+            "    // BUG: Diagnostic contains:",
             "    Mono.zip(Mono.empty(), a);",
             "    // BUG: Diagnostic contains:",
             "    Mono.zip(Mono.empty(), Mono.empty());",
@@ -29,108 +30,18 @@ final class MonoZipOfMonoVoidUsageTest {
             "    Mono.empty().zipWith(a);",
             "    // BUG: Diagnostic contains:",
             "    Mono.empty().zipWith(Mono.empty());",
-            "    // BUG: Diagnostic contains:",
             "    b.zipWith(b).zipWith(c).map(entry -> entry);",
             "    // BUG: Diagnostic contains:",
             "    c.zipWith(Mono.empty());",
-            "    // BUG: Diagnostic contains:",
             "    c.zipWith(b);",
             "    c.zipWith(d);",
             "    Mono.just(1).zipWith(Mono.just(1));",
+            "    Mono.zip(Mono.just(1), Mono.just(1));",
             "    c.zipWith(c);",
             "  }",
             "",
             "  private Mono<Integer> publisher() {return Mono.empty();}",
             "}")
         .doTest();
-  }
-
-  @Test
-  void replacementFirstSuggestedFix() {
-    BugCheckerRefactoringTestHelper.newInstance(MonoZipOfMonoVoidUsage.class, getClass())
-        .addInputLines(
-            "A.java",
-            "import reactor.core.publisher.Mono;",
-            "",
-            "class A {",
-            "  void m() {",
-            "    Mono<Void> a = Mono.empty();",
-            "    Mono<Integer> b = Mono.empty();",
-            "    Mono<Integer> c = Mono.just(1);",
-            "    Mono<Integer> d = this.publisher();",
-            "    Mono.zip(a, a);",
-            "    Mono.zip(Mono.empty(), a);",
-            "    Mono.zip(Mono.empty(), Mono.empty());",
-            "",
-            "    Mono.empty().zipWith(a);",
-            "    Mono.empty().zipWith(Mono.empty());",
-            "    b.zipWith(b).zipWith(c).map(entry -> entry);",
-            "    c.zipWith(Mono.empty());",
-            "    c.zipWith(b);",
-            "    c.zipWith(d);",
-            "    Mono.just(1).zipWith(Mono.just(1));",
-            "    c.zipWith(c);",
-            "  }",
-            "",
-            "  private Mono<Integer> publisher() {return Mono.empty();}",
-            "}")
-        .addOutputLines(
-            "A.java",
-            "import reactor.core.publisher.Mono;",
-            "",
-            "class A {",
-            "  void m() {",
-            "    Mono<Void> a = Mono.empty();",
-            "    Mono<Integer> b = Mono.empty();",
-            "    Mono<Integer> c = Mono.just(1);",
-            "    Mono<Integer> d = this.publisher();",
-            "    a.then(a);",
-            "    Mono.empty().then(a);",
-            "    Mono.empty().then(Mono.empty());",
-            "",
-            "    Mono.empty().concatWith(a);",
-            "    Mono.empty().concatWith(Mono.empty());",
-            "    b.concatWith(b).concatWith(c).map(entry -> entry);",
-            "    c.concatWith(Mono.empty());",
-            "    c.concatWith(b);",
-            "    c.zipWith(d);",
-            "    Mono.just(1).zipWith(Mono.just(1));",
-            "    c.zipWith(c);",
-            "  }",
-            "",
-            "  private Mono<Integer> publisher() {return Mono.empty();}",
-            "}")
-        .doTest(TestMode.TEXT_MATCH);
-  }
-
-  @Test
-  void replacementFirstSuggestedFixWithCompilationErrors() {
-    BugCheckerRefactoringTestHelper.newInstance(MonoZipOfMonoVoidUsage.class, getClass())
-        .addInputLines(
-            "A.java",
-            "import reactor.core.publisher.Mono;",
-            "",
-            "class A {",
-            "  void m() {",
-            "    Mono<Void> a = Mono.empty();",
-            "    Mono<Integer> b = Mono.empty();",
-            "    Mono.zip(a, b);",
-            "    a.zipWith(b);",
-            "  }",
-            "}")
-        .addOutputLines(
-            "A.java",
-            "import reactor.core.publisher.Mono;",
-            "",
-            "class A {",
-            "  void m() {",
-            "    Mono<Void> a = Mono.empty();",
-            "    Mono<Integer> b = Mono.empty();",
-            "    a.then(b);",
-            "    a.concatWith(b);",
-            "  }",
-            "}")
-        .allowBreakingChanges()
-        .doTest(TestMode.TEXT_MATCH);
   }
 }
