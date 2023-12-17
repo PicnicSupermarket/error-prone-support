a80d2597650318e96ad79142d3dfb163f2616b2c
Tim te Beek <tim@moderne.io>
2023-12-17 15:05:31 +0100

    Apply formatter to StringRulesRecipesTest

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/refasterrules/StringRulesRecipesTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/refasterrules/StringRulesRecipesTest.java
@@ -1,35 +1,33 @@
 package tech.picnic.errorprone.refasterrules;
 
+import static org.openrewrite.java.Assertions.java;
+
 import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-import static org.openrewrite.java.Assertions.java;
-
 class StringRulesRecipesTest implements RewriteTest {
 
-    @Override
-    public void defaults(RecipeSpec spec) {
-        spec.recipe(new StringRulesRecipes());
-    }
+  @Override
+  public void defaults(RecipeSpec spec) {
+    spec.recipe(new StringRulesRecipes());
+  }
 
-    @Test
-    void stringValueOf() {
-        rewriteRun(
-                //language=java
-                java(
-                        "import java.util.Objects;\n" +
-                        "class Test {\n" +
-                        "    String test(Object object) {\n" +
-                        "        return Objects.toString(object);\n" +
-                        "    }\n" +
-                        "}",
-                        "class Test {\n" +
-                        "    String test(Object object) {\n" +
-                        "        return String.valueOf(object);\n" +
-                        "    }\n" +
-                        "}"
-                )
-        );
-    }
+  @Test
+  void stringValueOf() {
+    rewriteRun(
+        // language=java
+        java(
+            "import java.util.Objects;\n"
+                + "class Test {\n"
+                + "    String test(Object object) {\n"
+                + "        return Objects.toString(object);\n"
+                + "    }\n"
+                + "}",
+            "class Test {\n"
+                + "    String test(Object object) {\n"
+                + "        return String.valueOf(object);\n"
+                + "    }\n"
+                + "}"));
+  }
 }
