ef9771ae206a43d3310cc49df3355ef211341453
Tim te Beek <tim@moderne.io>
2023-12-17 16:07:11 +0100

    Add a test that reuses StringRulesTestInput & Output

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/refasterrules/StringRulesRecipesTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/refasterrules/StringRulesRecipesTest.java
@@ -2,7 +2,12 @@ package tech.picnic.errorprone.refasterrules;
 
 import static org.openrewrite.java.Assertions.java;
 
+import com.google.errorprone.FileObjects;
+import java.io.IOException;
+import javax.tools.JavaFileObject;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
@@ -17,7 +22,7 @@ class StringRulesRecipesTest implements RewriteTest {
   void stringValueOf() {
     rewriteRun(
         // language=java
-        java(
+        java( // This would be much better with text blocks
             "import java.util.Objects;\n"
                 + "class Test {\n"
                 + "    String test(Object object) {\n"
@@ -29,5 +34,17 @@ class StringRulesRecipesTest implements RewriteTest {
                 + "        return String.valueOf(object);\n"
                 + "    }\n"
                 + "}"));
+  }
+
+  @Test
+  @Disabled("Not all rules are currently supported")
+  void reuseStringRulesTestInputOutput() throws IOException {
+    String rule = "StringRules";
+    JavaFileObject before = FileObjects.forResource(getClass(), rule + "TestInput.java");
+    JavaFileObject after = FileObjects.forResource(getClass(), rule + "TestOutput.java");
+    rewriteRun(
+        spec ->
+            spec.parser(JavaParser.fromJavaVersion().classpath("guava", "refaster-test-support")),
+        java(before.getCharContent(true).toString(), after.getCharContent(true).toString()));
   }
 }
