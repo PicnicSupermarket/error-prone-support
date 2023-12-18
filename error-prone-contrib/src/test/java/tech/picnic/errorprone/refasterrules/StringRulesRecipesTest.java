577156742e9bb6795f6ad77fb1c8c04f5f5a7f6b
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-12-18 06:52:38 +0100

    Suggestions

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/refasterrules/StringRulesRecipesTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/refasterrules/StringRulesRecipesTest.java
@@ -1,16 +1,20 @@
 package tech.picnic.errorprone.refasterrules;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.openrewrite.java.Assertions.java;
 
-import com.google.errorprone.FileObjects;
+import com.google.common.io.Resources;
 import java.io.IOException;
-import javax.tools.JavaFileObject;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
+// XXX: This class currently validates the OpenRewrite recipe generation by applying a single
+// recipe. Generalize this setup to cover all generated recipes (for _all_ Refaster rule
+// collections), ideally by reusing the `RefasterRulesTest` test resources. (This may introduce
+// additional hurdles, as OpenRewrite removes obsolete imports, while Refaster doesn't.)
 final class StringRulesRecipesTest implements RewriteTest {
   @Override
   public void defaults(RecipeSpec spec) {
@@ -21,30 +25,32 @@ final class StringRulesRecipesTest implements RewriteTest {
   void stringValueOf() {
     // XXX: Use text blocks once supported.
     rewriteRun(
-        // language=java
         java(
             "import java.util.Objects;\n"
+                + '\n'
                 + "class Test {\n"
-                + "    String test(Object object) {\n"
-                + "        return Objects.toString(object);\n"
-                + "    }\n"
-                + "}",
+                + "  String test(Object object) {\n"
+                + "    return Objects.toString(object);\n"
+                + "  }\n"
+                + '}',
             "class Test {\n"
-                + "    String test(Object object) {\n"
-                + "        return String.valueOf(object);\n"
-                + "    }\n"
-                + "}"));
+                + "  String test(Object object) {\n"
+                + "    return String.valueOf(object);\n"
+                + "  }\n"
+                + '}'));
   }
 
-  @Test
   @Disabled("Not all rules are currently supported")
-  void reuseStringRulesTestInputOutput() throws IOException {
-    String rule = "StringRules";
-    JavaFileObject before = FileObjects.forResource(getClass(), rule + "TestInput.java");
-    JavaFileObject after = FileObjects.forResource(getClass(), rule + "TestOutput.java");
+  @Test
+  void allRules() throws IOException {
     rewriteRun(
         spec ->
             spec.parser(JavaParser.fromJavaVersion().classpath("guava", "refaster-test-support")),
-        java(before.getCharContent(true).toString(), after.getCharContent(true).toString()));
+        java(
+            loadResource("StringRulesTestInput.java"), loadResource("StringRulesTestOutput.java")));
+  }
+
+  private String loadResource(String resource) throws IOException {
+    return Resources.toString(Resources.getResource(getClass(), resource), UTF_8);
   }
 }
