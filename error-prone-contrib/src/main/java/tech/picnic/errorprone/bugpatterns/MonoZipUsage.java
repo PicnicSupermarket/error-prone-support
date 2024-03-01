d682f644a4ede5a9e726c1cdada588f25cb40ed9
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2024-03-01 08:49:58 +0100

    More cleanup

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MonoZipUsage.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/MonoZipUsage.java
@@ -6,7 +6,9 @@ import static com.google.errorprone.BugPattern.StandardTags.LIKELY_ERROR;
 import static com.google.errorprone.matchers.Matchers.anyOf;
 import static com.google.errorprone.matchers.Matchers.instanceMethod;
 import static com.google.errorprone.matchers.Matchers.staticMethod;
+import static com.google.errorprone.matchers.Matchers.typePredicateMatcher;
 import static tech.picnic.errorprone.utils.Documentation.BUG_PATTERNS_BASE_URL;
+import static tech.picnic.errorprone.utils.MoreTypePredicates.isSubTypeOf;
 import static tech.picnic.errorprone.utils.MoreTypes.generic;
 import static tech.picnic.errorprone.utils.MoreTypes.type;
 
@@ -19,7 +21,6 @@ import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.suppliers.Supplier;
 import com.google.errorprone.suppliers.Suppliers;
-import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodInvocationTree;
@@ -32,17 +33,16 @@ import reactor.core.publisher.Mono;
  * Mono#zipWith(Mono)} with {@link Mono#empty()} as arguments.
  *
  * <p>Usage of {@code Mono.zip} and {@code Mono.zipWith} methods with {@code Mono.empty()} as
- * arguments can lead to unintended behavior. When a Mono completes empty or encounters an error,
- * the reactive chain may prematurely terminate, which might not align with the intended logic. This
- * BugChecker helps identify such cases, allowing developers to review and refactor their code to
- * avoid potential issues.
+ * arguments can lead to unintended behavior. When a {@code Mono} completes empty or encounters an
+ * error, the reactive chain may prematurely terminate, which might not align with the intended
+ * logic.
  *
  * @apiNote While {@code Mono<?>.zipWith(Mono<Void>)} is technically allowed by the Reactor API, it
  *     is considered an incorrect usage as it can lead to unexpected behavior.
  */
 @AutoService(BugChecker.class)
 @BugPattern(
-    summary = "Don't pass an `Mono<Void>` argument or `Mono.empty()` to Mono#{zip,With}`",
+    summary = "Don't pass a `Mono<Void>` argument or `Mono.empty()` to Mono#{zip,With}`",
     link = BUG_PATTERNS_BASE_URL + "MonoZipUsage",
     linkType = CUSTOM,
     severity = ERROR,
@@ -53,10 +53,11 @@ public final class MonoZipUsage extends BugChecker implements MethodInvocationTr
       Suppliers.typeFromString("reactor.core.publisher.Mono");
   private static final Matcher<ExpressionTree> MONO_EMPTY =
       staticMethod().onDescendantOf(MONO).named("empty");
-  private static final Supplier<Type> MONO_VOID =
-      VisitorState.memoize(generic(MONO, type(Void.class.getCanonicalName())));
-
-  private static final Matcher<ExpressionTree> MONO_ZIP_AND_ZIP_WITH =
+  private static final Matcher<ExpressionTree> MONO_VOID_OR_MONO_EMPTY =
+      anyOf(
+          MONO_EMPTY,
+          typePredicateMatcher(isSubTypeOf(generic(MONO, type(Void.class.getCanonicalName())))));
+  private static final Matcher<ExpressionTree> MONO_ZIP_OR_ZIP_WITH =
       anyOf(
           instanceMethod().onDescendantOf(type("reactor.core.publisher.Mono")).named("zipWith"),
           staticMethod().onClass(type("reactor.core.publisher.Mono")).named("zip"));
@@ -66,32 +67,25 @@ public final class MonoZipUsage extends BugChecker implements MethodInvocationTr
 
   @Override
   public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
-    if (!MONO_ZIP_AND_ZIP_WITH.matches(tree, state)) {
+    if (!MONO_ZIP_OR_ZIP_WITH.matches(tree, state)) {
       return Description.NO_MATCH;
     }
 
-    if (ownerIsMonoEmpty(tree, state)) {
+    if (isInvokedOnMonoEmpty(tree, state)) {
       return buildDescription(tree)
           .setMessage("Invoking a `Mono#zip` or `Mono#zipWith` on a `Mono#empty()` is a no-op.")
           .build();
     }
 
     List<? extends ExpressionTree> arguments = tree.getArguments();
-    if (arguments.stream()
-        .noneMatch(
-            arg ->
-                // XXX: We can probably provide a single `Matcher` for these two things.
-                MONO_EMPTY.matches(arg, state)
-                    || state
-                        .getTypes()
-                        .isSameType(ASTHelpers.getType(arg), MONO_VOID.get(state)))) {
+    if (arguments.stream().noneMatch(arg -> MONO_VOID_OR_MONO_EMPTY.matches(arg, state))) {
       return Description.NO_MATCH;
     }
     return describeMatch(tree);
   }
 
   // XXX: Can we do this in a nicer way?
-  private static boolean ownerIsMonoEmpty(MethodInvocationTree tree, VisitorState state) {
+  private static boolean isInvokedOnMonoEmpty(MethodInvocationTree tree, VisitorState state) {
     MemberSelectTree methodSelect = (MemberSelectTree) tree.getMethodSelect();
     return MONO_EMPTY.matches(methodSelect.getExpression(), state);
   }
