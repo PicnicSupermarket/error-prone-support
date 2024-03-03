f0fe44fc1d4ee4a2fd92a4aac39c15930632e455
mohamedsamehsalah <mohamed.sameh@teampicnic.com>
2024-03-03 11:47:01 +0100

    Address comments: Add test case for bug checker's flags

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/Slf4jLogDeclarationTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/Slf4jLogDeclarationTest.java
@@ -1,12 +1,70 @@
 package tech.picnic.errorprone.bugpatterns;
 
+import com.google.common.collect.ImmutableList;
 import com.google.errorprone.BugCheckerRefactoringTestHelper;
-import com.google.errorprone.BugCheckerRefactoringTestHelper.TestMode;
+import com.google.errorprone.CompilationTestHelper;
 import org.junit.jupiter.api.Test;
 
 final class Slf4jLogDeclarationTest {
   @Test
-  void replacement() {
+  void identification() {
+    CompilationTestHelper.newInstance(Slf4jLogDeclaration.class, getClass())
+        .addSourceLines(
+            "A.java",
+            "import org.slf4j.Logger;",
+            "import org.slf4j.LoggerFactory;",
+            "",
+            "// BUG: Diagnostic contains: SLF4J",
+            "class A {",
+            "  Logger FOO = LoggerFactory.getLogger(A.class);",
+            "",
+            "  // BUG: Diagnostic contains: SLF4J",
+            "  static class B {",
+            "    private Logger BAR = LoggerFactory.getLogger(B.class);",
+            "  }",
+            "",
+            "  // BUG: Diagnostic contains: SLF4J",
+            "  static class C {",
+            "    private static Logger BAZ = LoggerFactory.getLogger(C.class);",
+            "  }",
+            "",
+            "  // BUG: Diagnostic contains: SLF4J",
+            "  static class D {",
+            "    static final Logger QUX = LoggerFactory.getLogger(D.class);",
+            "  }",
+            "",
+            "  // BUG: Diagnostic contains: SLF4J",
+            "  static class E {",
+            "    private final Logger QUUX = LoggerFactory.getLogger(E.class);",
+            "  }",
+            "",
+            "  // BUG: Diagnostic contains: SLF4J",
+            "  static class F {",
+            "    private static final Logger CORGE = LoggerFactory.getLogger(F.class);",
+            "  }",
+            "",
+            "  // BUG: Diagnostic contains: SLF4J",
+            "  static class G {",
+            "    private static final Logger GRAPLY = LoggerFactory.getLogger(G.class);",
+            "  }",
+            "",
+            "  // BUG: Diagnostic contains: SLF4J",
+            "  static class H {",
+            "    private static final Logger WALDO = LoggerFactory.getLogger(J.class);",
+            "  }",
+            "",
+            "  class J {}",
+            "",
+            "  // BUG: Diagnostic contains: SLF4J",
+            "  interface K {",
+            "    Logger FRED = LoggerFactory.getLogger(A.class);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  void replacementWithDefaultCanonicalizedLoggerName() {
     BugCheckerRefactoringTestHelper.newInstance(Slf4jLogDeclaration.class, getClass())
         .addInputLines(
             "A.java",
@@ -14,42 +72,7 @@ final class Slf4jLogDeclarationTest {
             "import org.slf4j.LoggerFactory;",
             "",
             "class A {",
-            "  Logger NOT_PROPER_LOGGER_NAME = LoggerFactory.getLogger(A.class);",
-            "",
-            "  static class B {",
-            "    private Logger NOT_PROPER_LOGGER_NAME = LoggerFactory.getLogger(B.class);",
-            "  }",
-            "",
-            "  static class C {",
-            "    private static Logger NOT_PROPER_LOGGER_NAME = LoggerFactory.getLogger(C.class);",
-            "  }",
-            "",
-            "  static class D {",
-            "    static final Logger NOT_PROPER_LOGGER_NAME = LoggerFactory.getLogger(D.class);",
-            "  }",
-            "",
-            "  static class E {",
-            "    private final Logger NOT_PROPER_LOGGER_NAME = LoggerFactory.getLogger(E.class);",
-            "  }",
-            "",
-            "  static class F {",
-            "    private static final Logger NOT_PROPER_LOGGER_NAME = LoggerFactory.getLogger(F.class);",
-            "  }",
-            "",
-            "  static class G {",
-            "    private static final Logger NOT_PROPER_LOGGER_NAME = LoggerFactory.getLogger(G.class);",
-            "  }",
-            "",
-            "  static class H {",
-            "    private static final Logger LOGGER_WITH_WRONG_CLASS_AS_ARGUMENT =",
-            "        LoggerFactory.getLogger(J.class);",
-            "  }",
-            "",
-            "  class J {}",
-            "",
-            "  interface K {",
-            "    Logger NOT_PROPER_LOGGER_NAME = LoggerFactory.getLogger(A.class);",
-            "  }",
+            "  Logger FOO = LoggerFactory.getLogger(A.class);",
             "}")
         .addOutputLines(
             "A.java",
@@ -58,41 +81,30 @@ final class Slf4jLogDeclarationTest {
             "",
             "class A {",
             "  private static final Logger LOG = LoggerFactory.getLogger(A.class);",
-            "",
-            "  static class B {",
-            "    private static final Logger LOG = LoggerFactory.getLogger(B.class);",
-            "  }",
-            "",
-            "  static class C {",
-            "    private static final Logger LOG = LoggerFactory.getLogger(C.class);",
-            "  }",
-            "",
-            "  static class D {",
-            "    private static final Logger LOG = LoggerFactory.getLogger(D.class);",
-            "  }",
-            "",
-            "  static class E {",
-            "    private static final Logger LOG = LoggerFactory.getLogger(E.class);",
-            "  }",
-            "",
-            "  static class F {",
-            "    private static final Logger LOG = LoggerFactory.getLogger(F.class);",
-            "  }",
-            "",
-            "  static class G {",
-            "    private static final Logger LOG = LoggerFactory.getLogger(G.class);",
-            "  }",
-            "",
-            "  static class H {",
-            "    private static final Logger LOG = LoggerFactory.getLogger(H.class);",
-            "  }",
-            "",
-            "  class J {}",
-            "",
-            "  interface K {",
-            "    Logger LOG = LoggerFactory.getLogger(K.class);",
-            "  }",
             "}")
-        .doTest(TestMode.TEXT_MATCH);
+        .doTest();
+  }
+
+  @Test
+  void replacementWithOverriddenCanonicalizedLoggerName() {
+    BugCheckerRefactoringTestHelper.newInstance(Slf4jLogDeclaration.class, getClass())
+        .setArgs(ImmutableList.of("-XepOpt:Slf4jLogDeclaration:CanonicalizedLoggerName=BAR"))
+        .addInputLines(
+            "A.java",
+            "import org.slf4j.Logger;",
+            "import org.slf4j.LoggerFactory;",
+            "",
+            "class A {",
+            "  Logger FOO = LoggerFactory.getLogger(A.class);",
+            "}")
+        .addOutputLines(
+            "A.java",
+            "import org.slf4j.Logger;",
+            "import org.slf4j.LoggerFactory;",
+            "",
+            "class A {",
+            "  private static final Logger BAR = LoggerFactory.getLogger(A.class);",
+            "}")
+        .doTest();
   }
 }
