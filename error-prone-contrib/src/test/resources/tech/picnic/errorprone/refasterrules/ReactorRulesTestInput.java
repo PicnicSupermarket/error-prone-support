4558f8affb04ec1180f4a7a6a62de41a3b1066e0
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2024-04-16 17:04:48 +0200

    Update `FluxTake` Refaster rule for Reactor 3.5.0+ (#1128)

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/ReactorRulesTestInput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/ReactorRulesTestInput.java
@@ -142,7 +142,7 @@ final class ReactorRulesTest implements RefasterRuleCollectionTestCase {
   }
 
   Flux<Integer> testFluxTake() {
-    return Flux.just(1, 2, 3).take(1);
+    return Flux.just(1, 2, 3).take(1, true);
   }
 
   Mono<String> testMonoDefaultIfEmpty() {
