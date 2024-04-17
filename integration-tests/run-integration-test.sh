0c9e563c817a5ee60cccb01d2cc68b5503f52c1c
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2024-04-17 08:07:11 +0200

    Improve docs

--- integration-tests/run-integration-test.sh
+++ integration-tests/run-integration-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This script is not meant to be invoked manually, instead it should be invoked
-# through one of the integration test scripts such as the metrics or checkstyle one.
+# through one of the integration test scripts such as `checkstyle.sh`.
 
 set -e -u -o pipefail
 
