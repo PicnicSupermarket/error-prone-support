68d0635b9bfd771091d65166f1085f277a64bf1a
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2024-06-06 17:02:57 +0200

    Improve the third example

--- workshop/src/test/resources/tech/picnic/errorprone/workshop/refasterrules/WorkshopAssignment3RulesTestOutput.java
+++ workshop/src/test/resources/tech/picnic/errorprone/workshop/refasterrules/WorkshopAssignment3RulesTestOutput.java
@@ -2,16 +2,19 @@ package tech.picnic.errorprone.workshop.refasterrules;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
+import java.util.concurrent.ThreadLocalRandom;
 import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
 
 final class WorkshopAssignment3RulesTest implements RefasterRuleCollectionTestCase {
   void testCheckArgumentWithoutMessage() {
     checkArgument("foo".isEmpty());
-    checkArgument("bar".isEmpty());
+    checkArgument(ThreadLocalRandom.current().nextBoolean());
   }
 
   void testCheckArgumentWithMessage() {
     checkArgument("foo".isEmpty(), "The string is not empty");
-    checkArgument("bar".isEmpty(), "The rule should be able rewrite all kinds of messages ;).");
+    checkArgument(
+        ThreadLocalRandom.current().nextBoolean(),
+        "The rule should be able rewrite all kinds of messages ;).");
   }
 }
