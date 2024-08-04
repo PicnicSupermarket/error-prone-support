7364a3424776a9f07b75c08bba3c2a83114f2fd6
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2024-08-04 23:50:14 +0200

    Undo intentionally introduced issue

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/OptionalRules.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/OptionalRules.java
@@ -231,11 +231,7 @@ final class OptionalRules {
     @BeforeTemplate
     @SuppressWarnings("NullAway")
     Optional<S> before(Optional<T> optional) {
-      // XXX: This use of `.get()` rather than `.orElseThrow()` should be flagged by the
-      // `OptionalOrElseThrow` rule when building with
-      // `mvn clean install -Perror-prone-fork -Pnon-maven-central -Pself-check`
-      // (Requires a prior `mvn install`.)
-      return optional.map(v -> toOptionalFunction(v).get());
+      return optional.map(v -> toOptionalFunction(v).orElseThrow());
     }
 
     @AfterTemplate
