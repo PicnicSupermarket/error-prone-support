3d9aab7c5b9776929da49b32d41cdd9400dd1df1
Mohamed Sameh <110535847+mohamedsamehsalah@users.noreply.github.com>
2024-08-11 15:01:53 +0200

    Introduce `Class{Literal,Reference}Cast` Refaster rules (#1269)

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/ClassRulesTestInput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/ClassRulesTestInput.java
@@ -1,26 +1,34 @@
 package tech.picnic.errorprone.refasterrules;
 
 import com.google.common.collect.ImmutableSet;
-import java.io.IOException;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
 
 final class ClassRulesTest implements RefasterRuleCollectionTestCase {
-  boolean testClassIsInstance() throws IOException {
+  boolean testClassIsInstance() {
     return CharSequence.class.isAssignableFrom("foo".getClass());
   }
 
-  ImmutableSet<Boolean> testInstanceof() throws IOException {
+  ImmutableSet<Boolean> testInstanceof() {
     Class<?> clazz = CharSequence.class;
     return ImmutableSet.of(CharSequence.class.isInstance("foo"), clazz.isInstance("bar"));
   }
 
-  Predicate<String> testClassLiteralIsInstancePredicate() throws IOException {
+  Predicate<String> testClassLiteralIsInstancePredicate() {
     return s -> s instanceof CharSequence;
   }
 
-  Predicate<String> testClassReferenceIsInstancePredicate() throws IOException {
+  Predicate<String> testClassReferenceIsInstancePredicate() {
     Class<?> clazz = CharSequence.class;
     return s -> clazz.isInstance(s);
+  }
+
+  Function<Number, Integer> testClassLiteralCast() {
+    return i -> (Integer) i;
+  }
+
+  Function<Number, Integer> testClassReferenceCast() {
+    return i -> Integer.class.cast(i);
   }
 }
