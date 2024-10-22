880cbe5672109e3674c8b187c10856c7528c7772
Danylo Naumenko <danylo.naumenko@teampicnic.com>
2024-10-22 13:30:20 +0200

    Introduce MicrometerRules Refaster rule collection

---
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/MicrometerRulesTestInput.java
@@ -0,0 +1,60 @@
+package tech.picnic.errorprone.refasterrules;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import io.micrometer.core.instrument.Tag;
+import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
+
+final class MicrometerRulesTest implements RefasterRuleCollectionTestCase {
+  @Override
+  public ImmutableSet<Object> elidedTypesAndStaticImports() {
+    return ImmutableSet.of(ImmutableList.class);
+  }
+
+  ImmutableSet<Iterable<Tag>> testTagsOf1() {
+    return ImmutableSet.of(
+        ImmutableSet.of(Tag.of("foo", "bar")), ImmutableList.of(Tag.of("foo", "bar")));
+  }
+
+  ImmutableSet<Iterable<Tag>> testTagsOf2() {
+    return ImmutableSet.of(
+        ImmutableSet.of(Tag.of("foo", "bar"), Tag.of("foo1", "bar1")),
+        ImmutableList.of(Tag.of("foo", "bar"), Tag.of("foo1", "bar1")));
+  }
+
+  ImmutableSet<Iterable<Tag>> testTagsOf3() {
+    return ImmutableSet.of(
+        ImmutableSet.of(Tag.of("foo", "bar"), Tag.of("foo1", "bar1"), Tag.of("foo2", "bar2")),
+        ImmutableList.of(Tag.of("foo", "bar"), Tag.of("foo1", "bar1"), Tag.of("foo2", "bar2")));
+  }
+
+  ImmutableSet<Iterable<Tag>> testTagsOf4() {
+    return ImmutableSet.of(
+        ImmutableSet.of(
+            Tag.of("foo", "bar"),
+            Tag.of("foo1", "bar1"),
+            Tag.of("foo2", "bar2"),
+            Tag.of("foo3", "bar3")),
+        ImmutableList.of(
+            Tag.of("foo", "bar"),
+            Tag.of("foo1", "bar1"),
+            Tag.of("foo2", "bar2"),
+            Tag.of("foo3", "bar3")));
+  }
+
+  ImmutableSet<Iterable<Tag>> testTagsOf5() {
+    return ImmutableSet.of(
+        ImmutableSet.of(
+            Tag.of("foo", "bar"),
+            Tag.of("foo1", "bar1"),
+            Tag.of("foo2", "bar2"),
+            Tag.of("foo3", "bar3"),
+            Tag.of("foo4", "bar4")),
+        ImmutableList.of(
+            Tag.of("foo", "bar"),
+            Tag.of("foo1", "bar1"),
+            Tag.of("foo2", "bar2"),
+            Tag.of("foo3", "bar3"),
+            Tag.of("foo4", "bar4")));
+  }
+}
