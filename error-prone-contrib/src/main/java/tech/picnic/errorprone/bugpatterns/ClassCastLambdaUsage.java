caf2a869229463592a71cfffd18dd53d4fd7a48f
Mohamed Sameh <110535847+mohamedsamehsalah@users.noreply.github.com>
2024-10-29 10:35:36 +0100

    Introduce `ClassCastLambdaUsage` check (#1381)
    This new check replaces the `ClassLiteralCast` Refaster rule, as the 
    latter produced invalid code by suggesting expressions of the form
    `T.class::cast` for generic `T`.

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/ClassCastLambdaUsage.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/ClassCastLambdaUsage.java
@@ -14,47 +14,52 @@ import com.google.errorprone.bugpatterns.BugChecker.LambdaExpressionTreeMatcher;
 import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.util.ASTHelpers;
-import com.sun.source.tree.InstanceOfTree;
 import com.sun.source.tree.LambdaExpressionTree;
+import com.sun.source.tree.TypeCastTree;
 import com.sun.source.tree.VariableTree;
+import com.sun.tools.javac.code.Type;
 import tech.picnic.errorprone.utils.SourceCode;
 
 /**
  * A {@link BugChecker} that flags lambda expressions that can be replaced with a method reference
- * of the form {@code T.class::isInstance}.
+ * of the form {@code T.class::cast}.
  */
 // XXX: Consider folding this logic into the `MethodReferenceUsage` check of the
 // `error-prone-experimental` module.
-// XXX: This check and its tests are structurally nearly identical to `ClassCastLambdaUsage`. Unless
-// folded into `MethodReferenceUsage`, consider merging the two.
+// XXX: This check and its tests are structurally nearly identical to `IsInstanceLambdaUsage`.
+// Unless folded into `MethodReferenceUsage`, consider merging the two.
 @AutoService(BugChecker.class)
 @BugPattern(
-    summary = "Prefer `Class::isInstance` method reference over equivalent lambda expression",
-    link = BUG_PATTERNS_BASE_URL + "IsInstanceLambdaUsage",
+    summary = "Prefer `Class::cast` method reference over equivalent lambda expression",
+    link = BUG_PATTERNS_BASE_URL + "ClassCastLambdaUsage",
     linkType = CUSTOM,
     severity = SUGGESTION,
     tags = SIMPLIFICATION)
-public final class IsInstanceLambdaUsage extends BugChecker implements LambdaExpressionTreeMatcher {
+public final class ClassCastLambdaUsage extends BugChecker implements LambdaExpressionTreeMatcher {
   private static final long serialVersionUID = 1L;
 
-  /** Instantiates a new {@link IsInstanceLambdaUsage} instance. */
-  public IsInstanceLambdaUsage() {}
+  /** Instantiates a new {@link ClassCastLambdaUsage} instance. */
+  public ClassCastLambdaUsage() {}
 
   @Override
   public Description matchLambdaExpression(LambdaExpressionTree tree, VisitorState state) {
-    if (tree.getParameters().size() != 1
-        || !(tree.getBody() instanceof InstanceOfTree instanceOf)) {
+    if (tree.getParameters().size() != 1 || !(tree.getBody() instanceof TypeCastTree typeCast)) {
+      return Description.NO_MATCH;
+    }
+
+    Type type = ASTHelpers.getType(typeCast);
+    if (type == null || type.isParameterized()) {
       return Description.NO_MATCH;
     }
 
     VariableTree param = Iterables.getOnlyElement(tree.getParameters());
-    if (!ASTHelpers.getSymbol(param).equals(ASTHelpers.getSymbol(instanceOf.getExpression()))) {
+    if (!ASTHelpers.getSymbol(param).equals(ASTHelpers.getSymbol(typeCast.getExpression()))) {
       return Description.NO_MATCH;
     }
 
     return describeMatch(
         tree,
         SuggestedFix.replace(
-            tree, SourceCode.treeToString(instanceOf.getType(), state) + ".class::isInstance"));
+            tree, SourceCode.treeToString(typeCast.getType(), state) + ".class::cast"));
   }
 }
