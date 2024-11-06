06aa53e9b9acf170159fc7a421b9b90ab8e122bf
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2024-11-06 13:56:15 +0100

    Add fix for illegalaccess

--- workshop/src/main/java/tech/picnic/errorprone/workshop/refasterrules/WorkshopAssignment0Rules.java
+++ workshop/src/main/java/tech/picnic/errorprone/workshop/refasterrules/WorkshopAssignment0Rules.java
@@ -1,9 +1,23 @@
 package tech.picnic.errorprone.workshop.refasterrules;
 
+import com.google.errorprone.refaster.annotation.AfterTemplate;
+import com.google.errorprone.refaster.annotation.BeforeTemplate;
+import org.springframework.context.annotation.Bean;
+
 /** Refaster rule used as example for the assignments of the workshop. */
 final class WorkshopAssignment0Rules {
   private WorkshopAssignment0Rules() {}
 
   /** Prefer {@link String#isEmpty()} over alternatives that consult the string's length. */
-  static final class ExampleStringIsEmpty {}
+  static final class ExampleStringIsEmpty {
+    @BeforeTemplate
+    boolean before(String str) {
+      return str.length() == 0;
+    }
+
+    @AfterTemplate
+    boolean after(String str) {
+      return str.isEmpty();
+    }
+  }
 }
