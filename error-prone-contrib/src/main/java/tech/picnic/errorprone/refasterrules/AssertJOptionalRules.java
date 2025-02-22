1f6ee5bb551d1a1d3b3aae2305f2fe7fa4f6d6f4
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2025-02-22 13:41:11 +0100

    Demonstrate the issue

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/AssertJOptionalRules.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/AssertJOptionalRules.java
@@ -12,7 +12,6 @@ import java.util.function.Predicate;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.AbstractObjectAssert;
 import org.assertj.core.api.AbstractOptionalAssert;
-import org.assertj.core.api.AbstractStringAssert;
 import org.assertj.core.api.ObjectAssert;
 import org.assertj.core.api.OptionalAssert;
 import tech.picnic.errorprone.refaster.annotation.OnlineDocumentation;
@@ -106,7 +105,7 @@ final class AssertJOptionalRules {
 
   static final class AssertThatHasValue<T> {
     @BeforeTemplate
-    AbstractStringAssert<?> before(Optional<String> optional, String value) {
+    ObjectAssert<T> before(Optional<T> optional, T value) {
       return assertThat(optional.orElseThrow()).isEqualTo(value);
     }
 
