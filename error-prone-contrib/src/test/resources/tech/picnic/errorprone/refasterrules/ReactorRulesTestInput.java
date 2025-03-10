a0f959dbe1dd11be04ddb13d226a84d711a31a2e
Julian Broudy <Julian@Broudy.io>
2025-03-10 14:17:43 +0100

    Extend `FluxJust` Refaster rule

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/ReactorRulesTestInput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/ReactorRulesTestInput.java
@@ -188,7 +188,8 @@ final class ReactorRulesTest implements RefasterRuleCollectionTestCase {
   }
 
   ImmutableSet<Flux<Integer>> testFluxJust() {
-    return ImmutableSet.of(Flux.range(0, 1), Mono.just(2).repeat().take(1));
+    return ImmutableSet.of(
+        Flux.range(0, 1), Mono.just(2).repeat().take(1), Flux.fromIterable(ImmutableList.of(3)));
   }
 
   ImmutableSet<Mono<?>> testMonoIdentity() {
