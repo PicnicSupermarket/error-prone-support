b2760b4103a025056f27b10a654deb974b863c07
Tim te Beek <tim@moderne.io>
2025-06-04 12:36:25 +0200

    Add tests

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/AssertJThrowingCallableRulesTestOutput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/AssertJThrowingCallableRulesTestOutput.java
@@ -1,5 +1,6 @@
 package tech.picnic.errorprone.refasterrules;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatIOException;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -249,5 +250,13 @@ final class AssertJThrowingCallableRulesTest implements RefasterRuleCollectionTe
         assertThatThrownBy(() -> {})
             .isInstanceOf(IllegalArgumentException.class)
             .withFailMessage("foo %s %s", "bar", 1));
+  }
+
+  ImmutableSet<AbstractThrowableAssert<?, ? extends Throwable>>
+      testAbstractThrowableAssertHasCauseReference() {
+    IllegalArgumentException illegalArgumentException = new IllegalArgumentException();
+    IllegalStateException illegalStateException =
+        new IllegalStateException(illegalArgumentException);
+    return assertThat(illegalStateException).cause().isSameAs(illegalArgumentException);
   }
 }
