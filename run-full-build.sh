8ab7667d15cbd342088e0012e9cfe41580d8dcbf
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2025-06-12 11:58:51 +0200

    Raise build baseline to JDK 21 (#1686)
    For now the project still targets JDK 17, so this change does not impact
    users of the library.
    
    While there:
    - Simplify the build matrix configuration somewhat.
    - Drop the redundant `mvn --version` execution step.
    - Drop an obsolete comment.

--- run-full-build.sh
+++ run-full-build.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
+TARGET_JDK=17
+
 # Builds the project twice: once against the original Error Prone release,
 # using only Error Prone checks available on Maven Central, and once against
-# the Picnic Error Prone fork, additionally enabling all checks defined in this
-# project and any Error Prone checks available only from other artifact
-# repositories.
+# the Picnic Error Prone fork, additionally (a) enabling all checks defined in
+# this project and (b) with tests executed against the oldest JDK supported.
 
 set -e -u -o pipefail
 
 settings="$(dirname "${0}")/settings.xml"
+toolchains="${HOME}/.m2/toolchains.xml"
+
+if ! grep -q "<version>${TARGET_JDK}</version>" "${toolchains}"; then
+  echo "Error: JDK version ${TARGET_JDK} not specified in ${toolchains}." 1>&2
+  exit 1
+fi
 
 mvn clean install \
   -T1C \
   -s "${settings}" \
   $@
-mvn clean install \
+
+mvn clean verify \
   -T1C \
   -s "${settings}" \
   -Perror-prone-fork \
   -Pself-check \
+  -Dsurefire.jdk-toolchain-version="${TARGET_JDK}" \
   $@
