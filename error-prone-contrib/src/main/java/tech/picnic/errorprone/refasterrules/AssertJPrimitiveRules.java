0f421279a0eaf52ee21ce9d942e56410a07cfad5
Tim te Beek <tim@moderne.io>
2025-07-21 11:42:33 +0200

    Drop array length rules from AssertJPrimitiveRules

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/AssertJPrimitiveRules.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/AssertJPrimitiveRules.java
@@ -9,8 +9,6 @@ import com.google.errorprone.refaster.annotation.BeforeTemplate;
 import com.google.errorprone.refaster.annotation.UseImportPolicy;
 import org.assertj.core.api.AbstractBooleanAssert;
 import org.assertj.core.api.AbstractDoubleAssert;
-import org.assertj.core.api.AbstractIntegerAssert;
-import org.assertj.core.api.AbstractObjectArrayAssert;
 import tech.picnic.errorprone.refaster.annotation.OnlineDocumentation;
 
 @OnlineDocumentation
@@ -114,97 +112,6 @@ final class AssertJPrimitiveRules {
     @UseImportPolicy(STATIC_IMPORT_ALWAYS)
     AbstractDoubleAssert<?> after(double actual, double expected) {
       return assertThat(actual).isGreaterThanOrEqualTo(expected);
-    }
-  }
-
-  static final class AssertThatArrayIsEmpty<T> {
-    @BeforeTemplate
-    void before(T[] array) {
-      assertThat(array.length).isZero();
-    }
-
-    @AfterTemplate
-    @UseImportPolicy(STATIC_IMPORT_ALWAYS)
-    void after(T[] array) {
-      assertThat(array).isEmpty();
-    }
-  }
-
-  static final class AssertThatArrayHasSize<T> {
-    @BeforeTemplate
-    AbstractIntegerAssert<?> before(T[] array, int size) {
-      return assertThat(array.length).isEqualTo(size);
-    }
-
-    @AfterTemplate
-    @UseImportPolicy(STATIC_IMPORT_ALWAYS)
-    AbstractObjectArrayAssert<?, T> after(T[] array, int size) {
-      return assertThat(array).hasSize(size);
-    }
-  }
-
-  static final class AssertThatArrayHasSameSizeAs<T, U> {
-    @BeforeTemplate
-    AbstractIntegerAssert<?> before(T[] array1, U[] array2) {
-      return assertThat(array1.length).isEqualTo(array2.length);
-    }
-
-    @AfterTemplate
-    @UseImportPolicy(STATIC_IMPORT_ALWAYS)
-    AbstractObjectArrayAssert<?, T> after(T[] array1, U[] array2) {
-      return assertThat(array1).hasSameSizeAs(array2);
-    }
-  }
-
-  static final class AssertThatArrayHasSizeLessThanOrEqualTo<T> {
-    @BeforeTemplate
-    AbstractIntegerAssert<?> before(T[] array, int size) {
-      return assertThat(array.length).isLessThanOrEqualTo(size);
-    }
-
-    @AfterTemplate
-    @UseImportPolicy(STATIC_IMPORT_ALWAYS)
-    AbstractObjectArrayAssert<?, T> after(T[] array, int size) {
-      return assertThat(array).hasSizeLessThanOrEqualTo(size);
-    }
-  }
-
-  static final class AssertThatArrayHasSizeLessThan<T> {
-    @BeforeTemplate
-    AbstractIntegerAssert<?> before(T[] array, int size) {
-      return assertThat(array.length).isLessThan(size);
-    }
-
-    @AfterTemplate
-    @UseImportPolicy(STATIC_IMPORT_ALWAYS)
-    AbstractObjectArrayAssert<?, T> after(T[] array, int size) {
-      return assertThat(array).hasSizeLessThan(size);
-    }
-  }
-
-  static final class AssertThatArrayHasSizeGreaterThan<T> {
-    @BeforeTemplate
-    AbstractIntegerAssert<?> before(T[] array, int size) {
-      return assertThat(array.length).isGreaterThan(size);
-    }
-
-    @AfterTemplate
-    @UseImportPolicy(STATIC_IMPORT_ALWAYS)
-    AbstractObjectArrayAssert<?, T> after(T[] array, int size) {
-      return assertThat(array).hasSizeGreaterThan(size);
-    }
-  }
-
-  static final class AssertThatArrayHasSizeGreaterThanOrEqualTo<T> {
-    @BeforeTemplate
-    AbstractIntegerAssert<?> before(T[] array, int size) {
-      return assertThat(array.length).isGreaterThanOrEqualTo(size);
-    }
-
-    @AfterTemplate
-    @UseImportPolicy(STATIC_IMPORT_ALWAYS)
-    AbstractObjectArrayAssert<?, T> after(T[] array, int size) {
-      return assertThat(array).hasSizeGreaterThanOrEqualTo(size);
     }
   }
 }
--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/AssertJPrimitiveRulesTestOutput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/AssertJPrimitiveRulesTestOutput.java
@@ -120,32 +120,4 @@ final class AssertJPrimitiveRulesTest implements RefasterRuleCollectionTestCase 
         assertThat(1.0).isGreaterThanOrEqualTo(2.0),
         assertThat(1.0).isGreaterThanOrEqualTo(2.0));
   }
-
-  void testAssertThatArrayIsEmpty() {
-    assertThat(new String[0]).isEmpty();
-  }
-
-  AbstractAssert<?, ?> testAssertThatArrayHasSize() {
-    return assertThat(new String[7]).hasSize(7);
-  }
-
-  AbstractAssert<?, ?> testAssertThatArrayHasSameSizeAs() {
-    return assertThat(new String[3]).hasSameSizeAs(new Integer[3]);
-  }
-
-  AbstractAssert<?, ?> testAssertThatArrayHasSizeLessThanOrEqualTo() {
-    return assertThat(new String[2]).hasSizeLessThanOrEqualTo(2);
-  }
-
-  AbstractAssert<?, ?> testAssertThatArrayHasSizeLessThan() {
-    return assertThat(new String[4]).hasSizeLessThan(5);
-  }
-
-  AbstractAssert<?, ?> testAssertThatArrayHasSizeGreaterThan() {
-    return assertThat(new String[5]).hasSizeGreaterThan(4);
-  }
-
-  AbstractAssert<?, ?> testAssertThatArrayHasSizeGreaterThanOrEqualTo() {
-    return assertThat(new String[1]).hasSizeGreaterThanOrEqualTo(1);
-  }
 }
