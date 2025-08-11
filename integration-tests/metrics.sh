be06b34b48f1d8d76e07f29318ea6fd2ad5ef8b7
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2025-08-11 21:08:47 +0200

    Stabilize `metrics` integration test setup
    By disabling two identically-named tests that attempt to connect to
    `example.com:80`. This connection operation frequently times out.

--- integration-tests/metrics.sh
+++ integration-tests/metrics.sh
@@ -11,7 +11,15 @@ additional_source_directories=''
 shared_error_prone_flags='-XepExcludedPaths:.*/target/generated-sources/.* -XepOpt:Slf4jLoggerDeclaration:CanonicalStaticLoggerName=LOGGER'
 patch_error_prone_flags=''
 validation_error_prone_flags=''
-validation_build_flags=''
+# Validation skips two instances of the
+# `registersExpectedMetricsGivenNameStrategy` test because it attemps to
+# connect to `example.com:80`, an operation that often times out. This in turn
+# also requires specifying `-Dsurefire.failIfNoSpecifiedTests=false`, as
+# otherwise Surefire complains that no tests are found in the `docs` module.
+# Note that enabling the tests would also require adding `example.com:80` to
+# the set of allowed endpoints in the integration tests GitHub Actions
+# Harden-Runner configuration.
+validation_build_flags='-Dtest=!InstrumentedHttpClientsTest#registersExpectedMetricsGivenNameStrategy -Dsurefire.failIfNoSpecifiedTests=false'
 
 if [ "${#}" -gt 2 ] || ([ "${#}" = 2 ] && [ "${1:---sync}" != '--sync' ]); then
   >&2 echo "Usage: ${0} [--sync] [<report_directory>]"
