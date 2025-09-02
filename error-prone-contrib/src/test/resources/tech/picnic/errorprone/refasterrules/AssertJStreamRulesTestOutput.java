d96c59ebc8b51b550ce43ce2e6e79112bff90af8
Thanos Kirmizis <athanasios.kirmizis@teampicnic.com>
2025-09-02 13:00:44 +0200

    Introduce Refaster rules for usage of idiomatic AssertJ APIs for Streams

---
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/AssertJStreamRulesTestOutput.java
@@ -0,0 +1,48 @@
+package tech.picnic.errorprone.refasterrules;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.ImmutableSet;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import org.assertj.core.api.AbstractAssert;
+import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
+
+final class AssertJStreamRulesTest implements RefasterRuleCollectionTestCase {
+  public ImmutableSet<Object> elidedTypesAndStaticImports() {
+    return ImmutableSet.of();
+  }
+
+  AbstractAssert<?, ?> testAssertThatStreamFilteredOn() {
+    Predicate<Integer> pred = i -> i > 0;
+    return assertThat(Stream.of(1, 2, 3)).filteredOn(pred);
+  }
+
+  void testAssertThatStreamNoneMatch() {
+    Predicate<Integer> pred = i -> i > 0;
+    assertThat(Stream.of(1, 2, 3)).noneMatch(pred);
+    assertThat(Stream.of(1, 2, 3)).noneMatch(pred);
+    assertThat(Stream.of(1, 2, 3)).noneMatch(pred);
+  }
+
+  void testAssertThatStreamAnyMatch() {
+    Predicate<Integer> pred = i -> i > 0;
+    assertThat(Stream.of(1, 2, 3)).anyMatch(pred);
+    assertThat(Stream.of(1, 2, 3)).anyMatch(pred);
+    assertThat(Stream.of(1, 2, 3)).anyMatch(pred);
+  }
+
+  AbstractAssert<?, ?> testAssertThatCollectionStream() {
+    return assertThat(ImmutableSet.of("a", "b"));
+  }
+
+  void testAssertThatStreamCountZero() {
+    assertThat(Stream.of()).isEmpty();
+    assertThat(Stream.of()).isEmpty();
+  }
+
+  ImmutableSet<AbstractAssert<?, ?>> testAssertThatStreamSingleElement() {
+    return ImmutableSet.of(
+        assertThat(Stream.of(42)).singleElement(), assertThat(Stream.of(43)).singleElement());
+  }
+}
