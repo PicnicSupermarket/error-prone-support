e3092df92e2cedd713c5ace1ca5a560488f5ee97
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2025-11-15 13:10:28 +0100

    Migrate from Jackson 2 to Jackson 3
    Summary of changes:
    - Migrate documentation generation logic to Jackson 3.
    - Drop explicit exception handling in documentation generation logic.
    - Replace `JacksonRules` Refaster collection with Jackson 2 and Jackson
      3 equivalents.

--- error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/Jackson3RulesTestOutput.java
+++ error-prone-contrib/src/test/resources/tech/picnic/errorprone/refasterrules/Jackson3RulesTestOutput.java
@@ -1,14 +1,12 @@
 package tech.picnic.errorprone.refasterrules;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.NullNode;
 import com.google.common.collect.ImmutableSet;
 import java.util.Optional;
 import tech.picnic.errorprone.refaster.test.RefasterRuleCollectionTestCase;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.node.NullNode;
 
-final class JacksonRulesTest implements RefasterRuleCollectionTestCase {
+final class Jackson3RulesTest implements RefasterRuleCollectionTestCase {
   ImmutableSet<Optional<JsonNode>> testJsonNodeOptionalInt() {
     return ImmutableSet.of(
         NullNode.getInstance().optional(1),
@@ -23,9 +21,5 @@ final class JacksonRulesTest implements RefasterRuleCollectionTestCase {
         NullNode.getInstance().optional("bar"),
         NullNode.getInstance().optional("baz"),
         NullNode.getInstance().optional("qux"));
-  }
-
-  JsonNode testObjectMapperValueToTree() throws JsonProcessingException {
-    return new ObjectMapper().valueToTree("foo");
   }
 }
