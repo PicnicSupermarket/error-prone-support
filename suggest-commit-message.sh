d0599c8b25b7ff6f8cd54ea276294d284aaf93f3
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2025-11-29 12:43:30 +0100

    WIP
    TODO:
    - Add more test cases
    - Run more tests
    - Tweak promps
    - Rename (and move?) new files

--- suggest-commit-message.sh
+++ suggest-commit-message.sh
@@ -9,16 +9,20 @@ set -e -u -o pipefail
 workflow='suggest-commit-message.yml'
 branch="${1:?Specify a branch}"
 pr_number="${2:?Specify a PR number}"
+dry_run="${3:-false}"
+lock_file="${4:-$(mktemp)}"
 
-gh workflow run "${workflow}" --ref "${branch}" -f pr_number="${pr_number}"
+run_id="$(
+  xargs -0 <<EOF flock "${lock_file}" sh -c
+    gh workflow run '${workflow}' --ref '${branch}' -f pr_number='${pr_number}' -f dry_run='${dry_run}' 1>/dev/null
 
-# The new run may not start immediately, so we wait a bit.
-sleep 30
+    # The new run may not start immediately, so we wait a bit.
+    sleep 10
 
-run_id=$(
-  gh run list --workflow="${workflow}" --event workflow_dispatch --limit 1 --json databaseId \
-    | jq -r '.[].databaseId'
-)
+    gh run list --workflow='${workflow}' --event workflow_dispatch --limit 1 --json databaseId \
+      | jq -r '.[].databaseId'
+EOF
+)"
 
 gh run watch "${run_id}"
 
@@ -26,4 +30,6 @@ tmp_dir="$(mktemp -d)"
 trap 'rm -rf -- "${tmp_dir}"' INT TERM HUP EXIT
 
 gh run download -n suggested-commit-message --dir "${tmp_dir}" "${run_id}"
+echo 'Suggested commit message:'
+echo '---'
 cat "${tmp_dir}/suggested-commit-message.txt"
