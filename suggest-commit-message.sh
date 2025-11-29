84337881ad5f47153218b1dc1c6de3400fafd3b0
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2025-11-29 12:43:30 +0100

    Introduce suggested commit message workflow integration tests
    TODO:
    - Wait for `sschroevers/introduce-agentmd` to be merged.
    - Move the test files somewhere else. Maybe into a "suggested commit
      message improvement" skill? It would document that pushing the changes
      is required.

--- suggest-commit-message.sh
+++ suggest-commit-message.sh
@@ -9,16 +9,12 @@ set -e -u -o pipefail
 workflow='suggest-commit-message.yml'
 branch="${1:?Specify a branch}"
 pr_number="${2:?Specify a PR number}"
+dry_run="${3:-false}"
 
-gh workflow run "${workflow}" --ref "${branch}" -f pr_number="${pr_number}"
-
-# The new run may not start immediately, so we wait a bit.
-sleep 30
-
-run_id=$(
-  gh run list --workflow="${workflow}" --event workflow_dispatch --limit 1 --json databaseId \
-    | jq -r '.[].databaseId'
-)
+run_id="$(
+  gh workflow run "${workflow}" --ref "${branch}" -f pr_number="${pr_number}" -f dry_run="${dry_run}" \
+    | grep -oP '(?<=https://github.com/PicnicSupermarket/error-prone-support/actions/runs/)\d+'
+)"
 
 gh run watch "${run_id}"
 
@@ -26,4 +22,6 @@ tmp_dir="$(mktemp -d)"
 trap 'rm -rf -- "${tmp_dir}"' INT TERM HUP EXIT
 
 gh run download -n suggested-commit-message --dir "${tmp_dir}" "${run_id}"
+echo 'Suggested commit message:'
+echo '---'
 cat "${tmp_dir}/suggested-commit-message.txt"
