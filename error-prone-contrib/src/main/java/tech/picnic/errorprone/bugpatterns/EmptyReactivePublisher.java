98016a02de46fa26ec62de52b649d2339a8e5eed
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2025-12-05 10:55:03 +0100

    Suggestions

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/EmptyReactivePublisher.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/EmptyReactivePublisher.java
@@ -8,13 +8,14 @@ import static com.google.errorprone.matchers.Matchers.instanceMethod;
 import static com.google.errorprone.matchers.Matchers.isSubtypeOf;
 import static com.google.errorprone.matchers.Matchers.staticMethod;
 import static com.google.errorprone.matchers.Matchers.typePredicateMatcher;
+import static com.google.errorprone.suppliers.Suppliers.JAVA_LANG_VOID_TYPE;
+import static java.util.Objects.requireNonNull;
 import static tech.picnic.errorprone.utils.Documentation.BUG_PATTERNS_BASE_URL;
 import static tech.picnic.errorprone.utils.MoreTypePredicates.isSubTypeOf;
 import static tech.picnic.errorprone.utils.MoreTypes.generic;
 import static tech.picnic.errorprone.utils.MoreTypes.type;
 
 import com.google.auto.service.AutoService;
-import com.google.common.collect.Iterables;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
@@ -22,7 +23,6 @@ import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.suppliers.Supplier;
-import com.google.errorprone.suppliers.Suppliers;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.LambdaExpressionTree;
@@ -49,26 +49,16 @@ import reactor.core.publisher.Mono;
 public final class EmptyReactivePublisher extends BugChecker
     implements MethodInvocationTreeMatcher {
   private static final long serialVersionUID = 1L;
-  private static final Supplier<Type> MONO =
-      Suppliers.typeFromString("reactor.core.publisher.Mono");
-  private static final Supplier<Type> FLUX =
-      Suppliers.typeFromString("reactor.core.publisher.Flux");
+  private static final Supplier<Type> PUBLISHER = type("org.reactivestreams.Publisher");
   private static final Matcher<Tree> CONSUMER =
       isSubtypeOf(type(Consumer.class.getCanonicalName()));
-  private static final Supplier<Type> VOID = type(Void.class.getCanonicalName());
-
-  private static final Matcher<ExpressionTree> EMPTY_FLUX =
+  private static final Matcher<ExpressionTree> EMPTY_PUBLISHER =
       anyOf(
-          staticMethod().onDescendantOf(FLUX).named("empty"),
-          typePredicateMatcher(isSubTypeOf(generic(FLUX, VOID))));
-  private static final Matcher<ExpressionTree> EMPTY_MONO =
-      anyOf(
-          staticMethod().onDescendantOf(MONO).named("empty"),
-          typePredicateMatcher(isSubTypeOf(generic(MONO, VOID))));
-
+          staticMethod().onDescendantOf(PUBLISHER).named("empty"),
+          typePredicateMatcher(isSubTypeOf(generic(PUBLISHER, JAVA_LANG_VOID_TYPE))));
   private static final Matcher<ExpressionTree> VACUOUS_EMPTY_FLUX_OPERATORS =
       instanceMethod()
-          .onDescendantOf(FLUX)
+          .onDescendantOf(type("reactor.core.publisher.Flux"))
           .namedAnyOf(
               "concatMap",
               "doOnNext",
@@ -80,53 +70,52 @@ public final class EmptyReactivePublisher extends BugChecker
               "map");
   private static final Matcher<ExpressionTree> VACUOUS_EMPTY_MONO_OPERATORS =
       instanceMethod()
-          .onDescendantOf(MONO)
+          .onDescendantOf(type("reactor.core.publisher.Mono"))
           .namedAnyOf(
               "doOnNext", "filter", "flatMap", "flatMapMany", "flatMapIterable", "handle", "map");
-
   private static final Matcher<ExpressionTree> SUBSCRIBE =
-      instanceMethod()
-          .onDescendantOf(Suppliers.typeFromString("org.reactivestreams.Publisher"))
-          .named("subscribe");
+      instanceMethod().onDescendantOf(PUBLISHER).named("subscribe");
 
   /** Instantiates a new {@link EmptyReactivePublisher} instance. */
   public EmptyReactivePublisher() {}
 
   @Override
   public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
-    ExpressionTree receiver = ASTHelpers.getReceiver(tree);
-    if (receiver == null
-        || (!EMPTY_FLUX.matches(receiver, state) && !EMPTY_MONO.matches(receiver, state))) {
+    ExpressionTree receiver =
+        requireNonNull(
+            ASTHelpers.getReceiver(tree), "Instance method invocation must have receiver");
+    if (!EMPTY_PUBLISHER.matches(receiver, state)) {
       return Description.NO_MATCH;
     }
 
     if (SUBSCRIBE.matches(tree, state)) {
       // First argument passed to `#subscribe` is always an on next signal `Consumer`.
-      ExpressionTree firstArgument = Iterables.getFirst(tree.getArguments(), null);
+      ExpressionTree firstArgument = tree.getArguments().getFirst();
       if ((firstArgument instanceof LambdaExpressionTree
               || firstArgument instanceof MemberReferenceTree)
           && CONSUMER.matches(firstArgument, state)) {
         return buildDescription(firstArgument)
-            .setMessage("Passing an on next signal `Consumer` on empty `Publisher`s is a no-op")
+            .setMessage("Passing an on next signal `Consumer` on empty `Publisher` is a no-op")
             .build();
       }
     }
 
-    if (EMPTY_FLUX.matches(receiver, state) && VACUOUS_EMPTY_FLUX_OPERATORS.matches(tree, state)) {
-      return buildDescription(tree)
-          .setMessage(
-              "Operator `%s` on an empty `Flux`s is a no-op"
-                  .formatted(ASTHelpers.getSymbol(tree).getSimpleName()))
-          .build();
+    if (VACUOUS_EMPTY_FLUX_OPERATORS.matches(tree, state)) {
+      return buildDescriptionForType(tree, "Flux");
     }
 
-    if (EMPTY_MONO.matches(receiver, state) && VACUOUS_EMPTY_MONO_OPERATORS.matches(tree, state)) {
-      return buildDescription(tree)
-          .setMessage(
-              "Operator `%s` on an empty `Mono`s is a no-op"
-                  .formatted(ASTHelpers.getSymbol(tree).getSimpleName()))
-          .build();
+    if (VACUOUS_EMPTY_MONO_OPERATORS.matches(tree, state)) {
+      return buildDescriptionForType(tree, "Mono");
     }
+
     return Description.NO_MATCH;
+  }
+
+  private Description buildDescriptionForType(MethodInvocationTree tree, String type) {
+    return buildDescription(tree)
+        .setMessage(
+            "Operator `%s` on an empty `%s` is a no-op"
+                .formatted(ASTHelpers.getSymbol(tree).getSimpleName(), type))
+        .build();
   }
 }
