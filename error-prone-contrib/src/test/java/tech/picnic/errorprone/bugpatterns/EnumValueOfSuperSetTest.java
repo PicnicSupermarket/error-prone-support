99691c2536e3130b2f388f36d1d8d5e42a1b35ec
Gokhun Celik <gokhun.celik@teampicnic.com>
2025-12-12 18:38:20 +0100

    Add bug pattern for unchecked enum creation with valueOf

---
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/EnumValueOfSuperSetTest.java
@@ -0,0 +1,28 @@
+package tech.picnic.errorprone.bugpatterns;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.jupiter.api.Test;
+
+final class EnumValueOfSuperSetTest {
+  @Test
+  void identification() {
+    CompilationTestHelper.newInstance(EnumValueOfSuperSet.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "class Test {",
+            "  A convertUnsafe(B b) {",
+            "    // BUG: Diagnostic contains:",
+            "    return A.valueOf(b.name());",
+            "  }",
+            "",
+            "  enum A {",
+            "    ONE, TWO, THREE",
+            "  }",
+            "",
+            "  enum B {",
+            "    ONE, TWO, THREE, FOUR",
+            "  }",
+            "}")
+        .doTest();
+  }
+}
