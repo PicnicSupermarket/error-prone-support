fec8838ec9f7f0902ad28861d8f04fc18026f03a
Tim te Beek <tim@moderne.io>
2026-01-26 00:29:38 +0100

    Apply formatter

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/StringRules.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/StringRules.java
@@ -217,8 +217,8 @@ final class StringRules {
   }
 
   /**
-   * Prefer {@link String#join(CharSequence, CharSequence...)} over
-   * {@code Stream.of(...).collect(joining(...))}.
+   * Prefer {@link String#join(CharSequence, CharSequence...)} over {@code
+   * Stream.of(...).collect(joining(...))}.
    */
   static final class JoinStringsWithVarargs {
     @BeforeTemplate
