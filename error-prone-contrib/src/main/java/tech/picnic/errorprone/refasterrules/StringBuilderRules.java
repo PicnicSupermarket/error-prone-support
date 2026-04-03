9b5932fc1ce2f0ed154ac1c0669f38af75f37ec9
Your Name <stephan.schroevers@teampicnic.com>
2026-04-04 00:15:11 +0200

    Address feedback

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/StringBuilderRules.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/refasterrules/StringBuilderRules.java
@@ -9,7 +9,15 @@ import tech.picnic.errorprone.refaster.annotation.OnlineDocumentation;
 final class StringBuilderRules {
   private StringBuilderRules() {}
 
-  /** Prefer {@link StringBuilder#repeat(CharSequence, int)} over less efficient alternatives. */
+  /**
+   * Prefer {@link StringBuilder#repeat(CharSequence, int)} over less efficient alternatives.
+   *
+   * <p><strong>Warning:</strong> this rule is not behavior preserving: while the original code
+   * throws a {@link NullPointerException} if the repeated string is {@code null}, the replacement
+   * code will repeat the literal string {@code "null"}.
+   */
+  // XXX: Introduce an Error Prone check that replaces single-character strings with `char`s where
+  // possible. Here, that would enable invoking the `StringBuilder#repeat(int, int)` overload.
   static final class StringBuilderRepeat {
     @BeforeTemplate
     StringBuilder before(StringBuilder sb, String str, int count) {
