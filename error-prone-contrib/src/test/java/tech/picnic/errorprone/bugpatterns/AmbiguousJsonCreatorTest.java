446e504e92a7d4e8bb273b051788201ad0f83729
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2026-09-17 16:16:28 +0200

    Revert "Test"
    This reverts commit 465d7d1cba9e21dadb62c53418432bb04894a367.

--- error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/AmbiguousJsonCreatorTest.java
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/AmbiguousJsonCreatorTest.java
@@ -4,7 +4,7 @@ import com.google.errorprone.BugCheckerRefactoringTestHelper;
 import com.google.errorprone.CompilationTestHelper;
 import org.junit.jupiter.api.Test;
 
-public final class AmbiguousJsonCreatorTest {
+final class AmbiguousJsonCreatorTest {
   @Test
   void identification() {
     CompilationTestHelper.newInstance(AmbiguousJsonCreator.class, getClass())
