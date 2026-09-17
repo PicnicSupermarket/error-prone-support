1cb0b4d87c9eafb0cd95fd381119289c9ab1463f
Jakob Löhnertz <mail@jakob.codes>
2026-09-17 15:23:07 +0200

    Introduce `FluxGroupByUsage` check

---
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/FluxGroupByUsage.java
@@ -0,0 +1,63 @@
+package tech.picnic.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.LinkType.CUSTOM;
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+import static com.google.errorprone.BugPattern.StandardTags.CONCURRENCY;
+import static com.google.errorprone.BugPattern.StandardTags.LIKELY_ERROR;
+import static com.google.errorprone.matchers.method.MethodMatchers.instanceMethod;
+import static tech.picnic.errorprone.utils.Documentation.BUG_PATTERNS_BASE_URL;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.MemberReferenceTreeMatcher;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MemberReferenceTree;
+import com.sun.source.tree.MethodInvocationTree;
+import java.util.function.Function;
+import reactor.core.publisher.Flux;
+
+/**
+ * A {@link BugChecker} that flags usages of {@link Flux#groupBy(Function)}.
+ *
+ * <p>The groups emitted by {@link Flux#groupBy(Function)} are live views of the source and must be
+ * drained downstream. If the number of groups exceeds the downstream subscription concurrency,
+ * backpressure can prevent both the groups and their source from making progress. This can result
+ * in a deadlock.
+ *
+ * <p>As safe cardinality and consumption cannot generally be established statically, this check
+ * flags all usages. Suppress it only when the number of groups is provably small and the downstream
+ * consumption strategy is known to be safe.
+ */
+@AutoService(BugChecker.class)
+@BugPattern(
+    summary =
+        "`Flux#groupBy` can deadlock; prefer explicit aggregation or suppress this check for a "
+            + "proven low-cardinality use case",
+    link = BUG_PATTERNS_BASE_URL + "FluxGroupByUsage",
+    linkType = CUSTOM,
+    severity = ERROR,
+    tags = {CONCURRENCY, LIKELY_ERROR})
+public final class FluxGroupByUsage extends BugChecker
+    implements MethodInvocationTreeMatcher, MemberReferenceTreeMatcher {
+  private static final long serialVersionUID = 1L;
+  private static final Matcher<ExpressionTree> FLUX_GROUP_BY =
+      instanceMethod().onDescendantOf("reactor.core.publisher.Flux").named("groupBy");
+
+  /** Instantiates a new {@link FluxGroupByUsage} instance. */
+  public FluxGroupByUsage() {}
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    return FLUX_GROUP_BY.matches(tree, state) ? describeMatch(tree) : Description.NO_MATCH;
+  }
+
+  @Override
+  public Description matchMemberReference(MemberReferenceTree tree, VisitorState state) {
+    return FLUX_GROUP_BY.matches(tree, state) ? describeMatch(tree) : Description.NO_MATCH;
+  }
+}
---
+++ error-prone-contrib/src/test/java/tech/picnic/errorprone/bugpatterns/FluxGroupByUsageTest.java
@@ -0,0 +1,47 @@
+package tech.picnic.errorprone.bugpatterns;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.jupiter.api.Test;
+
+final class FluxGroupByUsageTest {
+  @Test
+  void identification() {
+    CompilationTestHelper.newInstance(FluxGroupByUsage.class, getClass())
+        .addSourceLines(
+            "A.java",
+            "import java.util.function.BiFunction;",
+            "import java.util.function.Function;",
+            "import reactor.core.publisher.Flux;",
+            "import reactor.core.publisher.GroupedFlux;",
+            "",
+            "class A {",
+            "  void m() {",
+            "    new Grouper().groupBy();",
+            "",
+            "    // BUG: Diagnostic contains:",
+            "    Flux.just(1).groupBy(Object::toString);",
+            "    // BUG: Diagnostic contains:",
+            "    Flux.just(2).groupBy(Object::toString, 1);",
+            "    // BUG: Diagnostic contains:",
+            "    Flux.just(3).groupBy(Object::toString, Object::toString);",
+            "    // BUG: Diagnostic contains:",
+            "    Flux.just(4).groupBy(Object::toString, Object::toString, 1);",
+            "",
+            "    // BUG: Diagnostic contains:",
+            "    this.<Integer, Flux<GroupedFlux<Integer, Integer>>>sink(Flux::groupBy);",
+            "  }",
+            "",
+            "  @SuppressWarnings(\"FluxGroupByUsage\")",
+            "  void suppressed() {",
+            "    Flux.just(5).groupBy(i -> i % 2 == 0);",
+            "  }",
+            "",
+            "  private <T, R> void sink(BiFunction<Flux<T>, Function<T, T>, R> function) {}",
+            "",
+            "  private static final class Grouper {",
+            "    void groupBy() {}",
+            "  }",
+            "}")
+        .doTest();
+  }
+}
